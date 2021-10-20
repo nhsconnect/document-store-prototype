f93a0f2e49fd57a297441eb22fcb967bb10b1f24
Mircea M <mircea.moise@gmail.com>
2021-10-20 13:58:46 +0100

    [MM] - NSD-26: Forcing terraform to re-deploy the API when there are changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,6 +115,15 @@ resource "aws_api_gateway_deployment" "api_deploy" {
 
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
   stage_name  = var.api_gateway_stage
+
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.lambda_api.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_lambda_permission" "api_gateway_permission_for_hello" {
