a24df9b9683b63e3183b7df13127aa28bd6c9213
Samradnyee <samradnyee.kolas@thoughtworks.com>
2021-10-26 13:52:48 +0100

    [MW/SK] NDS-30: Add put permissions to s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -142,6 +142,7 @@ resource "aws_iam_role_policy" "s3_get_document_data_policy" {
         "Effect" : "Allow",
         "Action" : [
           "s3:GetObject",
+          "s3:PutObject",
         ],
         "Resource" : "${aws_s3_bucket.document_store.arn}/*"
       }
