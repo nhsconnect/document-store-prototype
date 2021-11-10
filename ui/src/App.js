c0941dc327805ed5fe229ba92b915036cce600d1
Samradnyee <samradnyee.kolas@thoughtworks.com>
2021-11-10 16:51:35 +0000

    [MW/SK] Add Cognito authentication for login

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Amplify from "aws-amplify";
+import {AmplifyAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
+import awsconfig from "./aws-export";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+Amplify.configure(awsconfig);
+
+const App = () => (
+    <AmplifyAuthenticator>
+      <div>
+        Document Store
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
+);
 
 export default App;
---
+++ ui/src/aws-export.js
@@ -0,0 +1,19 @@
+const awsmobile = {
+    "aws_project_region": "eu-west-2",
+    "aws_cognito_region": "eu-west-2",
+    "aws_user_pools_id": "eu-west-2_x7sfxthL3",
+    "aws_user_pools_web_client_id": "664e0mj7v5igsoa8597l77h5vg",
+    "oauth": {
+        "domain": "doc-store-test.auth.eu-west-2.amazoncognito.com"
+    },
+    "aws_cognito_username_attributes": [],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [],
+    "aws_cognito_mfa_types": [],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": []
+};
+
+export default awsmobile;
