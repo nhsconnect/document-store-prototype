c3759ce598c996f5a1a4e916a4fbe7d65984b91d
Mike Winter <mike.winter1@nhs.net>
2021-11-10 11:08:41 +0000

    [MW/SK] NDS-37: Separate Amplify resources in to separate Terraform file

---
+++ terraform/amplify.tf
@@ -0,0 +1,23 @@
+resource "aws_amplify_app" "doc-store-ui" {
+  name = "DocStoreUi"
+
+  count = var.amplify_instances
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.doc-store-ui[0].id
+  branch_name = "main"
+
+  framework = "React"
+  stage     = "PRODUCTION"
+
+  environment_variables = {
+    REACT_APP_API_SERVER = aws_api_gateway_deployment.api_deploy.invoke_url
+  }
+
+  count = var.amplify_instances
+}
+
+output "amplify_app_ids" {
+  value = aws_amplify_app.doc-store-ui[*].id
+}
