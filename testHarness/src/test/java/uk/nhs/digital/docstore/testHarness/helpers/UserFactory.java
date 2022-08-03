856eb9024650651278c3aceeb9f4cb5e1f38f444
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-08-03 11:46:57 +0100

    MR-14: Remove debug output statement
    AD/MT

--- testHarness/src/test/java/uk/nhs/digital/docstore/testHarness/helpers/UserFactory.java
+++ testHarness/src/test/java/uk/nhs/digital/docstore/testHarness/helpers/UserFactory.java
@@ -4,8 +4,8 @@ import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.model.*;
 
 public class UserFactory {
-    private AWSCognitoIdentityProvider cognitoClient;
-    private String userPoolId;
+    private final AWSCognitoIdentityProvider cognitoClient;
+    private final String userPoolId;
 
     public UserFactory(AWSCognitoIdentityProvider cognitoClient, String userPoolId) {
         this.cognitoClient = cognitoClient;
@@ -24,10 +24,7 @@ public class UserFactory {
                 .withUserAttributes(emailAttr, emailVerifiedAttr)
                 .withMessageAction(MessageActionType.SUPPRESS);
 
-        AdminCreateUserResult createUserResult = cognitoClient.adminCreateUser(userRequest);
-
-        System.out.println("User " + createUserResult.getUser().getUsername()
-                + " is created. Status: " + createUserResult.getUser().getUserStatus());
+        cognitoClient.adminCreateUser(userRequest);
 
         // Make the password permanent and not temporary
         AdminSetUserPasswordRequest adminSetUserPasswordRequest = new AdminSetUserPasswordRequest()
