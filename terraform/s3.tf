ddfedd718577f562c2fa7b51bcc08c582cd634ac
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-08-04 11:54:42 +0100

    MR-14: Add CORS headers to doc store bucket config
    AD/CE/RB/SK/MT

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -26,6 +26,23 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   depends_on = [aws_lambda_permission.s3_permission_for_document_upload_event]
 }
 
+resource "aws_s3_bucket_cors_configuration" "document_store_bucket_cors_config" {
+  bucket = aws_s3_bucket.document_store.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT"]
+    allowed_origins = [var.cloud_only_service_instances > 0 ? "https://${aws_amplify_branch.main[0].branch_name}.${aws_amplify_app.doc-store-ui[0].id}.amplifyapp.com" : ""]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = [var.cloud_only_service_instances > 0 ? "https://${aws_amplify_branch.main[0].branch_name}.${aws_amplify_app.doc-store-ui[0].id}.amplifyapp.com" : ""]
+  }
+}
+
 output "document-store-bucket" {
   value = aws_s3_bucket.document_store.bucket
 }
