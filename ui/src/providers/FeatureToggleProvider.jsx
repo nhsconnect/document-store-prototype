b1137b7c506febd7db04b0565bbf4d1e94cfdece
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-08-05 14:24:26 +0100

    MR-15: Add a feature toggles provider to UI
    Enable features to be toggled on of off.
    
    RB/AD/SK/CE/MT

---
+++ ui/src/providers/FeatureToggleProvider.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+import config from "../config";
+
+const FeatureToggleContext = createContext();
+
+export const useFeatureToggle = (toggleName) => {
+    const featureToggles = useContext(FeatureToggleContext);
+    if (featureToggles?.[toggleName] === undefined) {
+        return false;
+    }
+    return featureToggles[toggleName];
+};
+
+const FeatureToggleProvider = ({ children }) => {
+    return (
+        <FeatureToggleContext.Provider
+            value={config.features[process.env.NODE_ENV]}
+        >
+            {children}
+        </FeatureToggleContext.Provider>
+    );
+};
+
+export default FeatureToggleProvider;
