b398290fd38b54c331d5008c401646bbdb4e2f5b
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-08-09 10:54:32 +0100

    MR-16: Collapse search component into page
    Remove the unnecessary extra level of abstraction.
    
    RB/MT

--- ui/src/pages/SearchPage.jsx
+++ ui/src/pages/SearchPage.jsx
@@ -1,15 +1,51 @@
-import Search from "../components/Search";
-import React from "react";
+import React, { useState } from "react";
+import { Input, Button, Table } from "nhsuk-react-components";
 
-const SearchPage = ({client}) => {
+const SearchPage = ({ client }) => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSubmit = async () => {
+        const results = await client.findByNhsNumber(searchTerm);
+        setSearchResults(results);
+    };
+
     return (
-        <>
-            <h2>View Stored Patient Record</h2>
+        <div>
             <div>
-                <Search apiClient={client} />
+                <Input
+                    id={"nhs-number-input"}
+                    value={searchTerm}
+                    label="Find by NHS number"
+                    onChange={(event) => setSearchTerm(event.target.value)}
+                />
+                <Button onClick={handleSubmit}>Search</Button>
             </div>
-        </>
-    )
-}
+            {searchResults.length > 0 && (
+                <Table caption="Documents">
+                    <Table.Head>
+                        <Table.Row>
+                            <Table.Cell>Description</Table.Cell>
+                            <Table.Cell>Type</Table.Cell>
+                            <Table.Cell />
+                        </Table.Row>
+                    </Table.Head>
 
-export default SearchPage
+                    <Table.Body>
+                        {searchResults.map((result, index) => (
+                            <Table.Row>
+                                <Table.Cell>{result.description}</Table.Cell>
+                                <Table.Cell>{result.type}</Table.Cell>
+                                <Table.Cell>
+                                    <a href={result.url}>Link</a>
+                                </Table.Cell>
+                            </Table.Row>
+                        ))}
+                    </Table.Body>
+                </Table>
+            )}
+        </div>
+    );
+};
+
+export default SearchPage;
