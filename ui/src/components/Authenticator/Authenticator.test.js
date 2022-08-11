eec13aac92c01396ad45191ec9185003a4c3bc8f
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-08-11 11:34:46 +0100

    MR-6: Create toggle describe block for authenticator tests
    CE/RB/AD/MT

--- ui/src/components/Authenticator/Authenticator.test.js
+++ ui/src/components/Authenticator/Authenticator.test.js
@@ -12,110 +12,115 @@ jest.mock("react-router", () => ({
 }));
 
 describe("Authenticator", () => {
-    let defaultAuthConfig;
+    describe("CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED feature toggle is active", () => {
+        let defaultAuthConfig;
 
-    beforeAll(() => {
-        defaultAuthConfig = config.Auth;
-        config.Auth.oauth = {
-            domain: "doc-store-user-pool.auth.eu-west-2.amazoncognito.com",
-            scope: ["openid"],
-            redirectSignIn: "http://localhost:3000",
-            redirectSignOut: "",
-            responseType: "code",
-        };
-    });
-
-    afterAll(() => {
-        config.Auth = defaultAuthConfig;
-        jest.clearAllMocks();
-    });
-
-    beforeEach(() => {
-        jest.spyOn(Auth, "currentSession").mockImplementation(async () => ({
-            idToken: {
-                jwtToken: "a-token",
-            },
-        }));
-        jest.spyOn(Auth, "federatedSignIn").mockImplementation(
-            async () => true
-        );
-        jest.spyOn(
-            FeatureToggleProvider,
-            "useFeatureToggle"
-        ).mockImplementation(() => true);
-        useLocation.mockImplementation(() => ({}));
-    });
-
-    it("does not render children when user IS NOT authenticated", () => {
-        render(
-            <Authenticator>
-                <Authenticator.Protected>
-                    <div>this-should-NOT-be-rendered</div>
-                </Authenticator.Protected>
-            </Authenticator>
-        );
-        expect(
-            screen.queryByText("this-should-NOT-be-rendered")
-        ).not.toBeInTheDocument();
-    });
-
-    it("renders children when user IS authenticated", async () => {
-        render(
-            <Authenticator>
-                <Authenticator.Protected>
-                    <div>this-should-be-rendered</div>
-                </Authenticator.Protected>
-            </Authenticator>
-        );
-        Hub.dispatch("auth", {
-            event: "signIn",
-            data: {},
+        beforeAll(() => {
+            defaultAuthConfig = config.Auth;
+            config.Auth.oauth = {
+                domain: "doc-store-user-pool.auth.eu-west-2.amazoncognito.com",
+                scope: ["openid"],
+                redirectSignIn: "http://localhost:3000",
+                redirectSignOut: "",
+                responseType: "code",
+            };
         });
-        await waitFor(() => {
+
+        afterAll(() => {
+            config.Auth = defaultAuthConfig;
+            jest.clearAllMocks();
+        });
+
+        beforeEach(() => {
+            jest.spyOn(Auth, "currentSession").mockImplementation(async () => ({
+                idToken: {
+                    jwtToken: "a-token",
+                },
+            }));
+            jest.spyOn(Auth, "federatedSignIn").mockImplementation(
+                async () => true
+            );
+            jest.spyOn(
+                FeatureToggleProvider,
+                "useFeatureToggle"
+            ).mockImplementation(
+                (toggleName) =>
+                    toggleName === "CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED"
+            );
+            useLocation.mockImplementation(() => ({}));
+        });
+
+        it("does not render children when user IS NOT authenticated", () => {
+            render(
+                <Authenticator>
+                    <Authenticator.Protected>
+                        <div>this-should-NOT-be-rendered</div>
+                    </Authenticator.Protected>
+                </Authenticator>
+            );
             expect(
-                screen.queryByText("this-should-be-rendered")
-            ).toBeInTheDocument();
+                screen.queryByText("this-should-NOT-be-rendered")
+            ).not.toBeInTheDocument();
         });
-    });
 
-    it("displays an error summary when authentication fails", async () => {
-        render(
-            <Authenticator>
-                <Authenticator.Errors />
-            </Authenticator>
-        );
-        Hub.dispatch("auth", {
-            event: "signIn_failure",
-            data: {
-                error_description:
-                    "The access token provided is expired, revoked, malformed, or invalid for other reasons.",
-                error: "invalid_token",
-            },
+        it("renders children when user IS authenticated", async () => {
+            render(
+                <Authenticator>
+                    <Authenticator.Protected>
+                        <div>this-should-be-rendered</div>
+                    </Authenticator.Protected>
+                </Authenticator>
+            );
+            Hub.dispatch("auth", {
+                event: "signIn",
+                data: {},
+            });
+            await waitFor(() => {
+                expect(
+                    screen.queryByText("this-should-be-rendered")
+                ).toBeInTheDocument();
+            });
         });
-        await waitFor(() => {
-            expect(
-                screen.queryByText(
-                    "The access token provided is expired, revoked, malformed, or invalid for other reasons."
-                )
-            ).toBeInTheDocument();
-        });
-    });
 
-    it("display an error summary when authentication redirects with an error", async () => {
-        useLocation.mockImplementation(() => ({
-            search: "?error_description=The%20access%20token%20provided%20is%20expired%2C%20revoked%2C%20malformed%2C%20or%20invalid%20for%20other%20reasons.",
-        }));
-        render(
-            <Authenticator>
-                <Authenticator.Errors />
-            </Authenticator>
-        );
-        await waitFor(() => {
-            expect(
-                screen.queryByText(
-                    "The access token provided is expired, revoked, malformed, or invalid for other reasons."
-                )
-            ).toBeInTheDocument();
+        it("displays an error summary when authentication fails", async () => {
+            render(
+                <Authenticator>
+                    <Authenticator.Errors />
+                </Authenticator>
+            );
+            Hub.dispatch("auth", {
+                event: "signIn_failure",
+                data: {
+                    error_description:
+                        "The access token provided is expired, revoked, malformed, or invalid for other reasons.",
+                    error: "invalid_token",
+                },
+            });
+            await waitFor(() => {
+                expect(
+                    screen.queryByText(
+                        "The access token provided is expired, revoked, malformed, or invalid for other reasons."
+                    )
+                ).toBeInTheDocument();
+            });
+        });
+
+        it("display an error summary when authentication redirects with an error", async () => {
+            useLocation.mockImplementation(() => ({
+                search: "?error_description=The%20access%20token%20provided%20is%20expired%2C%20revoked%2C%20malformed%2C%20or%20invalid%20for%20other%20reasons.",
+            }));
+            render(
+                <Authenticator>
+                    <Authenticator.Errors />
+                </Authenticator>
+            );
+            await waitFor(() => {
+                expect(
+                    screen.queryByText(
+                        "The access token provided is expired, revoked, malformed, or invalid for other reasons."
+                    )
+                ).toBeInTheDocument();
+            });
         });
     });
 });
