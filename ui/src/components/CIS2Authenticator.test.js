cc58cc6f9dea7f43721d78c758c2c2c2e846a74b
Caius Eugene <caius.eugene1@nhs.net>
2022-08-11 09:04:03 +0100

    MR-6: Removed screen.debug

--- ui/src/components/CIS2Authenticator.test.js
+++ ui/src/components/CIS2Authenticator.test.js
@@ -1,17 +1,19 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import { Auth, Hub } from "aws-amplify";
-import React from "react";
-import { useLocation } from "react-router";
-
+import {render, screen, waitFor} from "@testing-library/react";
+import CIS2Authenticator from "./CIS2Authenticator";
+import {Auth, Hub} from "aws-amplify";
 import config from "../config";
 import * as FeatureToggleProvider from "../providers/FeatureToggleProvider";
-import Authenticator from "./Authenticator";
 
 jest.mock("react-router", () => ({
-    useLocation: jest.fn(),
+    useLocation: jest.fn()
 }));
 
+import {useLocation} from "react-router";
+import Authenticator from "./Authenticator";
+import React from "react";
+
 describe("CIS2Authenticator", () => {
+
     let defaultAuthConfig;
 
     beforeAll(() => {
@@ -21,8 +23,8 @@ describe("CIS2Authenticator", () => {
             scope: ["openid"],
             redirectSignIn: "http://localhost:3000",
             redirectSignOut: "",
-            responseType: "code",
-        };
+            responseType: "code"
+        }
     });
 
     afterAll(() => {
@@ -31,90 +33,61 @@ describe("CIS2Authenticator", () => {
     });
 
     beforeEach(() => {
-        jest.spyOn(Auth, "currentSession").mockImplementation(async () => ({
-            idToken: {
-                jwtToken: "a-token",
-            },
-        }));
-        jest.spyOn(Auth, "federatedSignIn").mockImplementation(
-            async () => true
-        );
-        jest.spyOn(
-            FeatureToggleProvider,
-            "useFeatureToggle"
-        ).mockImplementation(() => true);
+        jest.spyOn(Auth, 'currentSession')
+            .mockImplementation(async () => ({
+                idToken: {
+                    jwtToken: "a-token"
+                }
+            }));
+        jest.spyOn(Auth, 'federatedSignIn')
+            .mockImplementation(async () => true);
+        jest.spyOn(FeatureToggleProvider, 'useFeatureToggle')
+            .mockImplementation(() => true);
         useLocation.mockImplementation(() => ({}));
-    });
+    })
 
     it("Given user IS NOT authenticated then no children should be rendered", () => {
-        render(
-            <Authenticator>
-                <Authenticator.Protected>
-                    <div>this-should-NOT-be-rendered</div>
-                </Authenticator.Protected>
-            </Authenticator>
-        );
-        expect(
-            screen.queryByText("this-should-NOT-be-rendered")
-        ).not.toBeInTheDocument();
-        screen.debug();
+        render(<Authenticator>
+            <Authenticator.Protected>
+                <div>this-should-NOT-be-rendered</div>
+            </Authenticator.Protected>
+        </Authenticator>);
+        expect(screen.queryByText("this-should-NOT-be-rendered")).not.toBeInTheDocument();
     });
 
     it("Given user IS authenticated then children should be rendered", async () => {
-        render(
-            <Authenticator>
-                <Authenticator.Protected>
-                    <div>this-should-be-rendered</div>
-                </Authenticator.Protected>
-            </Authenticator>
-        );
-        Hub.dispatch("auth", {
-            event: "signIn",
-            data: {},
+        render(<Authenticator>
+            <Authenticator.Protected>
+                <div>this-should-be-rendered</div>
+            </Authenticator.Protected>
+        </Authenticator>);
+        Hub.dispatch('auth', {
+            event: 'signIn', data: {}
         });
         await waitFor(() => {
-            expect(
-                screen.queryByText("this-should-be-rendered")
-            ).toBeInTheDocument();
+            expect(screen.queryByText("this-should-be-rendered")).toBeInTheDocument();
         });
     });
 
     it("Given authentication failed then display an error summery", async () => {
-        render(
-            <Authenticator>
-                <Authenticator.Errors />
-            </Authenticator>
-        );
-        Hub.dispatch("auth", {
-            event: "signIn_failure",
-            data: {
-                error_description:
-                    "The access token provided is expired, revoked, malformed, or invalid for other reasons.",
-                error: "invalid_token",
-            },
+        render(<Authenticator><Authenticator.Errors/></Authenticator>);
+        Hub.dispatch('auth', {
+            event: 'signIn_failure', data: {
+                "error_description": "The access token provided is expired, revoked, malformed, or invalid for other reasons.",
+                "error": "invalid_token"
+            }
         });
-        expect(
-            screen.queryByText(
-                "The access token provided is expired, revoked, malformed, or invalid for other reasons."
-            )
-        ).toBeInTheDocument();
+        expect(screen.queryByText("The access token provided is expired, revoked, malformed, or invalid for other reasons.")).toBeInTheDocument();
     });
 
     it("Given redirected with error then display an error summery", async () => {
         useLocation.mockImplementation(() => ({
-            search: "?error_description=The%20access%20token%20provided%20is%20expired%2C%20revoked%2C%20malformed%2C%20or%20invalid%20for%20other%20reasons.",
+            search: "?error_description=The%20access%20token%20provided%20is%20expired%2C%20revoked%2C%20malformed%2C%20or%20invalid%20for%20other%20reasons."
         }));
-        render(
-            <Authenticator>
-                <Authenticator.Errors />
-            </Authenticator>
-        );
+        render(<Authenticator><Authenticator.Errors/></Authenticator>);
         await waitFor(() => {
-            expect(
-                screen.queryByText(
-                    "The access token provided is expired, revoked, malformed, or invalid for other reasons."
-                )
-            ).toBeInTheDocument();
+            expect(screen.queryByText("The access token provided is expired, revoked, malformed, or invalid for other reasons.")).toBeInTheDocument();
         });
     });
+
 });
