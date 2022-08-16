17b4519c58feaf40304f1284ada597be5ba90744
Samradnyee <samradnyee.kolas1@nhs.net>
2022-08-16 13:52:35 +0100

    MR-15: Format file using prettier
    - RB/AD/MT/SK

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,31 +1,31 @@
 const config = {
-    Auth: {
-        region: "%region%",
-        userPoolId: "%pool-id%",
-        userPoolWebClientId: "%client-id%",
+  Auth: {
+    region: "%region%",
+    userPoolId: "%pool-id%",
+    userPoolWebClientId: "%client-id%",
+  },
+  API: {
+    endpoints: [
+      {
+        name: "doc-store-api",
+        endpoint: "%api-endpoint%",
+      },
+    ],
+  },
+  features: {
+    local: {
+      CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: true,
+      SHOW_METADATA_FIELDS_ON_UPLOAD_PAGE: true,
     },
-    API: {
-        endpoints: [
-            {
-                name: "doc-store-api",
-                endpoint: "%api-endpoint%",
-            },
-        ],
+    development: {
+      CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: false,
+      SHOW_METADATA_FIELDS_ON_UPLOAD_PAGE: true,
     },
-    features: {
-        local: {
-            CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: true,
-            SHOW_METADATA_FIELDS_ON_UPLOAD_PAGE: true,
-        },
-        development: {
-            CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: false,
-            SHOW_METADATA_FIELDS_ON_UPLOAD_PAGE: true,
-        },
-        production: {
-            CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: false,
-            SHOW_METADATA_FIELDS_ON_UPLOAD_PAGE: false,
-        },
+    production: {
+      CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: false,
+      SHOW_METADATA_FIELDS_ON_UPLOAD_PAGE: false,
     },
+  },
 };
 
 export default config;
