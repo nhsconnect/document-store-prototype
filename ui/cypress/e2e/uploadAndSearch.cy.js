6a240a13d97ae813c9f123ca28833d1dd0e1acec
Samradnyee <samradnyee.kolas1@nhs.net>
2022-08-23 11:03:47 +0100

    MR-24: E2E tests validate patient trace path before upload
    - introduces new branch in E2E test dependent on feature toggle
    - add cypress screenshots and videos folders to git ignore file
    
    - RB/SK

--- ui/cypress/e2e/uploadAndSearch.cy.js
+++ ui/cypress/e2e/uploadAndSearch.cy.js
@@ -1,43 +1,63 @@
+import config from "../../src/config";
+
 describe("upload and search transaction", () => {
-  it("allows the user to upload a document and search for a document", () => {
-    const nhsNumber = "1234567890";
-    const documentTitle = "Jane Doe - Patient Record";
-    cy.visit("/");
+    it("allows the user to upload a document and search for a document", () => {
+        const nhsNumber = "9000000009";
+        const documentTitle = "Jane Doe - Patient Record";
+        cy.visit("/");
 
-    // log into the website
-    cy.login(Cypress.env("cognito_username"), Cypress.env("cognito_password"));
+        // log into the website
+        cy.login(
+            Cypress.env("cognito_username"),
+            Cypress.env("cognito_password")
+        );
 
-    // navigate to the upload document page
-    cy.get('a[href="/upload"]').click();
-    cy.url().should("eq", Cypress.config("baseUrl") + "/upload");
+        if (
+            config.features[process.env.NODE_ENV].PDS_TRACE_FOR_UPLOAD_ENABLED
+        ) {
+            // navigate to the upload document page
+            cy.get('a[href="/upload/patient-trace"]').click();
+            cy.url().should(
+                "eq",
+                Cypress.config("baseUrl") + "/upload/patient-trace"
+            );
+            cy.get('input[name="nhsNumber"]').type(nhsNumber);
+            cy.contains("Search").click();
+            cy.contains("Next", { timeout: 30000 }).click();
+            cy.url().should("eq", Cypress.config("baseUrl") + "/upload/submit");
+        } else {
+            // navigate to the upload document page
+            cy.get('a[href="/upload"]').click();
+            cy.url().should("eq", Cypress.config("baseUrl") + "/upload");
+            cy.get('input[name="nhsNumber"]').type(nhsNumber);
+        }
 
-    // fill out fields on upload document page
-    cy.get('input[name="nhsNumber"]').type(nhsNumber);
-    cy.get('input[name="documentTitle"]').type(documentTitle);
-    cy.get('input[name="document"]').selectFile(
-      "cypress/fixtures/test_patient_record.pdf"
-    );
-    cy.get('button[type="submit"]').click();
+        // fill out fields on upload document page
+        cy.get('input[name="documentTitle"]').type(documentTitle);
+        cy.get('input[name="document"]').selectFile(
+            "cypress/fixtures/test_patient_record.pdf"
+        );
+        cy.get('button[type="submit"]').click();
 
-    // wait for lambda to return a success message
-    cy.get('p[data-testid="success-message"]', { timeout: 30000 }).should(
-      "be.visible"
-    );
+        // wait for lambda to return a success message
+        cy.get('p[data-testid="success-message"]', { timeout: 30000 }).should(
+            "be.visible"
+        );
 
-    // return to the home page
-    cy.visit("/");
+        // return to the home page
+        cy.visit("/");
 
-    // navigate to the view document page
-    cy.get('a[href="/search"]').click();
-    cy.url().should("eq", Cypress.config("baseUrl") + "/search");
+        // navigate to the view document page
+        cy.get('a[href="/search"]').click();
+        cy.url().should("eq", Cypress.config("baseUrl") + "/search");
 
-    //search for document
-    cy.get('input[name="nhsNumber"]').type(nhsNumber);
-    cy.get('button[type="submit"]').click();
+        //search for document
+        cy.get('input[name="nhsNumber"]').type(nhsNumber);
+        cy.get('button[type="submit"]').click();
 
-    // wait for lambda to return results
-    cy.get('a[data-testid="document-title"]', { timeout: 30000 })
-      .first()
-      .should("have.text", documentTitle);
-  });
+        // wait for lambda to return results
+        cy.get('a[data-testid="document-title"]', { timeout: 30000 })
+            .first()
+            .should("have.text", documentTitle);
+    });
 });
