2a9d1585f75d7e1ca11112d8a111d73ca8a65ef7
Caius Eugene <caius.eugene1@nhs.net>
2022-08-23 13:45:24 +0100

    Terraform apply CMDline args ordering

--- gocd/tasks.sh
+++ gocd/tasks.sh
@@ -107,7 +107,7 @@ deploy)
   cd terraform
   assume_ci_role
   terraform init
-  terraform apply tfplan -var-file="../terraform/development.tfvars"
+  terraform apply -var-file="../terraform/development.tfvars" tfplan
   ;;
 deploy-ui)
   get_terraform_output terraform_output.json
