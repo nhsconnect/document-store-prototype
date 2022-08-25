4a6b7f7f2a25da5c27e9524838cfed257d06e388
Caius Eugene <caius.eugene1@nhs.net>
2022-08-25 12:27:17 +0100

    Aligned start page to NHSUK design system

--- ui/src/pages/StartPage.jsx
+++ ui/src/pages/StartPage.jsx
@@ -4,13 +4,13 @@ import { ButtonLink } from "nhsuk-react-components";
 export default () => {
     return (
         <>
-            <h3>Document Store</h3>
+            <h1>Document Store</h1>
             <p>Use this service to:</p>
             <ul>
-                <li>upload patient digital records</li>
-                <li>download and view patient digital records</li>
+                <li>Upload patient digital records</li>
+                <li>Download and view patient digital records</li>
             </ul>
-            <h4>Before you start</h4>
+            <h2 class="nhsuk-heading-m">Before you start</h2>
             <p>
                 You can only use this service if you have a valid CIS2 account
             </p>
