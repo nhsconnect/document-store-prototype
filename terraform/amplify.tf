df108976de234c85b6c5ee88cf3a4bfe16e2e445
Caius Eugene <caius.eugene1@nhs.net>
2022-08-25 09:17:45 +0100

    Redirect rule for SPA to prevent access denied when directly navigating to routes

--- terraform/amplify.tf
+++ terraform/amplify.tf
@@ -1,6 +1,14 @@
 resource "aws_amplify_app" "doc-store-ui" {
   name = "DocStoreUi"
 
+  # Redirects for Single Page Web Apps (SPA)
+  # https://docs.aws.amazon.com/amplify/latest/userguide/redirects.html#redirects-for-single-page-web-apps-spa
+  custom_rule {
+    source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json)$)([^.]+$)/>"
+    status = "200"
+    target = "/index.html"
+  }
+
   count = var.cloud_only_service_instances
 }
 
