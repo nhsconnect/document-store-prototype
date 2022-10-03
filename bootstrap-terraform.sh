811b5eb873d5f652d1143bf3b8c8fc32e21aaf90
Shruthi Chandekar <shruthi.chandekar1@nhs.net>
2022-10-03 12:48:57 +0100

    [PRMT-2840] 1. Added pre-prod pipeline to the project. 2. Renamed pipeline to paperless-record-service. 3. Updated assume-ci-role tasks to work with multiple environments. 4. Ensure npm install uses the lock file in ci environments.
    Co-authored-by: ryanbrown-nhs <ryan.brown15@nhs.net>

--- bootstrap-terraform.sh
+++ bootstrap-terraform.sh
@@ -2,7 +2,7 @@
 
 ENVIRONMENT=${1:-local}
 REGION=eu-west-2
-ENDPOINT=https://amazonaws.com
+#ENDPOINT=https://${REGION}.amazonaws.com
 
 if [ $ENVIRONMENT == "local" ]; then
     ENDPOINT=http://localhost:4566
@@ -15,32 +15,34 @@ function createS3Bucket {
     --bucket $BUCKET_NAME\
     --acl private \
     --create-bucket-configuration '{ "LocationConstraint": "eu-west-2" }' \
-    --endpoint-url=$ENDPOINT
+    ${ENDPOINT:+--endpoint-url=$ENDPOINT}
+
 
   aws s3api put-bucket-encryption \
     --bucket $BUCKET_NAME \
     --server-side-encryption-configuration '{"Rules": [{"ApplyServerSideEncryptionByDefault": {"SSEAlgorithm": "AES256"}}]}' \
-    --endpoint-url=$ENDPOINT
+    ${ENDPOINT:+--endpoint-url=$ENDPOINT}
 
   aws s3api put-public-access-block\
     --bucket $BUCKET_NAME \
     --public-access-block-configuration BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true \
-    --endpoint-url=$ENDPOINT
+    ${ENDPOINT:+--endpoint-url=$ENDPOINT}
+
 
   aws s3api put-bucket-tagging \
     --bucket $BUCKET_NAME \
     --tagging "TagSet=[{Key=createdBy,Value=prm-repo-team},{Key=name,Value=PRS terraform state for ${ENVIRONMENT} environment}]" \
-    --endpoint-url=$ENDPOINT
+    ${ENDPOINT:+--endpoint-url=$ENDPOINT}
 
   aws s3api put-bucket-lifecycle \
     --bucket $BUCKET_NAME \
     --lifecycle-configuration '{"Rules": [{"ID": "Expiration lifecycle rule","Prefix": "","Status": "Enabled","NoncurrentVersionExpiration": {"NoncurrentDays": 360}}]}' \
-    --endpoint-url=$ENDPOINT
+    ${ENDPOINT:+--endpoint-url=$ENDPOINT}
 
   aws s3api put-bucket-versioning \
     --bucket $BUCKET_NAME \
     --versioning-configuration Status=Enabled \
-    --endpoint-url=$ENDPOINT
+    ${ENDPOINT:+--endpoint-url=$ENDPOINT}
 }
 
 function createDynamoDBTable {
@@ -53,28 +55,28 @@ function createDynamoDBTable {
       --billing-mode PROVISIONED \
       --provisioned-throughput ReadCapacityUnits=2,WriteCapacityUnits=2 \
       --tags Key=createdBy,Value=prm-repo-team Key=name,Value="PRS terraform state locking table for ${ENVIRONMENT} environment" \
-      --endpoint-url=$ENDPOINT
+      ${ENDPOINT:+--endpoint-url=$ENDPOINT}
 }
 
-function createDynamoDBTableForIamRoles {
-  TABLE_NAME=prs-${ENVIRONMENT}-iam-terraform-state-locking
-
-  aws dynamodb create-table \
-      --table-name $TABLE_NAME \
-      --attribute-definitions AttributeName=LockID,AttributeType=S \
-      --key-schema AttributeName=LockID,KeyType=HASH \
-      --billing-mode PROVISIONED \
-      --provisioned-throughput ReadCapacityUnits=2,WriteCapacityUnits=2 \
-      --tags Key=createdBy,Value=prm-repo-team Key=name,Value="PRS terraform state locking table for ${ENVIRONMENT} environment" \
-      --endpoint-url=$ENDPOINT
-}
+#function createDynamoDBTableForIamRoles {
+#  TABLE_NAME=prs-${ENVIRONMENT}-iam-terraform-state-locking
+#
+#  aws dynamodb create-table \
+#      --table-name $TABLE_NAME \
+#      --attribute-definitions AttributeName=LockID,AttributeType=S \
+#      --key-schema AttributeName=LockID,KeyType=HASH \
+#      --billing-mode PROVISIONED \
+#      --provisioned-throughput ReadCapacityUnits=2,WriteCapacityUnits=2 \
+#      --tags Key=createdBy,Value=prm-repo-team Key=name,Value="PRS terraform state locking table for ${ENVIRONMENT} environment" \
+#      --endpoint-url=$ENDPOINT
+#}
 
 read -r -p "Are you sure you want to bootstrap terraform for the ${ENVIRONMENT} environment at ${ENDPOINT}? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
     createS3Bucket
     createDynamoDBTable
-    createDynamoDBTableForIamRoles
+#    createDynamoDBTableForIamRoles
 else
     exit
 fi
