2c290531b07875bd8e633ce6e1003138d280e1ab
Anu Doddala <anu.doddala1@nhs.net>
2022-10-03 14:48:20 +0100

    [PRMT-2781] Implemented the redirect to StartPage when clicks on Log Out button
    Co-authored-by: Romane Castera <romane.castera1@nhs.net>

--- ui/src/components/Header/index.js
+++ ui/src/components/Header/index.js
@@ -1,20 +1,28 @@
 import React from "react";
-import { ButtonLink} from "nhsuk-react-components";
+import {ButtonLink} from "nhsuk-react-components";
+import {useNavigate} from "react-router";
 
-import { Header } from "nhsuk-react-components";
+import {Header} from "nhsuk-react-components";
 
-export const HeaderContainer = () => (
-  <Header transactional>
-    <Header.Container>
-      <Header.Logo href="/" />
-      <Header.ServiceName data-testid="header-service-name" href="/">
-        Document Store
-      </Header.ServiceName>
-      <Header.Content>
-        <ButtonLink>Logout</ButtonLink>
-      </Header.Content>
-    </Header.Container>
-  </Header>
-);
+
+export const HeaderContainer = () => {
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    navigate("/");
+  };
+  return (
+    <Header transactional>
+      <Header.Container>
+        <Header.Logo href="/"/>
+        <Header.ServiceName data-testid="header-service-name" href="/">
+          Document Store
+        </Header.ServiceName>
+        <Header.Content>
+          <ButtonLink onClick={handleLogOut}>Log Out</ButtonLink>
+        </Header.Content>
+      </Header.Container>
+    </Header>
+  );
+}
 
 export default HeaderContainer;
--- ui/src/components/Header/index.test.js
+++ ui/src/components/Header/index.test.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Header from ".";
+import {MemoryRouter, useNavigate} from "react-router";
+import userEvent from "@testing-library/user-event";
+
+const mockNavigate = jest.fn();
+ jest.mock("react-router",() => ({
+ ...jest.requireActual('react-router'),
+     useNavigate: ()=> mockNavigate
+ }));
 
 describe("Header component", () => {
   it("displays site service name", () => {
@@ -8,9 +16,17 @@ describe("Header component", () => {
 
     expect(screen.queryByText("Document Store")).toBeTruthy();
   });
-  it("displays a logout button", () => {
+  it("displays a logout button when user logged in", () => {
     render(<Header />);
 
-    expect(screen.queryByText("Logout")).toBeTruthy();
+    expect(screen.queryByText("Log Out")).toBeTruthy();
+  });
+  it("redirect to StartPage when user clicks logout button", () => {
+    render(
+      <MemoryRouter >
+        <Header />
+      </MemoryRouter>);
+    userEvent.click(screen.getByRole("button", { name: "Log Out" }));
+    expect(mockNavigate).toHaveBeenCalledWith("/");
   });
 });
