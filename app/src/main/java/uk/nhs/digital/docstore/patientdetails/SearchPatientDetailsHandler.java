bc9fc576296fc5f2b34aad989dc4fb0d6d1b21f1
Dan Moore <dan.moore1@nhs.net>
2022-10-27 18:10:02 +0100

    [PRMT-2799] Tidy up search handler internally

--- app/src/main/java/uk/nhs/digital/docstore/patientdetails/SearchPatientDetailsHandler.java
+++ app/src/main/java/uk/nhs/digital/docstore/patientdetails/SearchPatientDetailsHandler.java
@@ -16,87 +16,84 @@ import uk.nhs.digital.docstore.config.Tracer;
 import java.util.List;
 import java.util.Map;
 
-
-public class SearchPatientDetailsHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>  {
+public class SearchPatientDetailsHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private static final Logger logger = LoggerFactory.getLogger(SearchPatientDetailsHandler.class);
 
     private final FhirContext fhirContext;
     private final ApiConfig apiConfig;
     private final PdsAdaptorClient pdsAdaptorClient;
+    private final ErrorResponseGenerator errorResponseGenerator = new ErrorResponseGenerator();
 
-    public SearchPatientDetailsHandler()
-    {
+    public SearchPatientDetailsHandler() {
         this(new ApiConfig(), new PdsAdaptorClient());
     }
 
-    public SearchPatientDetailsHandler(ApiConfig apiConfig, PdsAdaptorClient pdsAdaptorClient)
-    {
+    public SearchPatientDetailsHandler(ApiConfig apiConfig, PdsAdaptorClient pdsAdaptorClient) {
         this(FhirContext.forR4(), apiConfig, pdsAdaptorClient);
     }
 
-    public SearchPatientDetailsHandler(FhirContext fhirContext,
-                                       ApiConfig apiConfig,
-                                       PdsAdaptorClient pdsAdaptorClient)
-    {
+    public SearchPatientDetailsHandler(FhirContext fhirContext, ApiConfig apiConfig, PdsAdaptorClient pdsAdaptorClient) {
         this.fhirContext = fhirContext;
         this.apiConfig = apiConfig;
         this.pdsAdaptorClient = pdsAdaptorClient;
     }
 
-
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
-
         Tracer.setMDCContext(context);
 
         logger.debug("API Gateway event received - processing starts");
-        var jsonParser = fhirContext.newJsonParser();
-        String nhsNumber = "";
-
-        Map<String, String> searchParameters = (requestEvent.getQueryStringParameters() == null
-                ? Map.of()
-                : requestEvent.getQueryStringParameters());
+        var searchParameters = queryParametersFrom(requestEvent);
 
         try {
-            NHSNumberSearchParameterForm nhsNumberSearchParameterForm = new NHSNumberSearchParameterForm(searchParameters);
-            nhsNumber = nhsNumberSearchParameterForm.getNhsNumber();
-        } catch (Exception e) {
-            ErrorResponseGenerator errorResponseGenerator = new ErrorResponseGenerator();
-            return errorResponseGenerator.errorResponse(e, jsonParser);
-        }
+            var parameterForm = new NHSNumberSearchParameterForm(searchParameters);
 
-        try {
-            var patientDetails = pdsAdaptorClient.fetchPatientDetails(nhsNumber);
+            var patientDetails = pdsAdaptorClient.fetchPatientDetails(parameterForm.getNhsNumber());
             if (patientDetails == null) {
                 return emptyBundleResponse();
             }
-            logger.debug("Generating response");
 
-            BundleMapper bundleMapper = new BundleMapper();
-            Bundle bundle = bundleMapper.toBundle(List.of(patientDetails));
+            logger.debug("Generating response body");
+            var body = toJson(fhirBundleOf(patientDetails));
+
             logger.debug("Processing finished - about to return the response");
-            return new APIGatewayProxyResponseEvent()
-                    .withStatusCode(200)
-                    .withHeaders(Map.of(
-                            "Content-Type", "application/fhir+json",
-                            "Access-Control-Allow-Origin", apiConfig.getAmplifyBaseUrl(),
-                            "Access-Control-Allow-Methods", "GET"))
-                    .withBody(jsonParser.encodeResourceToString(bundle));
-        } catch (Exception e) {
-            ErrorResponseGenerator errorResponseGenerator = new ErrorResponseGenerator();
-            return errorResponseGenerator.errorResponse(e, jsonParser);
+            return fhirResponse(200).withBody(body);
+        }
+        catch (Exception e) {
+            return errorResponseGenerator.errorResponse(e, fhirContext.newJsonParser());
         }
     }
 
-    private static APIGatewayProxyResponseEvent emptyBundleResponse() {
+    private APIGatewayProxyResponseEvent emptyBundleResponse() {
+        return fhirResponse(200).withBody("{\n" +
+                "  \"resourceType\": \"Bundle\",\n" +
+                "  \"type\": \"searchset\",\n" +
+                "  \"total\": 0\n" +
+                "}");
+    }
+
+    private static Map<String, String> queryParametersFrom(APIGatewayProxyRequestEvent requestEvent) {
+        return requestEvent.getQueryStringParameters() == null
+                ? Map.of()
+                : requestEvent.getQueryStringParameters();
+    }
+
+    private APIGatewayProxyResponseEvent fhirResponse(int statusCode) {
         return new APIGatewayProxyResponseEvent()
-                .withStatusCode(200)
+                .withStatusCode(statusCode)
                 .withHeaders(Map.of(
-                        "Content-Type", "application/fhir+json"))
-                .withBody("{\n" +
-                        "  \"resourceType\": \"Bundle\",\n" +
-                        "  \"type\": \"searchset\",\n" +
-                        "  \"total\": 0\n" +
-                        "}");
+                        "Content-Type", "application/fhir+json",
+                        "Access-Control-Allow-Origin", apiConfig.getAmplifyBaseUrl(),
+                        "Access-Control-Allow-Methods", "GET"));
+    }
+
+    private static Bundle fhirBundleOf(PatientDetails patientDetails) {
+        BundleMapper bundleMapper = new BundleMapper();
+        return bundleMapper.toBundle(List.of(patientDetails));
+    }
+
+    private String toJson(Bundle payload) {
+        var jsonParser = fhirContext.newJsonParser();
+        return jsonParser.encodeResourceToString(payload);
     }
 }
--- ui/src/config.js
+++ ui/src/config.js
@@ -1,13 +1,13 @@
 const config = {
     Auth: {
-        region: "%region%",
-        userPoolId: "%pool-id%",
-        userPoolWebClientId: "%client-id%",
-        providerId: "cis2devoidc",
+        region: "eu-west-2",
+        userPoolId: "eu-west-2_Y8etyk9V6",
+        userPoolWebClientId: "7mm2u6re8jsptrgallf74g2c6c",
+        providerId: "COGNITO",
         oauth: {
-            domain: "%cognito-domain%",
+            domain: "doc-store-user-pool.auth.eu-west-2.amazoncognito.com",
             scope: ["openid"],
-            redirectSignIn: "%cognito-redirect-signin%",
+            redirectSignIn: "https://main.d1p55zjnm05qd2.amplifyapp.com/cis2-auth-callback",
             redirectSignOut: "%cognito-redirect-signout%",
             responseType: "token",
         },
@@ -16,19 +16,19 @@ const config = {
         endpoints: [
             {
                 name: "doc-store-api",
-                endpoint: "%api-endpoint%",
+                endpoint: process.env.REACT_APP_DOCUMENT_STORE_BASE_URI
             },
         ],
     },
     features: {
         local: {
-            CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: true,
-        },
-        dev: {
-            CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: true,
-        },
-        "pre-prod": {
             CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: false,
+        },
+        development: {
+            CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: false,
+        },
+        production: {
+            CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: true,
         },
     },
 };
