ebe6e5fd2ced539b3081b069c10cc877ed044998
Ryan Brown <ryan.brown15@nhs.net>
2022-12-01 17:30:49 +0000

    [PRMT-2801] Remove width limit on documents input as no longer required now input state is updated and it looked bad on some browsers

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,14 +1,14 @@
 const config = {
     Auth: {
-        region: "%region%",
-        userPoolId: "%pool-id%",
-        userPoolWebClientId: "%client-id%",
-        providerId: "cis2devoidc",
+        region: "eu-west-2",
+        userPoolId: "eu-west-2_Y8etyk9V6",
+        userPoolWebClientId: "7mm2u6re8jsptrgallf74g2c6c",
+        providerId: "COGNITO",
         oauth: {
-            domain: "%cognito-domain%",
+            domain: "doc-store-user-pool.auth.eu-west-2.amazoncognito.com",
             scope: ["openid"],
-            redirectSignIn: "%cognito-redirect-signin%",
-            redirectSignOut: "%cognito-redirect-signout%",
+            redirectSignIn: "http://localhost:3000/cis2-auth-callback",
+            redirectSignOut: "http://localhost:3000",
             responseType: "token",
         },
     },
@@ -16,13 +16,13 @@ const config = {
         endpoints: [
             {
                 name: "doc-store-api",
-                endpoint: "%api-endpoint%",
+                endpoint: "http://localhost:3000/restapis/1d3tbej036/test/_user_request_",
             },
         ],
     },
     features: {
         local: {
-            CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: true,
+            CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: false,
         },
         dev: {
             CIS2_FEDERATED_IDENTITY_PROVIDER_ENABLED: true,
