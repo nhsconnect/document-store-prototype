1057c5e1f21fc561176928dfa73de2b55a49b248
Nicholas Hill <nick.hill3@nhs.net>
2022-12-08 16:04:56 +0000

    [PRMT-2813] Add cypress-testing-library and fix node detachment issues

--- ui/cypress/e2e/uploadAndDownload.cy.js
+++ ui/cypress/e2e/uploadAndDownload.cy.js
@@ -11,48 +11,49 @@ describe('upload and download', () => {
 
         cy.visit('/');
         cy.title().should('eq', 'Document Store');
-
-        cy.contains('Start now').click();
-        cy.cis2Login(username, password);
+        cy.findByRole('button', {name: 'Start now'}).click();
 
         cy.url().should('eq', baseUrl + '/home');
+        cy.findByPlaceholderText('User Name').type(username);
+        cy.findByPlaceholderText('Password').type(password);
+        cy.findByRole('button', {name: 'Continue'}).click();
 
-        cy.get('#upload').check();
-        cy.get('form').submit();
+        cy.url().should('eq', baseUrl + '/home');
+        // Todo: Replace cy.wait() with a guard to wait until React has completed it's initial render(s)
+        cy.wait(500);
+        cy.findByRole('radio', {name: /Upload/}).check()
+        cy.findByRole('button', {name: 'Continue'}).click();
 
         cy.url().should('eq', baseUrl + '/upload/patient-trace');
-        cy.get('input').type(nhsNumber);
-        cy.get('form').submit();
-        cy.contains('Next').click();
+        cy.findByRole('textbox', {name: 'NHS number'}).type(nhsNumber);
+        cy.findByRole('button', {name: 'Search'}).click();
+        cy.findByRole('button', {name: 'Next'}).click();
 
         cy.url().should('eq', baseUrl + '/upload/submit');
         cy.get('input[type=file]').selectFile(uploadedFilePathNames);
-        cy.get('form').submit();
-        cy.get('table').contains('Your documents are uploading');
-        cy.contains('Finish').click();
+        cy.findByRole('button', {name: 'Upload'}).click();
+        cy.findByRole('button', {name: 'Finish'}).click();
 
         cy.url().should('eq', Cypress.config('baseUrl') + '/home');
-
         cy.visit('/');
-        cy.contains('Start now').click();
+        cy.findByRole('button', {name: 'Start now'}).click();
 
-        cy.get('#download').then(($el) => {
-            assert.isTrue(Cypress.dom.isAttached($el))
-        })
-
-        cy.get('#download').check();
-        cy.get('form').submit();
+        cy.url().should('eq', baseUrl + '/home');
+        // Todo: Replace cy.wait() with a guard to wait until React has completed it's initial render(s)
+        cy.wait(500);
+        cy.findByRole('radio', {name: /Download/}).check();
+        cy.findByRole('button', {name: 'Continue'}).click();
 
         cy.url().should('eq', baseUrl + '/search/patient-trace');
-        cy.get('input').type(nhsNumber);
-        cy.get('form').submit();
-        cy.contains('Next').click();
+        cy.findByRole('textbox', {name: 'NHS number'}).type(nhsNumber);
+        cy.findByRole('button', {name: 'Search'}).click();
+        cy.findByRole('button', {name: 'Next'}).click();
 
         cy.url().should('eq', baseUrl + '/search/results');
         cy.readFile(downloadedDocumentPath).should('not.exist');
-        cy.contains('Download All').click();
+        cy.findByRole('button', {name: 'Download All'}).click();
         cy.readFile(downloadedDocumentPath).should('exist');
-        cy.contains('Log Out').click();
+        cy.findByRole('button', {name: 'Log Out'}).click();
 
         cy.url().should('eq', baseUrl + '/');
     });
--- ui/cypress/support/commands.js
+++ ui/cypress/support/commands.js
@@ -1,45 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add("login", (username, password) => {
-    cy.get("#username").type(username);
-    cy.get("#password").type(password);
-    cy.get('[data-test="sign-in-sign-in-button"]').last().click();
-    cy.url().should("eq", Cypress.config("baseUrl") + "/");
-});
-
-Cypress.Commands.add("cis2Login", (username, password) => {
-    cy.get('[placeholder="User Name"]').type(username);
-    cy.get('[placeholder="Password"]').type(password);
-    cy.contains("Continue").click();
-    cy.url().should(
-        "eq",
-        Cypress.config("baseUrl") + "/home"
-    );
-});
-
 Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
     if (Cypress.env('basic_auth_username')) {
         originalFn(url, {
@@ -48,8 +6,8 @@ Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
                 username: Cypress.env('basic_auth_username'),
                 password: Cypress.env('basic_auth_password')
             }
-        })
+        });
     } else {
-        originalFn(url, options)
+        originalFn(url, options);
     }
-})
+});
