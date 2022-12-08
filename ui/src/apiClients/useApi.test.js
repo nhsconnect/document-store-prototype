8b230998b8fdcf8b11e9cfada01416125fb57565
Nicholas Hill <nick.hill3@nhs.net>
2022-12-08 11:21:29 +0000

    [PRMT-2813] Fix jest mock for apiClient

--- ui/src/apiClients/useApi.test.js
+++ ui/src/apiClients/useApi.test.js
@@ -1,21 +1,21 @@
-import { renderHook } from "@testing-library/react-hooks";
-import ApiClient from "./apiClient";
-import useApi from "./useApi";
-import { API } from "aws-amplify"
-import { useAuth } from "react-oidc-context"
+import {renderHook} from '@testing-library/react-hooks';
+import ApiClient from './apiClient';
+import useApi from './useApi';
+import {API} from 'aws-amplify'
+import {useAuth} from 'react-oidc-context'
 
-jest.mock("./ApiClient")
-jest.mock("aws-amplify")
-jest.mock("react-oidc-context")
+jest.mock('./apiClient');
+jest.mock('aws-amplify');
+jest.mock('react-oidc-context');
 
-describe("The useApi hook", () => {
-    it("returns an instance of the API client", () => {
-        const user = "foo"
-        useAuth.mockImplementation(() => ({user}))
+describe('useApi', () => {
+    it('returns an instance of the API client', () => {
+        const user = 'foo';
+        useAuth.mockImplementation(() => ({user}));
 
-        const { result } = renderHook(() => useApi())
+        const {result} = renderHook(() => useApi());
         expect(result.current).toBeInstanceOf(ApiClient);
 
-        expect(ApiClient).toHaveBeenCalledWith(API, user)
-    })
-})
+        expect(ApiClient).toHaveBeenCalledWith(API, user);
+    });
+});
