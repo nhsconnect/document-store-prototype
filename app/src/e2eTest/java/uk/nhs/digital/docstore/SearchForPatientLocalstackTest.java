252c7a7913cae2a5c06138be6205d180fa3d51ab
romanenhs <romane.castera1@nhs.net>
2022-12-12 12:27:43 +0000

    [PRMT-2933] Removed wiremock from localstack test and fixed tests when PDS_FHIR_IS_STUBBED is true
    Co-authored-by: Mirla Chucre <mirla.chucre1@nhs.net>

--- app/src/e2eTest/java/uk/nhs/digital/docstore/SearchForPatientLocalstackTest.java
+++ app/src/e2eTest/java/uk/nhs/digital/docstore/SearchForPatientLocalstackTest.java
@@ -1,8 +1,9 @@
 package uk.nhs.digital.docstore;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,32 +11,21 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static java.net.http.HttpClient.newHttpClient;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.nhs.digital.docstore.helpers.BaseUriHelper.getBaseUri;
-import static uk.nhs.digital.docstore.helpers.BaseUriHelper.resolveContainerHost;
 
 
 public class SearchForPatientLocalstackTest {
 
-    @BeforeAll
-    static public void pointWiremockToPdsFhirContainer() {
-        WireMock.configureFor(resolveContainerHost("pds-fhir"), 8080);
-    }
-
-    //Will fail while pds adaptor is stubbed
     @Test
     void returnsSuccessResponse() throws IOException, InterruptedException {
-        var patientData = getContentFromResource("search-patient-details/pds-fhir-responses/complete-patient-details-response.json");
-
-        stubFor(get(urlEqualTo("/Patient/9000000009"))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(patientData)));
+        var expectedPatientDetailsResponse = (System.getProperty("PDS_FHIR_IS_STUBBED").equals("true") ||
+                System.getenv("PDS_FHIR_IS_STUBBED").equals("true"))
+                ? "search-patient-details/patient-details-stubbed-response.json"
+                : "search-patient-details/patient-details-response.json";
 
         var patientDetailsRequest = HttpRequest.newBuilder(getBaseUri().resolve("PatientDetails?subject:identifier=https://fhir.nhs.uk/Id/nhs-number%7C9000000009"))
                 .GET()
@@ -47,22 +37,17 @@ public class SearchForPatientLocalstackTest {
         assertThat(patientDetailsResponse.headers().firstValue("Content-Type")).contains("application/fhir+json");
         assertThatJson(patientDetailsResponse.body())
                 .whenIgnoringPaths("$.meta", "$.entry[*].resource.meta")
-                .isEqualTo(getContentFromResource("search-patient-details/patient-details-response.json"));
+                .isEqualTo(getContentFromResource(expectedPatientDetailsResponse));
     }
 
-    //Will fail while pds adaptor is stubbed as there is no patient not found case
+    @DisabledIfSystemProperty(named = "PDS_FHIR_IS_STUBBED", matches = "true")
+    @DisabledIfEnvironmentVariable(named = "PDS_FHIR_IS_STUBBED", matches = "true")
     @Test
     void returnsMissingPatientResponseWhenPatientNotFound() throws IOException, InterruptedException {
         var expectedPatientDetailsResponse = getContentFromResource("search-patient-details/missing-patient-response.json");
         var patientDetailsRequest = HttpRequest.newBuilder(getBaseUri().resolve("PatientDetails?subject:identifier=https://fhir.nhs.uk/Id/nhs-number%7C9111231130"))
                 .GET()
                 .build();
-
-        stubFor(get(urlEqualTo("/Patient/9111231130"))
-                .willReturn(aResponse()
-                        .withStatus(404)
-                        .withHeader("Content-Type", "application/json")
-                        .withBody("{}")));
 
         var patientDetailsResponse = newHttpClient().send(patientDetailsRequest, HttpResponse.BodyHandlers.ofString(UTF_8));
 
