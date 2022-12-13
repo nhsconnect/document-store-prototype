6fd3520a28130803ea2f6cd9514c8775e2c33a7f
Ryan Brown <ryan.brown15@nhs.net>
2022-12-13 15:13:50 +0000

    [PRMT-2805] Added simple test to pass in NHS number parameter to delete documents route
    Added base64encoding flag to API response builder as per APIGateway recommendations
    
    Co-authored-by: Anu Doddala <anu.doddala1@nhs.net>

--- app/src/e2eTest/java/uk/nhs/digital/docstore/DeleteDocumentReferenceE2eTest.java
+++ app/src/e2eTest/java/uk/nhs/digital/docstore/DeleteDocumentReferenceE2eTest.java
@@ -3,8 +3,10 @@ package uk.nhs.digital.docstore;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 import static java.net.http.HttpClient.newHttpClient;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,13 +15,16 @@ import static uk.nhs.digital.docstore.helpers.BaseUriHelper.getBaseUri;
 public class DeleteDocumentReferenceE2eTest {
 
     @Test
-    void shouldReturnResponseStatusAsSuccessful() throws IOException, InterruptedException  {
-        var deleteDocumentReferenceRequest = HttpRequest.newBuilder(getBaseUri().resolve("DocumentReference"))
+    void shouldReturnTheNhsNumberParameter() throws IOException, InterruptedException  {
+        var nhsNumber = "1234567890";
+        var nhsNumberParameter = URLEncoder.encode("https://fhir.nhs.uk/Id/nhs-number|" + nhsNumber, StandardCharsets.UTF_8);
+        var deleteDocumentReferenceRequest = HttpRequest.newBuilder(getBaseUri().resolve("DocumentReference?subject:identifier=" + nhsNumberParameter))
                 .DELETE()
                 .build();
 
         var deleteDocumentReferenceResponse = newHttpClient().send(deleteDocumentReferenceRequest, HttpResponse.BodyHandlers.ofString());
 
-        assertThat(deleteDocumentReferenceResponse.statusCode()).isEqualTo("200");
+        assertThat(deleteDocumentReferenceResponse.statusCode()).isEqualTo(200);
+        assertThat(deleteDocumentReferenceResponse.body()).isEqualTo(nhsNumber);
     }
 }
--- app/src/main/java/uk/nhs/digital/docstore/DeleteDocumentReferenceHandler.java
+++ app/src/main/java/uk/nhs/digital/docstore/DeleteDocumentReferenceHandler.java
@@ -4,10 +4,23 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import uk.nhs.digital.docstore.config.ApiConfig;
 
 public class DeleteDocumentReferenceHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private final ApiConfig apiConfig;
+
+    public DeleteDocumentReferenceHandler() {
+        this(new ApiConfig());
+    }
+
+    public DeleteDocumentReferenceHandler(ApiConfig apiConfig) {
+        this.apiConfig = apiConfig;
+    }
+
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-        throw new RuntimeException("not implemented");
+        String nhsNumber = new NHSNumberSearchParameterForm(input.getQueryStringParameters()).getNhsNumber();
+        return apiConfig.getApiGatewayResponse(200, nhsNumber, "DELETE", null);
     }
 }
