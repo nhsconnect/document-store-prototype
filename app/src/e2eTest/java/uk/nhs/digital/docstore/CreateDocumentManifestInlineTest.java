391832b18ac6e49cb84263503efddb1f70bc5b2b
Nicholas Hill <nick.hill3@nhs.net>
2023-01-03 15:08:54 +0000

    [PRMT-2816] Replace static RequestEvent with private builder method in CreateDocumentManifestInlineTest

--- app/src/e2eTest/java/uk/nhs/digital/docstore/CreateDocumentManifestInlineTest.java
+++ app/src/e2eTest/java/uk/nhs/digital/docstore/CreateDocumentManifestInlineTest.java
@@ -40,93 +40,89 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
-
 @ExtendWith(MockitoExtension.class)
 public class CreateDocumentManifestInlineTest {
-
     @Mock
     private Context context;
     @Mock
-    private DynamoDBMapper dynamoDBMapper;
+    private DynamoDBMapper dynamoDbMapper;
     @Mock
     private AmazonS3 s3Client;
     @Mock
-    private DocumentMetadataStore metadataStore;
-    @Mock
     private AmazonSQSClient sqsClient;
+    @Mock
+    private DocumentMetadataStore metadataStore;
+
     @Captor
-    ArgumentCaptor<SendMessageRequest> messageRequestCaptor;
+    ArgumentCaptor<SendMessageRequest> sendMessageRequestCaptor;
+
     private CreateDocumentManifestByNhsNumberHandler handler;
-    private RequestEventBuilder requestBuilder;
 
     @BeforeEach
     public void setUp() {
         handler = new CreateDocumentManifestByNhsNumberHandler(new StubbedApiConfig("http://ui-url"),
                 metadataStore,
-                new DocumentZipTraceStore(dynamoDBMapper),
+                new DocumentZipTraceStore(dynamoDbMapper),
                 new DocumentStore(s3Client, "bucket-name"),
                 new DocumentManifestService(new SplunkPublisher(sqsClient)),
                 "1");
-        requestBuilder = new RequestEventBuilder();
     }
 
     @Test
-    void shouldUploadZipOfAllDocumentsAndSaveMetadataForGivenNhsNumber() throws MalformedURLException {
+    void uploadsZipOfAllDocsAndSavesMetadataForGivenNhsNumber() throws MalformedURLException {
         var nhsNumber = "9000000009";
         var presignedUrl = "http://presigned-url";
+        var metadataList = List.of(createMetadata("some document"), createMetadata("another document"));
+        var requestEvent = createRequestEvent(nhsNumber);
 
-        when(metadataStore.findByNhsNumber(nhsNumber)).thenReturn(
-                List.of(createMetadata("some document"), createMetadata("another document")));
+        when(metadataStore.findByNhsNumber(nhsNumber)).thenReturn(metadataList);
         when(s3Client.getObject(anyString(), anyString())).thenReturn(new S3Object());
         when(s3Client.generatePresignedUrl(any())).thenReturn(new URL(presignedUrl));
-        doNothing().when(dynamoDBMapper).save(any());
+        doNothing().when(dynamoDbMapper).save(any());
+        var responseEvent = handler.handleRequest(requestEvent, context);
 
-        var request = requestBuilder
-                .addQueryParameter("subject:identifier", "https://fhir.nhs.uk/Id/nhs-number|" + nhsNumber)
-                .addHeader("Authorization", createBearerToken())
-                .build();
-        var responseEvent = handler.handleRequest(request, context);
-        var responseUrl =  JsonPath.<String>read(responseEvent.getBody(), "$.result.url");
-
+        var responseUrl = JsonPath.<String>read(responseEvent.getBody(), "$.result.url");
         assertThat(responseEvent.getStatusCode()).isEqualTo(200);
         assertThat(responseEvent.getHeaders().get("Content-Type")).isEqualTo("application/fhir+json");
         assertThat(responseUrl).isEqualTo(presignedUrl);
     }
 
     @Test
-    void shouldSendAuditMessageAfterZipIsUploadedSuccessfully() throws MalformedURLException {
+    void sendsAuditMessageAfterZipIsUploadedSuccessfully() throws MalformedURLException {
         var nhsNumber = "9000000009";
         var presignedUrl = "http://presigned-url";
         var now = Instant.now();
-
         var fileMetadata = new JSONObject();
-        fileMetadata.put("id","123");
-        fileMetadata.put("fileName","some document");
-        fileMetadata.put("fileType","text/plain");
-
+        fileMetadata.put("id", "123");
+        fileMetadata.put("fileName", "some document");
+        fileMetadata.put("fileType", "text/plain");
         var expectedMessageBody = new JSONObject();
         expectedMessageBody.put("nhsNumber", nhsNumber);
         expectedMessageBody.put("fileMetadataList", List.of(fileMetadata));
         expectedMessageBody.put("timestamp", now.toString());
+        var requestEvent = createRequestEvent(nhsNumber);
+        var metadataList = List.of(createMetadata("some document"));
 
-        var request = requestBuilder
-                .addQueryParameter("subject:identifier", "https://fhir.nhs.uk/Id/nhs-number|" + nhsNumber)
-                .addHeader("Authorization", createBearerToken())
-                .build();
-
-        when(metadataStore.findByNhsNumber(nhsNumber)).thenReturn(
-                List.of(createMetadata("some document")));
+        when(metadataStore.findByNhsNumber(nhsNumber)).thenReturn(metadataList);
         when(s3Client.getObject(anyString(), anyString())).thenReturn(new S3Object());
         when(s3Client.generatePresignedUrl(any())).thenReturn(new URL(presignedUrl));
-        doNothing().when(dynamoDBMapper).save(any());
+        doNothing().when(dynamoDbMapper).save(any());
+        handler.handleRequest(requestEvent, context);
 
-        handler.handleRequest(request, context);
-
-        verify(sqsClient).sendMessage(messageRequestCaptor.capture());
-        var messageBody = messageRequestCaptor.getValue().getMessageBody();
+        verify(sqsClient).sendMessage(sendMessageRequestCaptor.capture());
+        var messageBody = sendMessageRequestCaptor.getValue().getMessageBody();
         var timestamp = JsonPath.read(messageBody, "$.timestamp").toString();
         assertThatJson(messageBody).whenIgnoringPaths("fileMetadata.id", "timestamp").isEqualTo(expectedMessageBody);
         assertThat(Instant.parse(timestamp)).isCloseTo(now, within(1, ChronoUnit.SECONDS));
+    }
+
+    private APIGatewayProxyRequestEvent createRequestEvent(String nhsNumber) {
+        HashMap<String, String> headers = new HashMap<>();
+        HashMap<String, String> parameters = new HashMap<>();
+        headers.put("Authorization", "Bearer " + JWT.create().withClaim("email", "").sign(none()));
+        parameters.put("subject:identifier", "https://fhir.nhs.uk/Id/nhs-number|" + nhsNumber);
+
+        return new APIGatewayProxyRequestEvent().withQueryStringParameters(parameters).withHeaders(headers);
     }
 
     private DocumentMetadata createMetadata(String fileName) {
@@ -136,32 +132,7 @@ public class CreateDocumentManifestInlineTest {
         metadata.setDescription(fileName);
         metadata.setContentType("text/plain");
         metadata.setLocation("s3://bucket/key");
+
         return metadata;
-    }
-
-    public static class RequestEventBuilder {
-        private HashMap<String, String> parameters = new HashMap<>();
-        private HashMap<String, String> headers = new HashMap<>();
-
-        private RequestEventBuilder addQueryParameter(String name, String value) {
-            parameters.put(name, value);
-            return this;
-        }
-
-        private RequestEventBuilder addHeader(String name, String value) {
-            headers.put(name, value);
-            return this;
-        }
-
-        private APIGatewayProxyRequestEvent build() {
-            return new APIGatewayProxyRequestEvent().withQueryStringParameters(parameters).withHeaders(headers);
-        }
-    }
-
-    private String createBearerToken() {
-        String jwt = JWT.create()
-                .withClaim("email", "")
-                .sign(none());
-        return "Bearer " + jwt;
     }
 }
