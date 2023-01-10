e0efc1a934cb41627ffef3296e4a0c78d4c5403c
Nicholas Hill <nick.hill3@nhs.net>
2023-01-10 09:10:52 +0000

    [TECH] Add formatting, linting, and Cypress config files to /cypress

---
+++ cypress/cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: "http://localhost:3000",
+        defaultCommandTimeout: 30000,
+        includeShadowDom: true,
+        chromeWebSecurity: false,
+        watchForFileChanges: false,
+        videoUploadOnPasses: false,
+        setupNodeEvents(on) {
+            on("task", {
+                log(message) {
+                    console.log(message);
+                    return null;
+                },
+                table(message) {
+                    console.table(message);
+                    return null;
+                },
+            });
+        },
+    },
+});
