7ff885b374221c51bffb385b2b89f23894595d29
Nicholas Hill <nick.hill3@nhs.net>
2023-01-10 09:17:15 +0000

    [TECH] Add E2E test, fixtures, and support tools to /cypress

---
+++ cypress/support/e2e.js
@@ -0,0 +1,3 @@
+import "@testing-library/cypress/add-commands";
+import "cypress-axe";
+import "./commands";
---
+++ cypress/support/utils.js
@@ -0,0 +1,16 @@
+function logAccessibilityViolations(violations) {
+    const accessibilityViolationsTitle = `${violations.length} accessibility violation${
+        violations.length === 1 ? "" : "s"
+    } ${violations.length === 1 ? "was" : "were"} detected`;
+    const accessibilityViolationsData = violations.map(({ id, impact, description, nodes }) => ({
+        id,
+        impact,
+        description,
+        nodes: nodes.length,
+    }));
+
+    cy.task("log", accessibilityViolationsTitle);
+    cy.task("table", accessibilityViolationsData);
+}
+
+export { logAccessibilityViolations };
