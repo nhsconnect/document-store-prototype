fa88843abbe1f608c1b016676d346fead7b6e88b
Nicholas Hill <nick.hill3@nhs.net>
2023-01-11 10:20:14 +0000

    [PRMT-2816] Rename sensitive index queue from "document-store-audit" to "sensitive-index"

--- terraform/queue.tf
+++ terraform/queue.tf
@@ -1,10 +1,10 @@
-resource "aws_sqs_queue" "document-store" {
-  name = "document-store-audit"
+resource "aws_sqs_queue" "sensitive_audit" {
+  name = "sensitive-audit"
   message_retention_seconds = 1209600
   sqs_managed_sse_enabled = true
 }
 
-resource "aws_iam_role_policy" "document-store" {
+resource "aws_iam_role_policy" "sensitive_audit" {
   name = "send_audit_messages_policy"
   role = aws_iam_role.lambda_execution_role.id
   policy    = jsonencode({
@@ -17,7 +17,7 @@ resource "aws_iam_role_policy" "document-store" {
           "sqs:SendMessage",
           "sqs:GetQueueUrl"
         ],
-        "Resource" : aws_sqs_queue.document-store.arn
+        "Resource" : aws_sqs_queue.sensitive_audit.arn
       }
     ]
   })
