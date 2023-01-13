f09a08ca97ac9c1b3f1024ba738450ade68e6d4d
Nicholas Hill <nick.hill3@nhs.net>
2023-01-13 11:52:56 +0000

    [PRMT-2805] Enrich DeletedAllDocumentsAuditMessage with desc

--- app/src/main/java/uk/nhs/digital/docstore/audit/message/DeletedAllDocumentsAuditMessage.java
+++ app/src/main/java/uk/nhs/digital/docstore/audit/message/DeletedAllDocumentsAuditMessage.java
@@ -3,6 +3,6 @@ package uk.nhs.digital.docstore.audit.message;
 public class DeletedAllDocumentsAuditMessage extends BaseAuditMessage implements AuditMessage {
     @Override
     public String getDescription() {
-        return null;
+        return "Deleted all documents";
     }
 }
