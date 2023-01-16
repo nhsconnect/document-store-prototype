15a4c62ab4d5ced04ab5e24c7022dd8c69433ce8
Anu Doddala <anu.doddala1@nhs.net>
2023-01-16 11:08:30 +0000

    [PRMT-2782] Add test coverage for StartPage

--- ui/src/pages/StartPage.test.jsx
+++ ui/src/pages/StartPage.test.jsx
@@ -2,6 +2,27 @@ import {render, screen} from "@testing-library/react";
 import StartPage from "./StartPage";
 
 describe("<StartPage/>", () => {
+    it("renders the page header", () => {
+        render(<StartPage/>);
+
+        expect(screen.getByRole("heading", {name: "Document Store"})).toBeInTheDocument();
+    })
+
+    it('renders a list of service features', () => {
+        render(<StartPage/>);
+
+        expect(screen.getByText("Use this service to:")).toBeInTheDocument()
+        expect(screen.getByRole("list")).toBeInTheDocument();
+        expect(screen.getAllByRole("listitem")).toHaveLength(2);
+    });
+
+    it("renders a 'before you start' heading", () => {
+        render(<StartPage/>);
+
+        expect(screen.getByRole("heading", {name: "Before you start"})).toBeInTheDocument();
+        expect(screen.getByText("You can only use this service if you have a valid CIS2 account")).toBeInTheDocument();
+    });
+
     it("renders a button link with an href to /home", () => {
         render(<StartPage/>);
 
