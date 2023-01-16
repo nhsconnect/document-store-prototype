8b8eb21510f639d23320920811c35ffbf97810ae
Anu Doddala <anu.doddala1@nhs.net>
2023-01-16 11:51:35 +0000

    [PRMT-2782] update service info on the StartPage.
    Co-authored-by: Nick Hill <nick.hill3@nhs.net>

--- ui/src/pages/StartPage.jsx
+++ ui/src/pages/StartPage.jsx
@@ -4,15 +4,23 @@ import {ButtonLink} from "nhsuk-react-components";
 export default function StartPage() {
     return (
         <>
-            <h1>Document Store</h1>
-            <p>Use this service to:</p>
-            <ul>
-                <li>Upload patient digital records</li>
-                <li>Download and view patient digital records</li>
-            </ul>
+            <h1>Inactive Patient Record Administration</h1>
+            <p>
+                When a patient is inactive, NHS England via Primary Care Support England are responsible for
+                administration of their Electronic health record (EHR) and attachments until they register at their next
+                GP Practice.
+            </p>
+            <p>
+                General Practice Staff should use this service to upload an inactive patient&apos;s electronic health
+                record and attachments.
+            </p>
+            <p>
+                PCSE should use this service to search for and download patient records where there has been an access
+                request for an inactive patient health record.
+            </p>
             <h2 className="nhsuk-heading-m">Before you start</h2>
             <p>You can only use this service if you have a valid CIS2 account</p>
             <ButtonLink href="/home">Start now</ButtonLink>
         </>
     );
-};
+}
--- ui/src/pages/StartPage.test.jsx
+++ ui/src/pages/StartPage.test.jsx
@@ -3,29 +3,29 @@ import StartPage from "./StartPage";
 
 describe("<StartPage/>", () => {
     it("renders the page header", () => {
-        render(<StartPage/>);
+        render(<StartPage />);
 
-        expect(screen.getByRole("heading", {name: "Document Store"})).toBeInTheDocument();
-    })
+        expect(screen.getByRole("heading", { name: "Inactive Patient Record Administration" })).toBeInTheDocument();
+    });
 
-    it('renders a list of service features', () => {
-        render(<StartPage/>);
+    it("renders info about the service", () => {
+        render(<StartPage />);
 
-        expect(screen.getByText("Use this service to:")).toBeInTheDocument()
-        expect(screen.getByRole("list")).toBeInTheDocument();
-        expect(screen.getAllByRole("listitem")).toHaveLength(2);
+        expect(screen.getByText(/When a patient is inactive/)).toBeInTheDocument();
+        expect(screen.getByText(/General Practice Staff/)).toBeInTheDocument();
+        expect(screen.getByText(/PCSE should use this service/)).toBeInTheDocument();
     });
 
     it("renders a 'before you start' heading", () => {
-        render(<StartPage/>);
+        render(<StartPage />);
 
-        expect(screen.getByRole("heading", {name: "Before you start"})).toBeInTheDocument();
+        expect(screen.getByRole("heading", { name: "Before you start" })).toBeInTheDocument();
         expect(screen.getByText("You can only use this service if you have a valid CIS2 account")).toBeInTheDocument();
     });
 
     it("renders a button link with an href to /home", () => {
-        render(<StartPage/>);
+        render(<StartPage />);
 
-        expect(screen.getByRole("button", {name: "Start now"})).toHaveAttribute("href", "/home");
+        expect(screen.getByRole("button", { name: "Start now" })).toHaveAttribute("href", "/home");
     });
 });
