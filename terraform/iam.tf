848b954a1a61edb78c21775f66828cb8f9bfcb7e
Nicholas Hill <nick.hill3@nhs.net>
2023-01-18 12:04:05 +0000

    [PRMT-2816] Create splunk_access_policy aws_iam_policy_document

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -8,6 +8,26 @@ data "aws_iam_policy_document" "splunk_trust_policy" {
   }
 }
 
+data "aws_iam_policy_document" "splunk_access_policy" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "sqs:GetQueueAttributes",
+      "sqs:ListQueues",
+      "sqs:ReceiveMessage",
+      "sqs:GetQueueUrl",
+      "sqs:SendMessage",
+      "sqs:DeleteMessage"
+    ]
+    resources = ["arn:aws:sqs:eu-west-2:*:*-sensitive-audit"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["kms:Decrypt"]
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_role_policy" "sensitive_audit" {
   name   = "send_audit_messages_policy"
   role   = aws_iam_role.lambda_execution_role.id
