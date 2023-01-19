af268bc841a7bd9151a20fd9794444f73b90a8c1
romanenhs <romane.castera1@nhs.net>
2023-01-19 13:28:22 +0000

    [PRMT-2959] Add link to error page

--- ui/src/pages/ErrorPage.js
+++ ui/src/pages/ErrorPage.js
@@ -5,7 +5,7 @@ const ErrorPage = () => {
             <p>Try again later.</p>
             <p>
                 If there is an issue with the service please contact the{" "}
-                <a href={"#"}>NHS Digital National Service Desk</a>.
+                <a href={"https://digital.nhs.uk/about-nhs-digital/contact-us"}>NHS Digital National Service Desk</a>.
             </p>
         </>
     );
