043010fe70f265e41bc946d5e70b98b8ffbbdaa1
Kurtis <kurtis.angell1@nhs.net>
2023-01-19 17:26:44 +0000

    [PRMT-2933] Add nationalrbacaccess and associatedorgs scopes to cognito identity provider

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -23,13 +23,19 @@ resource "aws_cognito_user_pool_client" "client" {
   count = var.cloud_only_service_instances
 
   allowed_oauth_flows_user_pool_client = true
-  explicit_auth_flows                  = ["ALLOW_ADMIN_USER_PASSWORD_AUTH", "ALLOW_USER_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
+  explicit_auth_flows                  = [
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH", "ALLOW_USER_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
   allowed_oauth_flows                  = ["code", "implicit"]
   allowed_oauth_scopes                 = ["openid"]
   supported_identity_providers         = var.cognito_oidc_providers
-  callback_urls                        = concat(var.cognito_cis2_client_callback_urls, ["https://${aws_amplify_branch.main[0].branch_name}.${aws_amplify_app.doc-store-ui[0].id}.amplifyapp.com/cis2-auth-callback"])
+  callback_urls                        = concat(var.cognito_cis2_client_callback_urls, [
+    "https://${aws_amplify_branch.main[0].branch_name}.${aws_amplify_app.doc-store-ui[0].id}.amplifyapp.com/cis2-auth-callback"
+  ])
   default_redirect_uri                 = "https://${aws_amplify_branch.main[0].branch_name}.${aws_amplify_app.doc-store-ui[0].id}.amplifyapp.com/cis2-auth-callback"
-  logout_urls                          = concat(["https://${aws_amplify_branch.main[0].branch_name}.${aws_amplify_app.doc-store-ui[0].id}.amplifyapp.com"], var.cognito_cis2_client_signout_urls)
+  logout_urls                          = concat([
+    "https://${aws_amplify_branch.main[0].branch_name}.${aws_amplify_app.doc-store-ui[0].id}.amplifyapp.com"
+  ], var.cognito_cis2_client_signout_urls)
 }
 
 resource "aws_cognito_user_pool_domain" "domain" {
@@ -64,7 +70,7 @@ resource "aws_cognito_identity_provider" "cis2_identity_provider" {
   provider_type = "OIDC"
 
   provider_details = {
-    authorize_scopes          = "openid"
+    authorize_scopes          = "openid associatedorg nationalrbacaccess"
     client_id                 = var.cognito_cis2_provider_client_id
     client_secret             = var.cognito_cis2_provider_client_secret
     oidc_issuer               = var.cognito_cis2_provider_oidc_issuer
