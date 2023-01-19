070fa8ecfbd473c0d407c05f60be6bae76a2bb9c
Kurtis <kurtis.angell1@nhs.net>
2023-01-19 17:19:50 +0000

    Revert "[PRMT-2806] Enabling nationalrbacaccess and associatedorgs scopes"
    This reverts commit 6ae071901c55cbda8d8435822d85b73617573f69.

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/Authoriser.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/Authoriser.java
@@ -88,7 +88,7 @@ public class Authoriser implements RequestHandler<APIGatewayCustomAuthorizerEven
             iamPolicy.setPolicyDocument(policyDocument);
             return iamPolicy;
         } catch (NullPointerException e) {
-            throw new RuntimeException(e);
+            throw e;
         } catch (InvalidAccessTokenException | InvalidJWTException e) {
             throw new RuntimeException(e);
         }
