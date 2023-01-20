61697238c61112e7613e383ca53b79075f3a52ef
Nicholas Hill <nick.hill3@nhs.net>
2023-01-20 12:05:23 +0000

    [PRMT-2820] Remove array from dimensions.FunctionName on aws_cloudwatch_metric_alarm

--- terraform/alarm.tf
+++ terraform/alarm.tf
@@ -18,7 +18,7 @@ resource "aws_cloudwatch_metric_alarm" "document_uploaded_event_handler_error" {
   alarm_description = "Triggers when an error has occurred in DocumentUploadedEventHandler."
   namespace         = "AWS/Lambda"
   dimensions        = {
-    FunctionName = [aws_lambda_function.document_uploaded_lambda.function_name]
+    FunctionName = aws_lambda_function.document_uploaded_lambda.function_name
   }
   metric_name         = "Errors"
   comparison_operator = "GreaterThanThreshold"
@@ -33,7 +33,7 @@ resource "aws_cloudwatch_metric_alarm" "search_patient_details_handler_error" {
   alarm_description = "Triggers when an error has occurred in SearchPatientDetailsHandler."
   namespace         = "AWS/Lambda"
   dimensions        = {
-    FunctionName = [aws_lambda_function.search_patient_details_lambda.function_name]
+    FunctionName = aws_lambda_function.search_patient_details_lambda.function_name
   }
   metric_name         = "Errors"
   comparison_operator = "GreaterThanThreshold"
