1b0dce49fd988c90bd4aaea43aadd0f10a642b95
Mirla Chucre <mirla.chucre1@nhs.net>
2023-01-24 15:00:42 +0000

    [PRMT-2806] re-added simple custom attribute with 'string_attribute_constraints'
    Co-authored-by: kurtis <kurtis.angell1@nhs.net>

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -13,6 +13,19 @@ resource "aws_cognito_user_pool" "pool" {
     require_symbols   = true
   }
 
+  schema {
+    name                     = "nhsid_user_orgs"
+    attribute_data_type      = "String"
+    mutable                  = false
+    required                 = false
+    developer_only_attribute = false
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
+  }
+
   count = var.cloud_only_service_instances
 }
 
@@ -80,6 +93,10 @@ resource "aws_cognito_identity_provider" "cis2_identity_provider" {
     attributes_url            = var.cognito_cis2_provider_attributes_url
     jwks_uri                  = var.cognito_cis2_provider_jwks_uri
     attributes_request_method = "GET"
+  }
+
+  attribute_mapping = {
+    nhsid_user_orgs   = "custom:nhsid_user_orgs"
   }
 
   count = var.cloud_only_service_instances
