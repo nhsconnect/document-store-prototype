b1b6509cbd5f21a4c3df437c075322838d23d2f8
Kurtis <kurtis.angell1@nhs.net>
2023-01-24 10:28:12 +0000

    [PRMT-2814] Add default constructor to AuthConfig
    Co-authored-by: Mirla Chucre <mirla.chucre1@nhs.net>

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/AuthConfig.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/AuthConfig.java
@@ -3,13 +3,16 @@ package uk.nhs.digital.docstore.authoriser;
 import java.util.List;
 
 public class AuthConfig {
-    private final List<String> allowedResourcesForPCSEUsers;
-    private final List<String> allowedResourcesForClinicalUsers;
+    private  List<String> allowedResourcesForPCSEUsers;
+    private  List<String> allowedResourcesForClinicalUsers;
 
     //TODO avoid need to know about these constructor parameter names in terraform
     public AuthConfig (List<String> allowedResourcesForPCSEUsers, List<String> allowedResourcesForClinicalUsers) {
         this.allowedResourcesForPCSEUsers = allowedResourcesForPCSEUsers;
         this.allowedResourcesForClinicalUsers = allowedResourcesForClinicalUsers;
+    }
+
+    public AuthConfig(){
     }
 
     public List<String> getAllowedResourcesForPCSEUsers() {
