0ffa5abf52b2c04fb2b4588368ec47e4fff69fbd
Ryan Brown <ryan.brown15@nhs.net>
2023-01-27 12:02:41 +0000

    [PRMT-2820] Remove commented terraform resource for metric filter
    Co-authored-by: Anu Doddala <anu.doddala1@nhs.net>

--- terraform/modules/lambda_alarms/main.tf
+++ terraform/modules/lambda_alarms/main.tf
@@ -64,15 +64,3 @@ resource "aws_cloudwatch_metric_alarm" "lambda_memory_alarm" {
   evaluation_periods  = "1"
   statistic           = "Maximum"
 }
-
-#resource "aws_cloudwatch_log_metric_filter" "lambda_max_memory_used_log_metric_filter" {
-#  name           = "prs_${var.lambda_short_name}_max_memory_used_log_metric_filter"
-#  log_group_name = aws_cloudwatch_log_group.lambda_log_group.name
-#  pattern        = "[report_name=\"REPORT\", request_id_name=\"RequestId:\", request_id_value, duration_name=\"Duration:\", duration_value, duration_unit=\"ms\", billed_duration_name_1=\"Billed\", billed_duration_name_2=\"Duration:\", billed_duration_value, billed_duration_unit=\"ms\", memory_size_name_1=\"Memory\", memory_size_name_2=\"Size:\", memory_size_value, memory_size_unit=\"MB\", max_memory_used_name_1=\"Max\", max_memory_used_name_2=\"Memory\", max_memory_used_name_3=\"Used:\", max_memory_used_value, ...]"
-#  metric_transformation {
-#    name      = "${var.lambda_short_name}/MaxMemoryUsed"
-#    namespace = "Lambda"
-#    value     = "$max_memory_used_value"
-#    unit = "MB"
-#  }
-#}
