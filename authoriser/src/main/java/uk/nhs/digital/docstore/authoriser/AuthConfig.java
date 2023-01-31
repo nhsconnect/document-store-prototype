f9c4be50c4a296e05739b8485e11e7274795b91b
Kurtis <kurtis.angell1@nhs.net>
2023-01-31 11:34:03 +0000

    [PRMT-2806] Change name of fields in AuthConfig
    Co-authored-by: Mirla Chucre <mirla.chucre1@nhs.net>
    Co-authored-by: Richard Smith <richard.smith33@nhs.net>

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/AuthConfig.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/AuthConfig.java
@@ -3,23 +3,22 @@ package uk.nhs.digital.docstore.authoriser;
 import java.util.List;
 
 public class AuthConfig {
-    private  List<String> allowedResourcesForPCSEUsers;
-    private  List<String> allowedResourcesForClinicalUsers;
+    private  List<String> resourcesForPCSEUsers;
+    private  List<String> resourcesForClinicalUsers;
 
-    //TODO avoid need to know about these constructor parameter names in terraform
-    public AuthConfig (List<String> allowedResourcesForPCSEUsers, List<String> allowedResourcesForClinicalUsers) {
-        this.allowedResourcesForPCSEUsers = allowedResourcesForPCSEUsers;
-        this.allowedResourcesForClinicalUsers = allowedResourcesForClinicalUsers;
+    public AuthConfig(List<String> resourcesForPCSEUsers, List<String> resourcesForClinicalUsers) {
+        this.resourcesForPCSEUsers = resourcesForPCSEUsers;
+        this.resourcesForClinicalUsers = resourcesForClinicalUsers;
     }
 
     public AuthConfig(){
     }
 
     public List<String> getResourcesForPCSEUsers() {
-        return allowedResourcesForPCSEUsers;
+        return resourcesForPCSEUsers;
     }
 
     public List<String> getResourcesForClinicalUsers() {
-        return allowedResourcesForClinicalUsers;
+        return resourcesForClinicalUsers;
     }
 }
