a53fdf2bc52e69572130223af3c61663afc62768
Kurtis <kurtis.angell1@nhs.net>
2023-02-01 09:35:53 +0000

    [PRMT-2806] Change name of custom authoriser to string

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,7 +121,7 @@ resource "aws_api_gateway_authorizer" "cognito_authorizer" {
 }
 
 resource "aws_api_gateway_authorizer" "custom_authoriser" {
-  name                   = custom_authoriser
+  name                   = "custom_authoriser"
   rest_api_id            = aws_api_gateway_rest_api.lambda_api.id
   authorizer_uri         = aws_lambda_function.authoriser.invoke_arn
   authorizer_credentials = aws_iam_role.lambda_execution_role.arn
