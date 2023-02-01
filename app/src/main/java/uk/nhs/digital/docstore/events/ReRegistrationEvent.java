9c197ed1e399742644d3fb52c3c55b6a1ed64dd3
romanenhs <romane.castera1@nhs.net>
2023-02-01 16:37:24 +0000

    [PRMT-2845] fixed formatting for ReRegistrationEvent
    Co-authored-by: Ryan Brown <ryan.brown15@nhs.net>
    Co-authored-by: Anusha Doddala <anu.doddala1@nhs.net>

--- app/src/main/java/uk/nhs/digital/docstore/events/ReRegistrationEvent.java
+++ app/src/main/java/uk/nhs/digital/docstore/events/ReRegistrationEvent.java
@@ -9,14 +9,15 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import uk.nhs.digital.docstore.exceptions.IllFormedPatientDetailsException;
 import uk.nhs.digital.docstore.model.NhsNumber;
 
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ReRegistrationEvent {
     private final NhsNumber nhsNumber;
     private final String nemsMessageId;
 
-    public ReRegistrationEvent(@JsonProperty("nhsNumber") String nhsNumber,
-                               @JsonProperty("nemsMessageId") String nemsMessageId) throws IllFormedPatientDetailsException {
+    public ReRegistrationEvent(
+            @JsonProperty("nhsNumber") String nhsNumber,
+            @JsonProperty("nemsMessageId") String nemsMessageId)
+            throws IllFormedPatientDetailsException {
         this.nhsNumber = new NhsNumber(nhsNumber);
         this.nemsMessageId = nemsMessageId;
     }
@@ -39,5 +40,4 @@ public class ReRegistrationEvent {
     public int hashCode() {
         return HashCodeBuilder.reflectionHashCode(this);
     }
-
 }
--- app/src/test/java/uk/nhs/digital/docstore/events/ReRegistrationEventTest.java
+++ app/src/test/java/uk/nhs/digital/docstore/events/ReRegistrationEventTest.java
@@ -1,10 +1,10 @@
 package uk.nhs.digital.docstore.events;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import uk.nhs.digital.docstore.exceptions.IllFormedPatientDetailsException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ReRegistrationEventTest {
 
@@ -13,12 +13,13 @@ class ReRegistrationEventTest {
         var nhsNumber = "9876543210";
         var nemsMessageId = "123";
 
-        var message = new JSONObject()
-                .put("nhsNumber", nhsNumber)
-                .put("newlyRegisteredOdsCode", "ABC12")
-                .put("nemsMessageId", nemsMessageId)
-                .put("lastUpdated", "some date")
-                .toString();
+        var message =
+                new JSONObject()
+                        .put("nhsNumber", nhsNumber)
+                        .put("newlyRegisteredOdsCode", "ABC12")
+                        .put("nemsMessageId", nemsMessageId)
+                        .put("lastUpdated", "some date")
+                        .toString();
 
         var expectedReRegistrationEvent = new ReRegistrationEvent(nhsNumber, nemsMessageId);
 
@@ -26,5 +27,4 @@ class ReRegistrationEventTest {
 
         assertEquals(expectedReRegistrationEvent, reRegistrationEvent);
     }
-
 }
