ae5b1faa42ec4de159c7093669f60a44ded78e4f
Ryan Brown <ryan.brown15@nhs.net>
2023-02-02 17:23:20 +0000

    [TECH] Ensure delete documents handler integration test asserts on contents of DynamoDB and S3 bucket

--- app/src/e2eTest/java/uk/nhs/digital/docstore/DeleteDocumentReferenceInlineTest.java
+++ app/src/e2eTest/java/uk/nhs/digital/docstore/DeleteDocumentReferenceInlineTest.java
@@ -1,18 +1,13 @@
 package uk.nhs.digital.docstore;
 
-import static com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig.SaveBehavior.UPDATE_SKIP_NULL_ATTRIBUTES;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import java.util.HashMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,8 +19,22 @@ import uk.nhs.digital.docstore.config.StubbedApiConfig;
 import uk.nhs.digital.docstore.data.repository.DocumentMetadataStore;
 import uk.nhs.digital.docstore.data.repository.DocumentStore;
 import uk.nhs.digital.docstore.data.serialiser.DocumentMetadataSerialiser;
+import uk.nhs.digital.docstore.exceptions.IllFormedPatientDetailsException;
 import uk.nhs.digital.docstore.handlers.DeleteDocumentReferenceHandler;
+import uk.nhs.digital.docstore.helpers.AwsS3Helper;
+import uk.nhs.digital.docstore.helpers.BaseUriHelper;
+import uk.nhs.digital.docstore.helpers.DocumentMetadataBuilder;
+import uk.nhs.digital.docstore.model.DocumentLocation;
+import uk.nhs.digital.docstore.model.NhsNumber;
 import uk.nhs.digital.docstore.services.DocumentDeletionService;
+
+import java.io.ByteArrayInputStream;
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class DeleteDocumentReferenceInlineTest {
@@ -33,54 +42,66 @@ public class DeleteDocumentReferenceInlineTest {
     @Mock private AuditPublisher auditPublisher;
 
     private DeleteDocumentReferenceHandler deleteDocumentReferenceHandler;
-    private static final String AWS_ENDPOINT = "http://localhost:4566";
+
+    private DocumentStore documentStore;
+
+    private DocumentMetadataStore documentMetadataStore;
     private static final String AWS_REGION = "eu-west-2";
 
     @BeforeEach
     void setUp() {
         var apiConfig = new StubbedApiConfig("http://ui-url");
-        var documentDeletionService =
+        var endpoint = String.format("http://%s:4566", BaseUriHelper.getAwsHost());
+        var endpointConfiguration =
+                new AwsClientBuilder.EndpointConfiguration(endpoint, AWS_REGION);
+        var bucketName = new AwsS3Helper(endpointConfiguration).getDocumentStoreBucketName();
+
+        var dynamodbClient =
+                AmazonDynamoDBClientBuilder.standard()
+                        .withEndpointConfiguration(endpointConfiguration)
+                        .build();
+        var dynamoDBMapper = new DynamoDBMapper(dynamodbClient);
+
+        var s3Client =
+                AmazonS3ClientBuilder.standard()
+                        .withEndpointConfiguration(endpointConfiguration)
+                        .withPathStyleAccessEnabled(true)
+                        .build();
+
+        documentMetadataStore = new DocumentMetadataStore(dynamoDBMapper);
+        documentStore = new DocumentStore(s3Client, bucketName);
+        var deletionService =
                 new DocumentDeletionService(
-                        auditPublisher,
-                        // TODO: Change bucket name
-                        new DocumentStore(
-                                AmazonS3ClientBuilder.standard()
-                                        .withEndpointConfiguration(
-                                                new AwsClientBuilder.EndpointConfiguration(
-                                                        AWS_ENDPOINT, AWS_REGION))
-                                        .withPathStyleAccessEnabled(true)
-                                        .build(),
-                                "bucket-name"),
-                        new DocumentMetadataStore(
-                                new DynamoDBMapper(
-                                        AmazonDynamoDBClientBuilder.standard()
-                                                .withEndpointConfiguration(
-                                                        new AwsClientBuilder.EndpointConfiguration(
-                                                                AWS_ENDPOINT, AWS_REGION))
-                                                .build(),
-                                        DynamoDBMapperConfig.builder()
-                                                .withSaveBehavior(UPDATE_SKIP_NULL_ATTRIBUTES)
-                                                .build())),
-                        new DocumentMetadataSerialiser());
+                        auditPublisher, documentStore, documentMetadataStore, new DocumentMetadataSerialiser());
 
         deleteDocumentReferenceHandler =
-                new DeleteDocumentReferenceHandler(apiConfig, documentDeletionService);
+                new DeleteDocumentReferenceHandler(apiConfig, deletionService);
     }
 
-    // TODO: Use DocumentMetadataStore and DocumentStore to interact with AWS resources so that we
-    // can
-    // perform arrange and assert + change test name
     @Test
-    void sendsAuditMessageUponSuccessfulDeletion() throws JsonProcessingException {
+    void deletesDocumentAndPublishesAuditMessage() throws JsonProcessingException, IllFormedPatientDetailsException {
+        var nhsNumber = new NhsNumber("1234567890");
+        var metadata = DocumentMetadataBuilder.theMetadata().withNhsNumber(nhsNumber).build();
+        var content = "content of file stored in S3";
 
-        deleteDocumentReferenceHandler.handleRequest(createRequestEvent(), context);
+        documentMetadataStore.save(metadata);
+        documentStore.addDocument(
+                new DocumentLocation(metadata.getLocation()).getPath(),
+                new ByteArrayInputStream(content.getBytes()));
+
+        deleteDocumentReferenceHandler.handleRequest(createRequestEvent(nhsNumber), context);
+
+        assertThat(documentMetadataStore.findByNhsNumber(nhsNumber)).isEmpty();
+        assertThrows(
+                AmazonS3Exception.class,
+                () -> documentStore.getObjectFromS3(new DocumentLocation(metadata.getLocation())));
 
         verify(auditPublisher).publish(any(DeletedAllDocumentsAuditMessage.class));
     }
 
-    private APIGatewayProxyRequestEvent createRequestEvent() {
+    private APIGatewayProxyRequestEvent createRequestEvent(NhsNumber nhsNumber) {
         HashMap<String, String> parameters = new HashMap<>();
-        parameters.put("subject:identifier", "https://fhir.nhs.uk/Id/nhs-number|9000000009");
+        parameters.put("subject:identifier", "https://fhir.nhs.uk/Id/nhs-number|" + nhsNumber.getValue());
 
         return new APIGatewayProxyRequestEvent().withQueryStringParameters(parameters);
     }
