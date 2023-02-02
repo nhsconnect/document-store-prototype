e44275189ac74918ec177d84d13f6ebb730892d3
Kurtis <kurtis.angell1@nhs.net>
2023-02-02 10:46:24 +0000

    [PRMT-2806] Change underscore to dash for consistency
    Co-authored-by: Richard Smith <richard.smith33@nhs.net>

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,7 +121,7 @@ resource "aws_api_gateway_authorizer" "cognito_authorizer" {
 }
 
 resource "aws_api_gateway_authorizer" "cis2_authoriser" {
-  name                   = "cis2_authoriser"
+  name                   = "cis2-authoriser"
   rest_api_id            = aws_api_gateway_rest_api.lambda_api.id
   authorizer_uri         = aws_lambda_function.authoriser.invoke_arn
   authorizer_credentials = aws_iam_role.authoriser_execution.arn
