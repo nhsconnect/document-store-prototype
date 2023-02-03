21745b219aa869e8a67992227202b83aeecff9f3
Ryan Brown <ryan.brown15@nhs.net>
2023-02-03 17:31:23 +0000

    [TECH] Stabilise storage axios instance for rendering control

--- ui/src/apiClients/documentStore.js
+++ ui/src/apiClients/documentStore.js
@@ -102,6 +102,6 @@ export const useDocumentStore = () => {
                 }
             },
         }),
-        [request]
+        [request, storage]
     );
 };
--- ui/src/apiClients/useStorage.js
+++ ui/src/apiClients/useStorage.js
@@ -2,5 +2,5 @@ import axios from "axios";
 import { useMemo } from "react";
 
 export const useStorage = () => {
-    return useMemo(() => axios.create());
+    return useMemo(() => axios.create(), []);
 };
