56e7a967f09a35f5c955216a587f825d9d1281ed
Ryan Brown <ryan.brown15@nhs.net>
2023-02-03 16:35:24 +0000

    [BUGFIX] Ensure config file is not required to run jest tests
    Co-authored-by: Nicholas Hill <nick.hill3@nhs.net>

--- ui/src/apiClients/useApi.js
+++ ui/src/apiClients/useApi.js
@@ -1,11 +1,12 @@
 import { useAuth } from "react-oidc-context";
 import axios from "axios";
-import config from "../config";
+import { useConfig } from "../utils/useConfig";
 import { useMemo } from "react";
 
 export const useApiRequest = (apiName) => {
     const { user } = useAuth();
-    const baseUrl = config.API.endpoints.find((endpoint) => endpoint.name === apiName).endpoint;
+    const { API } = useConfig();
+    const baseUrl = API.endpoints.find((endpoint) => endpoint.name === apiName).endpoint;
     return useMemo(
         () =>
             axios.create({
---
+++ ui/src/utils/useConfig.js
@@ -0,0 +1,3 @@
+import config from "../config";
+
+export const useConfig = () => config;
