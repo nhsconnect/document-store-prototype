cdd51bd9bb1d4c5bd20d8e05009374e4832313f9
Ryan Brown <ryan.brown15@nhs.net>
2023-02-08 15:45:10 +0000

    [PRMT-3018]: Attempt to ensure access control allow method wildcard is not interpreted as VTL
    Co-authored-by: Nicholas Hill <nick.hill3@nhs.net>

--- terraform/main.tf
+++ terraform/main.tf
@@ -137,7 +137,7 @@ resource "aws_api_gateway_gateway_response" "doc_store_unauthorised_response" {
 
   response_parameters = {
     "gatewayresponse.header.Access-Control-Allow-Origin" = "${local.amplify_base_url}"
-    "gatewayresponse.header.Access-Control-Allow-Methods" = "*"
+    "gatewayresponse.header.Access-Control-Allow-Methods" = "'*'"
     "gatewayresponse.header.Access-Control-Allow-Headers" = "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
   }
 }
@@ -152,7 +152,7 @@ resource "aws_api_gateway_gateway_response" "doc_store_bad_gateway_response" {
 
   response_parameters = {
     "gatewayresponse.header.Access-Control-Allow-Origin" = "${local.amplify_base_url}"
-    "gatewayresponse.header.Access-Control-Allow-Methods" = "*"
+    "gatewayresponse.header.Access-Control-Allow-Methods" = "'*'"
     "gatewayresponse.header.Access-Control-Allow-Headers" = "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
   }
 }
