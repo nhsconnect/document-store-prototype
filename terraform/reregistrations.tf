be927c9948ae7827c3cc6e802ac7d2105a5a81e2
romanenhs <romane.castera1@nhs.net>
2023-02-09 14:52:21 +0000

    [PRMT-2845] reverted to using default batch size (10) for event_source_mapping
    Co-authored-by: Nicholas Hill <nick.hill3@nhs.net>

--- terraform/reregistrations.tf
+++ terraform/reregistrations.tf
@@ -19,9 +19,7 @@ resource "aws_lambda_function" "re_registration_lambda" {
 
 resource "aws_lambda_event_source_mapping" "event_source_mapping" {
   event_source_arn = aws_sqs_queue.re_registration.arn
-  enabled          = true
   function_name    = aws_lambda_function.re_registration_lambda.arn
-  batch_size       = 1
   function_response_types = ["ReportBatchItemFailures"]
 }
 
