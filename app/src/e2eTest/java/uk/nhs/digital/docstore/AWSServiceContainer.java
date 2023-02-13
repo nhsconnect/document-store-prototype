78b5108eccb2680dc40e6aab0db1232d134db8d1
Ryan Brown <ryan.brown15@nhs.net>
2023-02-13 16:40:45 +0000

    [TECH] Remove AWS Service Container dependency on BaseUriHelper

--- app/src/e2eTest/java/uk/nhs/digital/docstore/AWSServiceContainer.java
+++ app/src/e2eTest/java/uk/nhs/digital/docstore/AWSServiceContainer.java
@@ -6,16 +6,18 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.IDynamoDBMapper;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import uk.nhs.digital.docstore.helpers.BaseUriHelper;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class AWSServiceContainer {
     private static final String AWS_REGION = "eu-west-2";
-    private IDynamoDBMapper dynamoDBMapper;
+    private final IDynamoDBMapper dynamoDBMapper;
 
-    private AmazonS3 s3Client;
+    private final AmazonS3 s3Client;
 
     public AWSServiceContainer() {
-        var endpoint = String.format("http://%s:4566", BaseUriHelper.getAwsHost());
+        var endpoint = String.format("http://%s:4566", getAwsHost());
 
         var endpointConfiguration =
                 new AwsClientBuilder.EndpointConfiguration(endpoint, AWS_REGION);
@@ -38,5 +40,14 @@ public class AWSServiceContainer {
 
     public AmazonS3 getS3Client() {
         return s3Client;
+    }
+
+    private static String getAwsHost() {
+        try {
+            InetAddress.getByName("localstack");
+            return "localstack";
+        } catch (UnknownHostException e) {
+            return "localhost";
+        }
     }
 }
