a3d25eb3f3d8793ea350045d0c80e3611e8243b5
Ryan Brown <ryan.brown15@nhs.net>
2023-02-16 15:03:53 +0000

    Added debug code to pipeline

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -133,6 +133,7 @@ resource aws_iam_role "integration_test_runner" {
 data "aws_iam_policy_document" "integration_test_runner_trust_policy" {
   statement {
     actions = ["sts:AssumeRole"]
+    effect = "Allow"
     principals {
       identifiers = ["arn:aws:iam::327778747031:role/gocd_agent-prod"]
       type        = "AWS"
