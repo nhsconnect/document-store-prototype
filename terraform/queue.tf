cd477969262ad91910e91b34c2276b394e64cee5
Nicholas Hill <nick.hill3@nhs.net>
2023-02-17 16:26:24 +0000

    [PRMT-2845] Create policy to allow sending message to re-reg queue

--- terraform/queue.tf
+++ terraform/queue.tf
@@ -35,3 +35,27 @@ resource "aws_sqs_queue_redrive_allow_policy" "re_registration_redrive_allow_pol
     redrivePermission = "byQueue"
   })
 }
+
+resource "aws_sqs_queue_policy" "re_registration_queue_policy" {
+  queue_url = aws_sqs_queue.re_registration.id
+  policy    = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "SendMessageToReRegistrationQueue"
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "*"
+        },
+        "Action" : "sqs:SendMessage",
+        "Resource" : aws_sqs_queue.re_registration.arn,
+        "Condition" : {
+          "ArnLike" : {
+            "aws:SourceArn" : data.aws_ssm_parameter.re_registration_sns_topic_arn[0].value
+          }
+        }
+      }
+    ]
+  })
+  count = var.cloud_only_service_instances
+}
