1413e6b64355d0410f60e43aa260dff0d01dca56
Nicholas Hill <nick.hill3@nhs.net>
2023-02-24 17:25:07 +0000

    [PRMT-3129] Refactor PatientTracePage tests

--- ui/src/components/patientSummary/PatientSummary.jsx
+++ ui/src/components/patientSummary/PatientSummary.jsx
@@ -4,30 +4,28 @@ import { getFormattedDate } from "../../utils/utils";
 
 const PatientSummary = ({ patientDetails }) => {
     return (
-        <>
-            <SummaryList>
-                <SummaryList.Row>
-                    <SummaryList.Key>NHS Number</SummaryList.Key>
-                    <SummaryList.Value>{patientDetails?.nhsNumber}</SummaryList.Value>
-                </SummaryList.Row>
-                <SummaryList.Row>
-                    <SummaryList.Key>Surname</SummaryList.Key>
-                    <SummaryList.Value>{patientDetails?.familyName}</SummaryList.Value>
-                </SummaryList.Row>
-                <SummaryList.Row>
-                    <SummaryList.Key>First name</SummaryList.Key>
-                    <SummaryList.Value>{patientDetails?.givenName?.map((name) => `${name} `)}</SummaryList.Value>
-                </SummaryList.Row>
-                <SummaryList.Row>
-                    <SummaryList.Key>Date of birth</SummaryList.Key>
-                    <SummaryList.Value>{getFormattedDate(patientDetails?.birthDate)}</SummaryList.Value>
-                </SummaryList.Row>
-                <SummaryList.Row>
-                    <SummaryList.Key>Postcode</SummaryList.Key>
-                    <SummaryList.Value>{patientDetails?.postalCode}</SummaryList.Value>
-                </SummaryList.Row>
-            </SummaryList>
-        </>
+        <SummaryList>
+            <SummaryList.Row>
+                <SummaryList.Key>NHS Number</SummaryList.Key>
+                <SummaryList.Value>{patientDetails?.nhsNumber}</SummaryList.Value>
+            </SummaryList.Row>
+            <SummaryList.Row>
+                <SummaryList.Key>Surname</SummaryList.Key>
+                <SummaryList.Value>{patientDetails?.familyName}</SummaryList.Value>
+            </SummaryList.Row>
+            <SummaryList.Row>
+                <SummaryList.Key>First name</SummaryList.Key>
+                <SummaryList.Value>{patientDetails?.givenName?.map((name) => `${name} `)}</SummaryList.Value>
+            </SummaryList.Row>
+            <SummaryList.Row>
+                <SummaryList.Key>Date of birth</SummaryList.Key>
+                <SummaryList.Value>{getFormattedDate(patientDetails?.birthDate)}</SummaryList.Value>
+            </SummaryList.Row>
+            <SummaryList.Row>
+                <SummaryList.Key>Postcode</SummaryList.Key>
+                <SummaryList.Value>{patientDetails?.postalCode}</SummaryList.Value>
+            </SummaryList.Row>
+        </SummaryList>
     );
 };
 
--- ui/src/pages/patientTracePage/PatientTracePage.test.jsx
+++ ui/src/pages/patientTracePage/PatientTracePage.test.jsx
@@ -1,162 +1,83 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { PatientTracePage } from "./PatientTracePage";
-import * as ReactRouter from "react-router";
+import { MemoryRouter, useNavigate } from "react-router";
 import "../../apiClients/documentStore";
-import { usePatientDetailsProviderContext } from "../../providers/PatientDetailsProvider";
+import { PatientDetailsProvider } from "../../providers/PatientDetailsProvider";
+import { useDocumentStore } from "../../apiClients/documentStore";
 
-const mockDocumentStore = {
-    getPatientDetails: () => ({}),
-};
-
-jest.mock("../../providers/PatientDetailsProvider", () => ({
-    usePatientDetailsProviderContext: jest.fn(),
-}));
-jest.mock("../../apiClients/documentStore", () => {
-    return { useDocumentStore: () => mockDocumentStore };
+jest.mock("react-router", () => {
+    return {
+        ...jest.requireActual("react-router"),
+        useNavigate: jest.fn(),
+    };
 });
+jest.mock("../../apiClients/documentStore");
 
 describe("<PatientTracePage/>", () => {
-    const fakeNhsNumber = "9000000009";
-    const patientData = {
-        birthDate: "2010-10-22",
-        familyName: "Smith",
-        givenName: ["Jane"],
-        nhsNumber: "9000000009",
-        postalCode: "LS1 6AE",
-        superseded: false,
-    };
-    const patientDetailsResponse = {
-        result: {
-            patientDetails: patientData,
-        },
-    };
-    const mockSetPatientDetails = jest.fn();
-    const mockNavigate = jest.fn();
-    let useNavigateSpy;
-
-    afterEach(() => {
-        jest.clearAllMocks();
-        useNavigateSpy.mockRestore();
-    });
+    const getPatientDetailsMock = jest.fn();
 
     beforeEach(() => {
-        useNavigateSpy = jest.spyOn(ReactRouter, "useNavigate");
-        useNavigateSpy.mockImplementation(() => mockNavigate);
-        usePatientDetailsProviderContext.mockReturnValue([patientData, mockSetPatientDetails]);
+        useDocumentStore.mockReturnValue({ getPatientDetails: getPatientDetailsMock });
     });
 
     it("renders the page", () => {
-        render(<PatientTracePage />);
+        renderPatientTracePage();
 
         expect(screen.getByRole("heading", { name: "Search for patient" })).toBeInTheDocument();
-        expect(screen.queryByText("Enter NHS number")).toBeInTheDocument();
+        expect(screen.getByRole("textbox", { name: "Enter NHS number" })).toBeInTheDocument();
         expect(screen.getByText("A 10-digit number, for example, 485 777 3456")).toBeInTheDocument();
-        expect(screen.queryByRole("button", { name: "Search" })).toBeInTheDocument();
-        expect(screen.queryByRole("button", { name: "Next" })).not.toBeInTheDocument();
+        expect(screen.getByRole("button", { name: "Search" })).toBeInTheDocument();
     });
 
-    it("gets the patient's data when the NHS number is submitted", async () => {
-        mockDocumentStore.getPatientDetails = (nhsNumber) => {
-            if (nhsNumber === fakeNhsNumber) {
-                return patientDetailsResponse;
-            }
-        };
+    it("displays the patient details when their data is found", async () => {
+        const nhsNumber = "9000000000";
+        const familyName = "Smith";
+        const patientDetails = buildPatientDetails({ familyName, nhsNumber });
+        const patientDetailsResponse = { result: { patientDetails } };
 
-        render(<PatientTracePage />);
+        getPatientDetailsMock.mockResolvedValue(patientDetailsResponse);
 
-        enterNhsNumber(fakeNhsNumber);
-        startSearch();
+        renderPatientTracePage();
+        userEvent.type(screen.getByRole("textbox", { name: "Enter NHS number" }), nhsNumber);
+        userEvent.click(screen.getByRole("button", { name: "Search" }));
 
-        await waitFor(() => {
-            expect(screen.queryByText(patientData.familyName)).toBeInTheDocument();
-        });
-    });
-
-    it("displays the patient's details when their demographic data is found", async () => {
-        mockDocumentStore.getPatientDetails = (nhsNumber) => {
-            if (nhsNumber === fakeNhsNumber) {
-                return patientDetailsResponse;
-            }
-        };
-
-        render(<PatientTracePage />);
-
-        enterNhsNumber(fakeNhsNumber);
-        startSearch();
-
-        await waitFor(() => {
-            expect(screen.queryByText(`${patientData.givenName[0]}`)).toBeInTheDocument();
-        });
-
-        expect(screen.getByText("Verify patient details")).toBeInTheDocument();
-        expect(screen.getByText(patientData.nhsNumber)).toBeInTheDocument();
-        expect(screen.queryByText(`${patientData.familyName}`)).toBeInTheDocument();
-        expect(screen.queryByText(`${patientData.postalCode}`)).toBeInTheDocument();
-        expect(screen.queryByText("22 October 2010")).toBeInTheDocument();
-        expect(screen.queryByRole("button", { name: "Next" })).toBeInTheDocument();
-        expect(screen.queryByRole("button", { name: "Search" })).not.toBeInTheDocument();
-        expect(screen.queryByRole("textbox", { name: "Enter NHS number" })).not.toBeInTheDocument();
-        expect(screen.queryByRole("progressbar")).not.toBeInTheDocument();
-    });
-
-    it("stores the patient's details in context when moving to next page", async () => {
-        mockDocumentStore.getPatientDetails = (nhsNumber) => {
-            if (nhsNumber === fakeNhsNumber) {
-                return patientDetailsResponse;
-            }
-        };
-
-        render(<PatientTracePage />);
-
-        enterNhsNumber(fakeNhsNumber);
-        startSearch();
-        await waitFor(() => {
-            expect(screen.queryByText(`${patientData.givenName[0]}`)).toBeInTheDocument();
-        });
-        clickNext();
-
-        await waitFor(() => {
-            expect(mockSetPatientDetails).toBeCalledWith(patientData);
-        });
+        expect(await screen.findByRole("heading", { name: "Verify patient details" })).toBeInTheDocument();
+        expect(screen.getByText(familyName)).toBeInTheDocument();
+        expect(screen.getByRole("button", { name: "Next" })).toBeInTheDocument();
     });
 
     it("navigates to specified page when moving to next page", async () => {
+        const nhsNumber = "9000000000";
+        const patientDetails = buildPatientDetails({ nhsNumber });
+        const patientDetailsResponse = { result: { patientDetails } };
+        const mockNavigate = jest.fn();
         const expectedNextPage = "test/submit";
-        mockDocumentStore.getPatientDetails = (nhsNumber) => {
-            if (nhsNumber === fakeNhsNumber) {
-                return patientDetailsResponse;
-            }
-        };
-        render(<PatientTracePage nextPage={expectedNextPage} />);
 
-        enterNhsNumber(fakeNhsNumber);
-        startSearch();
-        await waitFor(() => {
-            expect(nextButton()).toBeInTheDocument();
-        });
-        clickNext();
+        useNavigate.mockImplementation(() => mockNavigate);
+        getPatientDetailsMock.mockResolvedValue(patientDetailsResponse);
 
-        await waitFor(() => {
-            expect(mockNavigate).toHaveBeenCalledWith(expectedNextPage);
-        });
+        renderPatientTracePage({ nextPage: expectedNextPage });
+        userEvent.type(screen.getByRole("textbox", { name: "Enter NHS number" }), nhsNumber);
+        userEvent.click(screen.getByRole("button", { name: "Search" }));
+        userEvent.click(await screen.findByRole("button", { name: "Next" }));
+
+        expect(mockNavigate).toHaveBeenCalledWith(expectedNextPage);
     });
 
     it("displays text specific to upload path if user has selected upload", async () => {
+        const nhsNumber = "9000000000";
+        const patientDetails = buildPatientDetails({ nhsNumber });
+        const patientDetailsResponse = { result: { patientDetails } };
         const expectedNextPage = "upload";
-        mockDocumentStore.getPatientDetails = (nhsNumber) => {
-            if (nhsNumber === fakeNhsNumber) {
-                return patientDetailsResponse;
-            }
-        };
-        render(<PatientTracePage nextPage={expectedNextPage} />);
 
-        enterNhsNumber(fakeNhsNumber);
-        startSearch();
-        await waitFor(() => {
-            expect(nextButton()).toBeInTheDocument();
-        });
-        clickNext();
+        getPatientDetailsMock.mockResolvedValue(patientDetailsResponse);
+        useNavigate.mockImplementation(() => jest.fn());
+
+        renderPatientTracePage({ nextPage: expectedNextPage });
+        userEvent.type(screen.getByRole("textbox", { name: "Enter NHS number" }), nhsNumber);
+        userEvent.click(screen.getByRole("button", { name: "Search" }));
+        userEvent.click(await screen.findByRole("button", { name: "Next" }));
 
         expect(
             screen.getByText(
@@ -166,21 +87,17 @@ describe("<PatientTracePage/>", () => {
     });
 
     it("doesn't display text specific to upload path if user has selected download", async () => {
+        const nhsNumber = "9000000000";
+        const patientDetailsResponse = { result: { patientDetails: buildPatientDetails({ nhsNumber }) } };
         const expectedNextPage = "download";
-        mockDocumentStore.getPatientDetails = (nhsNumber) => {
-            if (nhsNumber === fakeNhsNumber) {
-                return patientDetailsResponse;
-            }
-        };
 
-        render(<PatientTracePage nextPage={expectedNextPage} />);
+        useNavigate.mockImplementation(() => jest.fn());
+        getPatientDetailsMock.mockResolvedValue(patientDetailsResponse);
 
-        enterNhsNumber(fakeNhsNumber);
-        startSearch();
-        await waitFor(() => {
-            expect(nextButton()).toBeInTheDocument();
-        });
-        clickNext();
+        renderPatientTracePage({ nextPage: expectedNextPage });
+        userEvent.type(screen.getByRole("textbox", { name: "Enter NHS number" }), nhsNumber);
+        userEvent.click(screen.getByRole("button", { name: "Search" }));
+        userEvent.click(await screen.findByRole("button", { name: "Next" }));
 
         expect(
             screen.queryByText(
@@ -189,141 +106,124 @@ describe("<PatientTracePage/>", () => {
         ).not.toBeInTheDocument();
     });
 
-    it("displays a loading spinner when the patient's details are being requested", async () => {
-        mockDocumentStore.getPatientDetails = (nhsNumber) => {
-            if (nhsNumber === fakeNhsNumber) {
-                return [];
-            }
-        };
-        render(<PatientTracePage />);
+    it("displays a loading spinner when the patients details are being requested", async () => {
+        getPatientDetailsMock.mockResolvedValue([]);
 
-        userEvent.type(screen.getByLabelText("Enter NHS number"), fakeNhsNumber);
-        startSearch();
+        renderPatientTracePage();
+        userEvent.type(screen.getByRole("textbox", { name: "Enter NHS number" }), "9000000009");
+        userEvent.click(screen.getByRole("button", { name: "Search" }));
 
         await waitFor(() => {
             expect(screen.getByRole("progressbar")).toBeInTheDocument();
         });
+        expect(screen.queryByRole("progressbar")).not.toBeInTheDocument();
     });
 
-    it("displays a message when nhs number is superseded", async () => {
-        const patientDetails = {
-            birthDate: "2010-10-22",
-            familyName: "Smith",
-            givenName: ["Steve"],
-            nhsNumber: "9000000012",
-            postalCode: "LS1 6AE",
-            superseded: true,
-        };
+    it("displays a message when NHS number is superseded", async () => {
+        const nhsNumber = "9000000000";
+        const supersededNhsNumber = "9000000012";
+        const patientDetails = buildPatientDetails({ superseded: true, nhsNumber });
+        const patientDetailsResponse = { result: { patientDetails } };
 
-        usePatientDetailsProviderContext.mockReturnValue([patientDetails, mockSetPatientDetails]);
+        getPatientDetailsMock.mockResolvedValue(patientDetailsResponse);
 
-        mockDocumentStore.getPatientDetails = () => {
-            return {
-                result: { patientDetails },
-            };
-        };
-
-        render(<PatientTracePage />);
-
-        userEvent.type(screen.getByLabelText("Enter NHS number"), fakeNhsNumber);
-        startSearch();
+        renderPatientTracePage();
+        userEvent.type(screen.getByRole("textbox", { name: "Enter NHS number" }), supersededNhsNumber);
+        userEvent.click(screen.getByRole("button", { name: "Search" }));
 
         expect(await screen.findByText("The NHS number for this patient has changed."));
     });
 
     it("displays an error message when the form is submitted and the NHS number is missing", async () => {
-        render(<PatientTracePage />);
+        renderPatientTracePage();
+        userEvent.click(screen.getByRole("button", { name: "Search" }));
 
-        startSearch();
-
-        await waitFor(() => {
-            expect(screen.getByText("Please enter a 10 digit NHS number")).toBeInTheDocument();
-        });
+        expect(await screen.findByText("Please enter a 10 digit NHS number")).toBeInTheDocument();
     });
 
     it.each([["123456789"], ["12345678901"], ["123456789A"]])(
         "displays an error message when the form is submitted and the NHS number is '%s''",
         async (nhsNumber) => {
-            render(<PatientTracePage />);
+            renderPatientTracePage();
+            userEvent.type(screen.getByRole("textbox", { name: "Enter NHS number" }), nhsNumber);
+            userEvent.click(screen.getByRole("button", { name: "Search" }));
 
-            enterNhsNumber(nhsNumber);
-            startSearch();
-
-            await waitFor(() => {
-                expect(screen.getByText("Please enter a 10 digit NHS number")).toBeInTheDocument();
-            });
+            expect(await screen.findByText("Please enter a 10 digit NHS number")).toBeInTheDocument();
         }
     );
 
-    it("displays an error message when there is a problem retrieving the patient's details", async () => {
-        mockDocumentStore.getPatientDetails = () => {
-            throw Error("Error");
-        };
-        render(<PatientTracePage />);
+    it("displays an error message when there is a problem retrieving the patient details", async () => {
+        getPatientDetailsMock.mockRejectedValue(new Error("Problem retrieving patient details"));
 
-        enterNhsNumber("0987654321");
-        startSearch();
+        renderPatientTracePage();
+        userEvent.type(screen.getByRole("textbox", { name: "Enter NHS number" }), "0987654321");
+        userEvent.click(screen.getByRole("button", { name: "Search" }));
 
-        await waitFor(() => {
-            expect(screen.getByRole("alert")).toBeInTheDocument();
-        });
+        expect(await screen.findByRole("alert")).toBeInTheDocument();
+        expect(screen.getByRole("button", { name: "Search" })).toBeInTheDocument();
         expect(screen.queryByRole("button", { name: "Next" })).not.toBeInTheDocument();
-        expect(screen.queryByRole("button", { name: "Search" })).toBeInTheDocument();
     });
 
     it("displays a message when no patient details are found", async () => {
-        const fakeNhsNumber = "0987654321";
         const errorResponse = {
-            status: 404,
-            message: "404 Patient not found.",
+            response: {
+                status: 404,
+                message: "404 Patient not found.",
+            },
         };
-        mockDocumentStore.getPatientDetails = () => {
-            throw { response: errorResponse };
-        };
-        render(<PatientTracePage />);
 
-        enterNhsNumber(fakeNhsNumber);
-        startSearch();
+        getPatientDetailsMock.mockRejectedValue(errorResponse);
 
-        await waitFor(() => {
-            expect(screen.getByText("Patient Not Found")).toBeInTheDocument();
-        });
+        renderPatientTracePage();
+        userEvent.type(screen.getByRole("textbox", { name: "Enter NHS number" }), "0987654321");
+        userEvent.click(screen.getByRole("button", { name: "Search" }));
+
+        expect(await screen.findByText("Patient Not Found")).toBeInTheDocument();
     });
 
-    it("displays a message when nhs number is invalid", async () => {
-        const fakeNhsNumber = "9000000000";
+    it("displays a message when NHS number is invalid", async () => {
         const errorResponse = {
-            status: 400,
-            message: "400 Invalid NHS number.",
+            response: {
+                status: 400,
+                message: "400 Invalid NHS number.",
+            },
         };
-        mockDocumentStore.getPatientDetails = () => {
-            throw { response: errorResponse };
-        };
-        render(<PatientTracePage />);
 
-        enterNhsNumber(fakeNhsNumber);
-        startSearch();
+        getPatientDetailsMock.mockRejectedValue(errorResponse);
 
-        await waitFor(() => {
-            expect(
-                screen.getByText("The NHS number provided is invalid. Please check the number you have entered.")
-            ).toBeInTheDocument();
-        });
+        renderPatientTracePage();
+        userEvent.type(screen.getByRole("textbox", { name: "Enter NHS number" }), "9000000000");
+        userEvent.click(screen.getByRole("button", { name: "Search" }));
+
+        expect(
+            await screen.findByText("The NHS number provided is invalid. Please check the number you have entered.")
+        ).toBeInTheDocument();
     });
 });
 
-function clickNext() {
-    userEvent.click(screen.queryByRole("button", { name: "Next" }));
-}
+const renderPatientTracePage = (propsOverride) => {
+    const props = {
+        expectedNextPage: "download",
+        ...propsOverride,
+    };
 
-function enterNhsNumber(nhsNumber) {
-    userEvent.type(screen.getByLabelText("Enter NHS number"), nhsNumber);
-}
+    render(
+        <MemoryRouter>
+            <PatientDetailsProvider>
+                <PatientTracePage {...props} />
+            </PatientDetailsProvider>
+        </MemoryRouter>
+    );
+};
 
-function startSearch() {
-    userEvent.click(screen.queryByRole("button", { name: "Search" }));
-}
-
-function nextButton() {
-    return screen.queryByRole("button", { name: "Next" });
-}
+const buildPatientDetails = (patientDetailsOverride) => {
+    return {
+        birthDate: "1970-01-01",
+        familyName: "Default Surname",
+        givenName: ["Default Given Name"],
+        nhsNumber: "0000000000",
+        postalCode: "AA1 1AA",
+        superseded: false,
+        ...patientDetailsOverride,
+    };
+};
