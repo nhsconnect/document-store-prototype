e085c4c7edfd20b0ac4541cebdcd66fd0318e611
Kurtis <kurtis.angell1@nhs.net>
2023-02-24 15:23:39 +0000

    [PRMT-2977] Add redirect uri from query parameter to response header in logout handler
    Co-authored-by: Anusha Doddala <anu.doddala1@nhs.net>
    Co-authored-by: Ryan Brown <ryan.brown15@nhs.net>
    Co-authored-by: Richard Smith <richard.smith33@nhs.net>

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/LogoutHandler.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/LogoutHandler.java
@@ -7,20 +7,31 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static uk.nhs.digital.docstore.authoriser.LoginHandler.LOGGER;
+import java.util.Map;
 
 public class LogoutHandler
         implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     public static final Logger LOGGER = LoggerFactory.getLogger(LogoutHandler.class);
     public static final int SEE_OTHER_STATUS_CODE = 303;
+
     @Override
     public APIGatewayProxyResponseEvent handleRequest(
             APIGatewayProxyRequestEvent input, Context context) {
 
+        var queryStringParameters = input.getQueryStringParameters();
+        var headers = Map.of("Location", queryStringParameters.get("redirect_uri"));
+
         var response = new APIGatewayProxyResponseEvent();
+
         response.setStatusCode(SEE_OTHER_STATUS_CODE);
+        response.setHeaders(headers);
         response.setBody("");
         response.setIsBase64Encoded(false);
+
         return response;
     }
 }
+
+//Location header
+//cookie
+//delete the session id
--- authoriser/src/test/java/uk/nhs/digital/docstore/authoriser/LogoutHandlerTest.java
+++ authoriser/src/test/java/uk/nhs/digital/docstore/authoriser/LogoutHandlerTest.java
@@ -2,33 +2,34 @@ package uk.nhs.digital.docstore.authoriser;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.nimbusds.oauth2.sdk.ResponseType;
-import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import uk.nhs.digital.docstore.authoriser.stubs.InMemorySessionStore;
 
 import java.util.Map;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LogoutHandlerTest {
     @Test
-    public void removeExistingSessionIdFromSessionStore(){
+    public void removeExistingSessionIdFromSessionStore() {
         var request = new APIGatewayProxyRequestEvent();
         var sessionID = UUID.randomUUID();
         LogoutHandler handler = new LogoutHandler();
         request.setHeaders(Map.of(
                 "Cookie",
-                "SessionId="+sessionID
+                "SessionId=" + sessionID
         ));
-
+        String redirectUrl = "some-url";
+        var location = Map.of("redirect_uri", redirectUrl);
+        request.setQueryStringParameters(location);
 
         var response = handler.handleRequest(request, Mockito.mock(Context.class));
-
 
         Assertions.assertThat(response.getBody()).isEqualTo("");
         Assertions.assertThat(response.getIsBase64Encoded()).isFalse();
         Assertions.assertThat(response.getStatusCode()).isEqualTo(303);
+        assertThat(response.getHeaders().get("Location")).isEqualTo(redirectUrl);
     }
 }
