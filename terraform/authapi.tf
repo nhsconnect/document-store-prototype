d687bf9b29ececcf580208f8991e9817d6ec4c41
Richard Smith <richard.smith33@nhs.net>
2023-02-24 10:16:07 +0000

    [PRMT-2977] LogoutHandler endpoint terraform
    Co-authored-by: Ryan Brown <ryan.brown15@nhs.net>
    Co-authored-by: Anu Doddala <anu.doddala1@nhs.net>
    Co-authored-by: Kurtis <kurtis.angell1@nhs.net>

--- terraform/authapi.tf
+++ terraform/authapi.tf
@@ -40,14 +40,14 @@ resource "aws_lambda_permission" "api_gateway_permission_for_login_redirect_hand
   source_arn    = "${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/GET/${aws_api_gateway_resource.auth_resource.path_part}/${aws_api_gateway_resource.login_resource.path_part}"
 }
 
-resource "aws_api_gateway_method" "proxy_method" {
+resource "aws_api_gateway_method" "login_proxy_method" {
   rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
   resource_id   = aws_api_gateway_resource.login_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
+resource "aws_api_gateway_integration" "login_lambda_integration" {
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
   resource_id = aws_api_gateway_resource.login_resource.id
   http_method = "GET"
@@ -55,4 +55,57 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.login_redirect_lambda.invoke_arn
+}
+
+# logout infra
+
+resource "aws_lambda_function" "logout_lambda" {
+  handler          = "uk.nhs.digital.docstore.authoriser.LogoutHandler::handleRequest"
+  function_name    = "LogoutHandler"
+  runtime          = "java11"
+  role             = aws_iam_role.authoriser_execution_role.arn
+  timeout          = 15
+  memory_size      = 256
+  filename         = var.authoriser_lambda_jar_filename
+  source_code_hash = filebase64sha256(var.authoriser_lambda_jar_filename)
+  layers           = [
+    "arn:aws:lambda:eu-west-2:580247275435:layer:LambdaInsightsExtension:21"
+  ]
+  environment {
+    variables = {
+      DYNAMODB_ENDPOINT  = var.dynamodb_endpoint
+    }
+  }
+}
+
+resource "aws_api_gateway_resource" "logout_resource" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  parent_id   = aws_api_gateway_resource.auth_resource.id
+  path_part   = "Logout"
+}
+
+resource "aws_lambda_permission" "api_gateway_permission_for_logout_handler" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.logout_lambda.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/GET/${aws_api_gateway_resource.auth_resource.path_part}/${aws_api_gateway_resource.logout_resource.path_part}"
+}
+
+resource "aws_api_gateway_method" "logout_proxy_method" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+  resource_id   = aws_api_gateway_resource.logout_resource.id
+  http_method   = "GET"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.cis2_authoriser.id
+}
+
+resource "aws_api_gateway_integration" "logout_lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.logout_resource.id
+  http_method = "GET"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.logout_lambda.invoke_arn
 }
