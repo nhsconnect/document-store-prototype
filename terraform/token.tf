2ff60ff64395cfc5e39ea7e50ad49a2ff608f44e
Nicholas Hill <nick.hill3@nhs.net>
2023-03-01 09:33:27 +0000

    [TECH] Add http_method to token_request_lambda_integration

--- terraform/token.tf
+++ terraform/token.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "token_request_lambda" {
   ]
   environment {
     variables = {
-      DYNAMODB_ENDPOINT  = var.dynamodb_endpoint
+      DYNAMODB_ENDPOINT = var.dynamodb_endpoint
     }
   }
 }
@@ -42,7 +42,7 @@ resource "aws_api_gateway_method" "token_request_proxy_method" {
 resource "aws_api_gateway_integration" "token_request_lambda_integration" {
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
   resource_id = aws_api_gateway_resource.token_request_resource.id
-  http_method = "GET"
+  http_method = aws_api_gateway_method.token_request_proxy_method.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
