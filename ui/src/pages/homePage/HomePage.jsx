073feffc81c1249781c47920fffcda3eb019fd26
Nicholas Hill <nick.hill3@nhs.net>
2023-03-01 10:54:01 +0000

    [BUGFIX] Disable continue button on HomePage until an option has been selected

--- ui/src/pages/homePage/HomePage.jsx
+++ ui/src/pages/homePage/HomePage.jsx
@@ -5,37 +5,47 @@ import { useNavigate } from "react-router";
 import BackButton from "../../components/backButton/BackButton";
 
 const HomePage = () => {
-    const { register, handleSubmit } = useForm();
-    let navigate = useNavigate();
+    const { register, handleSubmit, formState, getFieldState } = useForm();
+    const navigate = useNavigate();
 
-    const { ref: trxRef, ...trxProps } = register("trx");
+    const { ref: isDownloadOrUploadRef, ...isDownloadOrUploadProps } = register("isDownloadOrUpload");
+    const { isDirty: isDownloadOrUploadDirty } = getFieldState("isDownloadOrUpload", formState);
 
-    const uploadPathHref = "/upload/patient-trace";
-    const searchPathHref = "/search/patient-trace";
-
-    const doSubmit = async (data) => {
-        const location = data.trx === "download" ? searchPathHref : uploadPathHref;
-        navigate(location, { replace: false });
+    const submit = ({ isDownloadOrUpload }) => {
+        const path = isDownloadOrUpload === "download" ? "search" : "upload";
+        navigate(`/${path}/patient-trace`, { replace: false });
     };
 
     return (
         <>
             <BackButton />
-            <form onSubmit={handleSubmit(doSubmit)}>
+            <form onSubmit={handleSubmit(submit)}>
                 <Fieldset>
                     <Fieldset.Legend headingLevel="h1" isPageHeading>
                         How do you want to use the Document Store?
                     </Fieldset.Legend>
-                    <Radios name="document-store-action" hint="Select an option">
-                        <Radios.Radio id="download" value="download" inputRef={trxRef} {...trxProps}>
+                    <Radios hint="Select an option">
+                        <Radios.Radio
+                            {...isDownloadOrUploadProps}
+                            id="download"
+                            value="download"
+                            inputRef={isDownloadOrUploadRef}
+                        >
                             Download and view a stored document
                         </Radios.Radio>
-                        <Radios.Radio id="upload" value="upload" inputRef={trxRef} {...trxProps}>
+                        <Radios.Radio
+                            {...isDownloadOrUploadProps}
+                            id="upload"
+                            value="upload"
+                            inputRef={isDownloadOrUploadRef}
+                        >
                             Upload a document
                         </Radios.Radio>
                     </Radios>
                 </Fieldset>
-                <Button type="submit">Continue</Button>
+                <Button type="submit" disabled={!isDownloadOrUploadDirty}>
+                    Continue
+                </Button>
             </form>
         </>
     );
--- ui/src/pages/homePage/HomePage.test.jsx
+++ ui/src/pages/homePage/HomePage.test.jsx
@@ -1,25 +1,50 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor, within } from "@testing-library/react";
 import HomePage from "./HomePage";
-import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { useNavigate } from "react-router";
 
-describe("The home page", () => {
-    it("provides a link to the multi step upload path", () => {
-        render(
-            <BrowserRouter>
-                <HomePage />
-            </BrowserRouter>
-        );
+jest.mock("react-router");
 
-        expect(screen.getByText("Upload a document")).toBeInTheDocument();
+describe("<HomePage />", () => {
+    it("renders the page", () => {
+        render(<HomePage />);
+
+        expect(screen.getByText("Back")).toBeInTheDocument();
+        const form = within(screen.getByRole("group", { name: "How do you want to use the Document Store?" }));
+        expect(form.getByRole("heading", { name: "How do you want to use the Document Store?" })).toBeInTheDocument();
+        expect(form.getByText("Select an option")).toBeInTheDocument();
+        expect(form.getByRole("radio", { name: "Upload a document" })).toBeInTheDocument();
+        expect(form.getByRole("radio", { name: "Download and view a stored document" })).toBeInTheDocument();
+        expect(screen.getByRole("button", { name: "Continue" })).toBeInTheDocument();
     });
 
-    it("provides a link to the multi step search path", () => {
-        render(
-            <BrowserRouter>
-                <HomePage />
-            </BrowserRouter>
-        );
+    it("no radio buttons selected by default", () => {
+        render(<HomePage />);
 
-        expect(screen.getByText("Download and view a stored document")).toBeInTheDocument();
+        expect(screen.getByRole("radio", { name: "Upload a document" })).not.toBeChecked();
+        expect(screen.getByRole("radio", { name: "Download and view a stored document" })).not.toBeChecked();
+    });
+
+    it.each([
+        ["upload", "Upload a document"],
+        ["search", "Download and view a stored document"],
+    ])("navigates to %s path when '%s' selected", async (path, radioButtonName) => {
+        const navigateMock = jest.fn();
+
+        useNavigate.mockImplementation(() => navigateMock);
+
+        render(<HomePage />);
+        userEvent.click(screen.getByRole("radio", { name: radioButtonName }));
+        userEvent.click(screen.getByRole("button", { name: "Continue" }));
+
+        await waitFor(() => {
+            expect(navigateMock).toHaveBeenCalledWith(`/${path}/patient-trace`, { replace: false });
+        });
+    });
+
+    it("disables continue button if nothing is selected", async () => {
+        render(<HomePage />);
+
+        expect(screen.getByRole("button", { name: "Continue" })).toBeDisabled();
     });
 });
