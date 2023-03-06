e08ac1966809098caee76eeabe5dfd6071653893
Nicholas Hill <nick.hill3@nhs.net>
2023-03-06 10:17:47 +0000

    [PRMT-2779] Rename AuthCallbackRouter test suite root describe block

--- ui/src/components/authenticator/AuthCallbackRouter.test.jsx
+++ ui/src/components/authenticator/AuthCallbackRouter.test.jsx
@@ -6,41 +6,41 @@ import AuthCallbackRouter from "./AuthCallbackRouter";
 jest.mock("react-router");
 jest.mock("react-oidc-context");
 
-describe("CIS2AuthenticationResultNavigator", () => {
-    it("navigates to home page when authentication is successful", () => {
+describe("<AuthCallbackRouter />", () => {
+    it("navigates to /home when auth is successful", () => {
         useAuth.mockImplementationOnce(() => ({
             isAuthenticated: true,
             isLoading: false,
             error: false,
         }));
-
         Navigate.mockImplementation(() => null);
+
         render(<AuthCallbackRouter />);
 
         expect(Navigate).toBeCalledWith(expect.objectContaining({ to: "/home", replace: true }), expect.anything());
     });
 
-    it("navigates to start page when authentication is unsuccessful", () => {
+    it("navigates to start page when auth is unsuccessful", () => {
         useAuth.mockImplementationOnce(() => ({
             isAuthenticated: false,
             isLoading: false,
             error: true,
         }));
-
         Navigate.mockImplementation(() => null);
+
         render(<AuthCallbackRouter />);
 
         expect(Navigate).toBeCalledWith(expect.objectContaining({ to: "/", replace: true }), expect.anything());
     });
 
-    it("displays loading if authentication is incomplete", () => {
+    it("displays loading if auth is incomplete", () => {
         useAuth.mockImplementationOnce(() => ({
             isAuthenticated: true,
             isLoading: true,
             error: true,
         }));
-
         Navigate.mockImplementation(() => null);
+
         render(<AuthCallbackRouter />);
 
         expect(screen.getByText("Loading...")).toBeInTheDocument();
