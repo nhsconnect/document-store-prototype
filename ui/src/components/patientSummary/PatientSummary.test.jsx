62807f9c4b0305cd2011d82f1aa8263b05a453e9
Nicholas Hill <nick.hill3@nhs.net>
2023-03-06 09:52:26 +0000

    [REFACTOR] Add unit test coverage for summary keys in PatientSummary

--- ui/src/components/patientSummary/PatientSummary.test.jsx
+++ ui/src/components/patientSummary/PatientSummary.test.jsx
@@ -1,22 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import PatientSummary from "./PatientSummary";
 import React from "react";
+import { buildPatientDetails } from "../../utils/testBuilders";
 
-describe("Patient Summary test", () => {
-    const patientData = {
-        birthDate: "2003-01-22",
-        familyName: "Smith",
-        givenName: ["Jane"],
-        nhsNumber: "9234567801",
-        postalCode: "LS1 6AE",
-    };
-
+describe("<PatientSummary />", () => {
     it("renders a summary with patient details", () => {
-        render(<PatientSummary patientDetails={patientData} />);
-        expect(screen.getByText(patientData.nhsNumber)).toBeInTheDocument();
-        expect(screen.getByText(patientData.postalCode)).toBeInTheDocument();
-        expect(screen.getByText(patientData.familyName)).toBeInTheDocument();
+        const birthDate = "2003-01-22";
+        const familyName = "Smith";
+        const givenName = ["Jane"];
+        const nhsNumber = "9234567801";
+        const postalCode = "LS1 6AE";
+        const patientDetails = buildPatientDetails({ birthDate, familyName, givenName, nhsNumber, postalCode });
+
+        render(<PatientSummary patientDetails={patientDetails} />);
+
+        expect(screen.getByText("NHS Number")).toBeInTheDocument();
+        expect(screen.getByText(nhsNumber)).toBeInTheDocument();
+        expect(screen.getByText("Surname")).toBeInTheDocument();
+        expect(screen.getByText(familyName)).toBeInTheDocument();
+        expect(screen.getByText("First name")).toBeInTheDocument();
+        expect(screen.getByText(givenName[0])).toBeInTheDocument();
+        expect(screen.getByText("Date of birth")).toBeInTheDocument();
         expect(screen.getByText("22 January 2003")).toBeInTheDocument();
-        expect(screen.getByText(patientData.givenName[0])).toBeInTheDocument();
+        expect(screen.getByText("Postcode")).toBeInTheDocument();
+        expect(screen.getByText(postalCode)).toBeInTheDocument();
     });
 });
