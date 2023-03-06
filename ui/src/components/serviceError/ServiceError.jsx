6dbfd562172a3018fc52a25636e59a42b3d56ed4
Nicholas Hill <nick.hill3@nhs.net>
2023-03-06 09:45:41 +0000

    [REFACTOR] Refactor ServiceError

--- ui/src/components/serviceError/ServiceError.jsx
+++ ui/src/components/serviceError/ServiceError.jsx
@@ -1,19 +1,18 @@
 import { ErrorSummary } from "nhsuk-react-components";
 
 const ServiceError = ({ message }) => {
+    const serviceErrorSummaryId = "service-error-summary";
+
     return (
-        <ErrorSummary aria-labelledby="service-error-summary" role="alert" tabIndex={-1}>
-            <ErrorSummary.Title id="service-error-summary-title">
+        <ErrorSummary aria-labelledby={serviceErrorSummaryId} role="alert" tabIndex={-1}>
+            <ErrorSummary.Title id={serviceErrorSummaryId}>
                 Sorry, the service is currently unavailable.
             </ErrorSummary.Title>
             <ErrorSummary.Body>
                 <p>{message || "Please try again later."}</p>
                 <p>
                     Please check your internet connection. If the issue persists please contact the{" "}
-                    <a href={"https://digital.nhs.uk/about-nhs-digital/contact-us"}>
-                        NHS Digital National Service Desk
-                    </a>
-                    .
+                    <a href="https://digital.nhs.uk/about-nhs-digital/contact-us">NHS Digital National Service Desk</a>.
                 </p>
             </ErrorSummary.Body>
         </ErrorSummary>
--- ui/src/components/serviceError/ServiceError.test.jsx
+++ ui/src/components/serviceError/ServiceError.test.jsx
@@ -1,19 +1,36 @@
 import { render, screen } from "@testing-library/react";
 import ServiceError from "./ServiceError";
 
-describe("ServiceError", () => {
-    it("should render the component", () => {
+describe("<ServiceError />", () => {
+    it("renders the service error", () => {
         render(<ServiceError />);
 
-        expect(screen.getByText("Sorry, the service is currently unavailable.")).toBeInTheDocument();
+        expect(screen.getByRole("alert", { name: "Sorry, the service is currently unavailable." })).toBeInTheDocument();
+        expect(
+            screen.getByRole("heading", { name: "Sorry, the service is currently unavailable." })
+        ).toBeInTheDocument();
         expect(screen.getByText("Please try again later.")).toBeInTheDocument();
+        expect(
+            screen.getByText(/Please check your internet connection. If the issue persists please contact the/)
+        ).toBeInTheDocument();
+        expect(screen.getByRole("link", { name: "NHS Digital National Service Desk" })).toBeInTheDocument();
     });
 
-    it("should display the error message if there is one", () => {
+    it("renders a link to the NHS Digital National Service Desk", () => {
+        render(<ServiceError />);
+
+        expect(screen.getByRole("link", { name: "NHS Digital National Service Desk" })).toHaveAttribute(
+            "href",
+            "https://digital.nhs.uk/about-nhs-digital/contact-us"
+        );
+    });
+
+    it("displays the error message if there is one", () => {
         const message = "Error message";
+
         render(<ServiceError message={message} />);
 
-        expect(screen.getByText("Sorry, the service is currently unavailable.")).toBeInTheDocument();
         expect(screen.getByText(message)).toBeInTheDocument();
+        expect(screen.queryByText("Please try again later.")).not.toBeInTheDocument();
     });
 });
