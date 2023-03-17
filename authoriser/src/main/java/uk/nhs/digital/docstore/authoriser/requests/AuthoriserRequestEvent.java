453da4b2f8159ba7ad8fcd654939dba932d35695
Nicholas Hill <nick.hill3@nhs.net>
2023-03-17 16:02:05 +0000

    [PRMT-2779] Add invalid request handling to AuthoriserRequestEvent

---
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/requests/AuthoriserRequestEvent.java
@@ -0,0 +1,35 @@
+package uk.nhs.digital.docstore.authoriser.requests;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.nimbusds.oauth2.sdk.id.Subject;
+import java.util.*;
+
+public class AuthoriserRequestEvent extends APIGatewayProxyRequestEvent {
+    public Optional<Subject> getSubject() {
+        return getCookie("SubjectClaim").map(Subject::new);
+    }
+
+    public Optional<UUID> getSessionId() {
+        return getCookie("SessionId").map(UUID::fromString);
+    }
+
+    private Optional<String> getCookie(String cookieName) {
+        var headers = getHeaders();
+
+        if (headers == null || headers.get("cookie") == null || headers.get("cookie").isEmpty()) {
+            return Optional.empty();
+        }
+
+        var cookieHeader = headers.get("cookie");
+        var cookies = new HashMap<String, String>();
+
+        Arrays.stream(cookieHeader.split(";"))
+                .forEach(
+                        cookie -> {
+                            var keyValue = cookie.split("=");
+                            cookies.put(keyValue[0].trim(), keyValue[1].trim());
+                        });
+
+        return Optional.ofNullable(cookies.get(cookieName));
+    }
+}
---
+++ authoriser/src/test/java/uk/nhs/digital/docstore/authoriser/requests/AuthoriserRequestEventTest.java
@@ -0,0 +1,102 @@
+package uk.nhs.digital.docstore.authoriser.requests;
+
+import com.nimbusds.oauth2.sdk.id.Subject;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.UUID;
+
+class AuthoriserRequestEventTest {
+    @Test
+    void getsSubjectCookieFromHeaders() {
+        var requestEventSubject = new Subject("some-subject");
+        var headers =
+                Map.of(
+                        "cookie",
+                        "SessionId=some-session-id; SubjectClaim="
+                                + requestEventSubject.getValue());
+        var authoriserRequestEvent = new AuthoriserRequestEvent();
+        authoriserRequestEvent.setHeaders(headers);
+
+        var subject = authoriserRequestEvent.getSubject();
+
+        Assertions.assertThat(subject).isPresent();
+        Assertions.assertThat(subject.get().getValue()).isEqualTo(requestEventSubject.getValue());
+    }
+
+    @Test
+    void returnsNullableIfNoSubjectCookieFound() {
+        var headers = Map.of("cookie", "SessionId=some-session-id;");
+        var authoriserRequestEvent = new AuthoriserRequestEvent();
+        authoriserRequestEvent.setHeaders(headers);
+
+        var subject = authoriserRequestEvent.getSubject();
+
+        Assertions.assertThat(subject).isNotPresent();
+    }
+
+    @Test
+    void getsSessionIdCookieFromHeaders() {
+        var requestEventSessionId = UUID.randomUUID();
+        var headers =
+                Map.of(
+                        "cookie",
+                        "SessionId=" + requestEventSessionId + "; SubjectClaim=some-subject;");
+        var authoriserRequestEvent = new AuthoriserRequestEvent();
+        authoriserRequestEvent.setHeaders(headers);
+
+        var sessionId = authoriserRequestEvent.getSessionId();
+
+        Assertions.assertThat(sessionId).isPresent();
+        Assertions.assertThat(sessionId.get()).isEqualTo(requestEventSessionId);
+    }
+
+    @Test
+    void returnsNullableIfNoSessionIdCookieFound() {
+        var headers = Map.of("cookie", "SubjectClaim=some-subject;");
+        var authoriserRequestEvent = new AuthoriserRequestEvent();
+        authoriserRequestEvent.setHeaders(headers);
+
+        var sessionId = authoriserRequestEvent.getSessionId();
+
+        Assertions.assertThat(sessionId).isNotPresent();
+    }
+
+    @Test
+    void returnsNullableIfThereAreNoHeaders() {
+        var authoriserRequestEvent = new AuthoriserRequestEvent();
+
+        var subject = authoriserRequestEvent.getSubject();
+        var sessionId = authoriserRequestEvent.getSessionId();
+
+        Assertions.assertThat(subject).isNotPresent();
+        Assertions.assertThat(sessionId).isNotPresent();
+    }
+
+    @Test
+    void returnsNullableIfThereAreNoCookies() {
+        var headers = Map.of("some-header", "some-header-value");
+        var authoriserRequestEvent = new AuthoriserRequestEvent();
+        authoriserRequestEvent.setHeaders(headers);
+
+        var subject = authoriserRequestEvent.getSubject();
+        var sessionId = authoriserRequestEvent.getSessionId();
+
+        Assertions.assertThat(subject).isNotPresent();
+        Assertions.assertThat(sessionId).isNotPresent();
+    }
+
+    @Test
+    void returnsNullableIfCookiesAreEmpty() {
+        var headers = Map.of("cookie", "");
+        var authoriserRequestEvent = new AuthoriserRequestEvent();
+        authoriserRequestEvent.setHeaders(headers);
+
+        var subject = authoriserRequestEvent.getSubject();
+        var sessionId = authoriserRequestEvent.getSessionId();
+
+        Assertions.assertThat(subject).isNotPresent();
+        Assertions.assertThat(sessionId).isNotPresent();
+    }
+}
