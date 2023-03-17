28a66e2ef85735081985c6bc284508530834bd27
Nicholas Hill <nick.hill3@nhs.net>
2023-03-17 17:04:36 +0000

    [PRMT-2779] Add session store to AuthoriserHandler

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/AuthoriserHandler.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/AuthoriserHandler.java
@@ -8,21 +8,36 @@ import uk.nhs.digital.docstore.authoriser.requests.AuthoriserRequestEvent;
 
 public class AuthoriserHandler
         implements RequestHandler<AuthoriserRequestEvent, IamPolicyResponse> {
+    private final SessionStore sessionStore;
+
+    public AuthoriserHandler(SessionStore sessionStore) {
+        this.sessionStore = sessionStore;
+    }
 
     @Override
     public IamPolicyResponse handleRequest(AuthoriserRequestEvent requestEvent, Context context) {
-        IamPolicyResponse.Statement statement;
+        var sessionId = requestEvent.getSessionId();
+        var subject = requestEvent.getSubject();
+        var policyDocumentBuilder = IamPolicyResponse.PolicyDocument.builder();
+        var iamPolicyResponse = new IamPolicyResponse();
 
-        if (requestEvent.getSessionId().isPresent() && requestEvent.getSubject().isPresent()) {
-            statement = IamPolicyResponse.allowStatement("*");
-        } else {
-            statement = IamPolicyResponse.denyStatement("*");
+        if (sessionId.isPresent() && subject.isPresent()) {
+            var session = sessionStore.load(subject.get(), sessionId.get());
+
+            if (session.isPresent()) {
+                var statement = IamPolicyResponse.allowStatement("*");
+                var policyDocument =
+                        policyDocumentBuilder.withStatement(List.of(statement)).build();
+
+                iamPolicyResponse.setPolicyDocument(policyDocument);
+
+                return iamPolicyResponse;
+            }
         }
 
-        var policyDocumentBuilder = IamPolicyResponse.PolicyDocument.builder();
+        var statement = IamPolicyResponse.denyStatement("*");
         var policyDocument = policyDocumentBuilder.withStatement(List.of(statement)).build();
 
-        var iamPolicyResponse = new IamPolicyResponse();
         iamPolicyResponse.setPolicyDocument(policyDocument);
 
         return iamPolicyResponse;
--- authoriser/src/test/java/uk/nhs/digital/docstore/authoriser/AuthoriserHandlerTest.java
+++ authoriser/src/test/java/uk/nhs/digital/docstore/authoriser/AuthoriserHandlerTest.java
@@ -5,11 +5,16 @@ import static org.mockito.Mockito.mock;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.IamPolicyResponse;
+import com.nimbusds.oauth2.sdk.id.Subject;
+import com.nimbusds.openid.connect.sdk.claims.SessionID;
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
+import uk.nhs.digital.docstore.authoriser.models.Session;
 import uk.nhs.digital.docstore.authoriser.requests.AuthoriserRequestEvent;
+import uk.nhs.digital.docstore.authoriser.stubs.InMemorySessionStore;
 
 class AuthoriserHandlerTest {
     @Test
@@ -21,7 +26,7 @@ class AuthoriserHandlerTest {
                         .build();
         var expectedIamPolicyResponse = new IamPolicyResponse();
         expectedIamPolicyResponse.setPolicyDocument(policyDocument);
-        var authoriserHandler = new AuthoriserHandler();
+        var authoriserHandler = new AuthoriserHandler(new InMemorySessionStore());
 
         var actualIamPolicyResponse =
                 authoriserHandler.handleRequest(new AuthoriserRequestEvent(), mock(Context.class));
@@ -42,7 +47,7 @@ class AuthoriserHandlerTest {
         expectedIamPolicyResponse.setPolicyDocument(policyDocument);
         var requestEvent = new AuthoriserRequestEvent();
         requestEvent.setHeaders(Map.of("cookie", "Subject=some-subject;"));
-        var authoriserHandler = new AuthoriserHandler();
+        var authoriserHandler = new AuthoriserHandler(new InMemorySessionStore());
 
         var actualIamPolicyResponse =
                 authoriserHandler.handleRequest(requestEvent, mock(Context.class));
@@ -63,7 +68,7 @@ class AuthoriserHandlerTest {
         expectedIamPolicyResponse.setPolicyDocument(policyDocument);
         var requestEvent = new AuthoriserRequestEvent();
         requestEvent.setHeaders(Map.of("cookie", "SessionId=" + UUID.randomUUID() + ";"));
-        var authoriserHandler = new AuthoriserHandler();
+        var authoriserHandler = new AuthoriserHandler(new InMemorySessionStore());
 
         var actualIamPolicyResponse =
                 authoriserHandler.handleRequest(requestEvent, mock(Context.class));
@@ -74,7 +79,7 @@ class AuthoriserHandlerTest {
     }
 
     @Test
-    void respondsWithIamAllowPolicyWhenSessionIdAndSubjectCookieIsPresent() {
+    void respondsWithIamAllowPolicyWhenSessionIsPresent() {
         var policyDocumentBuilder = IamPolicyResponse.PolicyDocument.builder();
         var policyDocument =
                 policyDocumentBuilder
@@ -83,11 +88,43 @@ class AuthoriserHandlerTest {
         var expectedIamPolicyResponse = new IamPolicyResponse();
         expectedIamPolicyResponse.setPolicyDocument(policyDocument);
         var requestEvent = new AuthoriserRequestEvent();
+        var sessionId = UUID.randomUUID();
+        var subject = new Subject();
+        requestEvent.setHeaders(
+                Map.of("cookie", "SessionId=" + sessionId + "; SubjectClaim=" + subject + ";"));
+        var inMemorySessionStore = new InMemorySessionStore();
+        var authoriserHandler = new AuthoriserHandler(inMemorySessionStore);
+        var session =
+                Session.create(
+                        sessionId,
+                        Instant.ofEpochSecond(1L),
+                        subject,
+                        new SessionID("some-session-id"));
+
+        inMemorySessionStore.save(session);
+        var actualIamPolicyResponse =
+                authoriserHandler.handleRequest(requestEvent, mock(Context.class));
+
+        assertThat(actualIamPolicyResponse.getPolicyDocument())
+                .usingRecursiveComparison()
+                .isEqualTo(expectedIamPolicyResponse.getPolicyDocument());
+    }
+
+    @Test
+    void respondsWithIamDenyPolicyWhenSessionIsNotPresent() {
+        var policyDocumentBuilder = IamPolicyResponse.PolicyDocument.builder();
+        var policyDocument =
+                policyDocumentBuilder
+                        .withStatement(List.of(IamPolicyResponse.denyStatement("*")))
+                        .build();
+        var expectedIamPolicyResponse = new IamPolicyResponse();
+        expectedIamPolicyResponse.setPolicyDocument(policyDocument);
+        var requestEvent = new AuthoriserRequestEvent();
         requestEvent.setHeaders(
                 Map.of(
                         "cookie",
                         "SessionId=" + UUID.randomUUID() + "; SubjectClaim=some-subject;"));
-        var authoriserHandler = new AuthoriserHandler();
+        var authoriserHandler = new AuthoriserHandler(new InMemorySessionStore());
 
         var actualIamPolicyResponse =
                 authoriserHandler.handleRequest(requestEvent, mock(Context.class));
