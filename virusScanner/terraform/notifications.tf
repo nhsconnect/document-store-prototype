4f9629694b481cd0b415a82dac59e023b465537f
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-03-17 14:15:42 +0000

    [PRMT-3059]
    Adding SNS subscription so the lambda can receive and react to events from the virus scanner process
    
    Co-authored-by: romanenhs <romane.castera1@nhs.net>
    Co-authored-by: RachelHowellNHS <rachel.howell6@nhs.net>
    Co-authored-by: Anu Doddala <anu.doddala1@nhs.net>

--- virusScanner/terraform/notifications.tf
+++ virusScanner/terraform/notifications.tf
@@ -3,7 +3,14 @@ data "aws_cloudformation_export" "proactive_notifications_sns_topic" {
 }
 
 resource "aws_sns_topic_subscription" "proactive_notifications_sns_topic_subscription" {
-  endpoint = data.aws_ssm_parameter.cloud_security_email.value
-  protocol = "email"
+  endpoint  = data.aws_ssm_parameter.cloud_security_email.value
+  protocol  = "email"
   topic_arn = data.aws_cloudformation_export.proactive_notifications_sns_topic.value
 }
+
+resource "aws_sns_topic_subscription" "virus_scanned_lambda_topic_subscription" {
+  endpoint  = aws_lambda_function.virus_scanned_event_lambda.arn
+  protocol  = "lambda"
+  topic_arn = data.aws_cloudformation_export.proactive_notifications_sns_topic.value
+}
+
