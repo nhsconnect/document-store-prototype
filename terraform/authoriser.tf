f2b58ea0b5c1b0419d7dd6e8093588f60b403ddd
Richard Smith <richard.smith33@nhs.net>
2023-03-21 15:03:39 +0000

    [PRMT-2779] specify auth type for CIS2 authoriser
    Co-authored-by: Nicholas Hill <nick.hill3@nhs.net>
    Co-authored-by: Ryan Brown <ryan.brown15@nhs.net>

--- terraform/authoriser.tf
+++ terraform/authoriser.tf
@@ -57,6 +57,7 @@ resource "aws_api_gateway_authorizer" "cis2_authoriser" {
   rest_api_id            = aws_api_gateway_rest_api.lambda_api.id
   authorizer_uri         = aws_lambda_function.authoriser_lambda.invoke_arn
   authorizer_credentials = aws_iam_role.authoriser_execution.arn
+  type                   = "REQUEST"
 }
 
 resource "aws_lambda_function" "authoriser_lambda" {
