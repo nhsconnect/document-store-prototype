c3fda82632a7181a9a2ebb903fe32ce36423a754
Richard Smith <richard.smith33@nhs.net>
2023-03-21 15:16:21 +0000

    [PRMT-2779] Default constructor for back channel logout handler
    Co-authored-by: Nicholas Hill <nick.hill3@nhs.net>
    Co-authored-by: Ryan Brown <ryan.brown15@nhs.net>

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/BackChannelLogoutHandler.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/BackChannelLogoutHandler.java
@@ -1,10 +1,12 @@
 package uk.nhs.digital.docstore.authoriser.handlers;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.proc.BadJOSEException;
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTParser;
@@ -14,19 +16,35 @@ import com.nimbusds.openid.connect.sdk.validators.LogoutTokenValidator;
 import java.text.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.nhs.digital.docstore.authoriser.DynamoDBSessionStore;
 import uk.nhs.digital.docstore.authoriser.SessionStore;
 
 @SuppressWarnings("unused")
-public class BackChannelLogoutHandler
+public class BackChannelLogoutHandler extends BaseAuthRequestHandler
         implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private final SessionStore sessionStore;
     private final LogoutTokenValidator tokenValidator;
     public static final Logger LOGGER = LoggerFactory.getLogger(BackChannelLogoutHandler.class);
 
+    public BackChannelLogoutHandler() {
+        this(
+                getLogoutTokenValidator(),
+                new DynamoDBSessionStore(new DynamoDBMapper(getDynamodbClient())));
+    }
+
     public BackChannelLogoutHandler(
             LogoutTokenValidator tokenValidator, SessionStore sessionStore) {
         this.sessionStore = sessionStore;
         this.tokenValidator = tokenValidator;
+    }
+
+    private static LogoutTokenValidator getLogoutTokenValidator() {
+        var providerMetadata = getProviderMetadata();
+        return new LogoutTokenValidator(
+                providerMetadata.getIssuer(),
+                getClientInformation().getID(),
+                JWSAlgorithm.RS256,
+                providerMetadata.getJWKSet());
     }
 
     @Override
