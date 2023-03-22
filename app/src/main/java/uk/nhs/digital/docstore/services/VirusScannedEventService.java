082d0c346c3d5baf2dfbca0e92915f90e05ec43c
Anu Doddala <anu.doddala1@nhs.net>
2023-03-22 15:46:38 +0000

    [PRMT-3059] Saving virus Scan result in DynamoDB
    Co-authored-by: Romane Castera <romane.castera1@nhs.net>

--- app/src/main/java/uk/nhs/digital/docstore/services/VirusScannedEventService.java
+++ app/src/main/java/uk/nhs/digital/docstore/services/VirusScannedEventService.java
@@ -1,5 +1,7 @@
 package uk.nhs.digital.docstore.services;
 
+import java.time.Clock;
+import java.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.nhs.digital.docstore.data.repository.DocumentMetadataStore;
@@ -14,13 +16,28 @@ public class VirusScannedEventService {
 
     private final DocumentMetadataSerialiser metadataSerialiser;
 
+    private final Clock clock;
+
     public VirusScannedEventService(
-            DocumentMetadataStore metadataStore, DocumentMetadataSerialiser metadataSerialiser) {
+            DocumentMetadataStore metadataStore,
+            DocumentMetadataSerialiser metadataSerialiser,
+            Clock clock) {
         this.metadataStore = metadataStore;
         this.metadataSerialiser = metadataSerialiser;
+        this.clock = clock;
     }
 
     public void updateVirusScanResult(DocumentLocation location, String scanResult) {
         var metadata = metadataStore.getByLocation(location);
+        if (metadata != null) {
+            metadata.setDocumentUploaded(true);
+            metadata.setIndexed(Instant.now(clock).toString());
+            metadata.setVirusScanResult(scanResult);
+
+            LOGGER.info(
+                    "Updating DocumentReference {} to uploaded and adding virusScan result",
+                    metadata.getId());
+            metadataStore.save(metadata);
+        }
     }
 }
--- app/src/test/java/uk/nhs/digital/docstore/services/VirusScannedEventServiceTest.java
+++ app/src/test/java/uk/nhs/digital/docstore/services/VirusScannedEventServiceTest.java
@@ -1,27 +1,30 @@
 package uk.nhs.digital.docstore.services;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
+import java.time.Clock;
 import org.junit.jupiter.api.Test;
 import uk.nhs.digital.docstore.data.repository.DocumentMetadataStore;
 import uk.nhs.digital.docstore.data.serialiser.DocumentMetadataSerialiser;
+import uk.nhs.digital.docstore.exceptions.IllFormedPatientDetailsException;
+import uk.nhs.digital.docstore.helpers.DocumentMetadataBuilder;
 import uk.nhs.digital.docstore.model.DocumentLocation;
 
 class VirusScannedEventServiceTest {
 
     @Test
-    public void testSavesVirusScanResultsInDynamoDb() {
+    public void testSavesVirusScanResultsInDynamoDb() throws IllFormedPatientDetailsException {
         DocumentMetadataStore metadataStore = mock(DocumentMetadataStore.class);
         DocumentMetadataSerialiser metadataSerialiser = mock(DocumentMetadataSerialiser.class);
         var virusScannedEventService =
-                new VirusScannedEventService(metadataStore, metadataSerialiser);
+                new VirusScannedEventService(metadataStore, metadataSerialiser, Clock.systemUTC());
         DocumentLocation location = new DocumentLocation("s3://test/test");
         String scanResult = "Infected";
+        var metadata = DocumentMetadataBuilder.theMetadata().build();
 
+        when(metadataStore.getByLocation(location)).thenReturn(metadata);
         virusScannedEventService.updateVirusScanResult(location, scanResult);
 
-        verify(metadataStore).getByLocation(location);
+        verify(metadataStore).save(metadata);
     }
 }
