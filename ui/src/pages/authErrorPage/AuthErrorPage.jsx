adcecbd884c52b7199e3b36c5678752f4b8e3ad8
Rio Knightley <rio.knightley2@nhs.net>
2023-03-23 15:43:31 +0000

    [PRMT-2779] Update AuthErrorPage test and component

--- ui/src/pages/authErrorPage/AuthErrorPage.jsx
+++ ui/src/pages/authErrorPage/AuthErrorPage.jsx
@@ -1,4 +1,34 @@
+import { ButtonLink } from "nhsuk-react-components";
+import { useBaseAPIUrl } from "../../providers/ConfigurationProvider";
+
 const AuthErrorPage = () => {
-    return <h1>Authorisation Error</h1>;
+    const baseAPIUrl = useBaseAPIUrl("doc-store-api");
+
+    const timeoutString = "15 minutes";
+
+    return (
+        <>
+            <h1 role="heading">You have been logged out</h1>
+            <p>
+                For security reasons, {"you're"} automatically logged out if you have not used the service for
+                {` ${timeoutString}`}.
+            </p>
+            <p>If you were entering information, it has not been saved and you will need to re-enter it.</p>
+            <p>
+                If the issue persists please contact the{" "}
+                <a
+                    role="link"
+                    href="https://digital.nhs.uk/about-nhs-digital/contact-us#nhs-digital-service-desks"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    NHS National Service Desk
+                </a>
+                .
+            </p>
+
+            <ButtonLink href={`${baseAPIUrl}/Auth/Login`}>Log In</ButtonLink>
+        </>
+    );
 };
 export default AuthErrorPage;
--- ui/src/pages/authErrorPage/AuthErrorPage.test.jsx
+++ ui/src/pages/authErrorPage/AuthErrorPage.test.jsx
@@ -1,10 +1,21 @@
 import AuthErrorPage from "./AuthErrorPage";
 import { render, screen } from "@testing-library/react";
+import { useBaseAPIUrl } from "../../providers/ConfigurationProvider";
+
+jest.mock("../../providers/ConfigurationProvider");
 
 describe("AuthErrorPage", () => {
     it("renders the error page", () => {
+        const baseApiUrl = "https://api.url";
+        const loginHandlerUrl = `${baseApiUrl}/Auth/Login`;
+        const helpDeskUrl = "https://digital.nhs.uk/about-nhs-digital/contact-us#nhs-digital-service-desks";
+        useBaseAPIUrl.mockReturnValue(baseApiUrl);
+
         render(<AuthErrorPage />);
 
-        expect(screen.getByRole("heading", { name: "Authorisation Error" })).toBeInTheDocument();
+        expect(useBaseAPIUrl).toHaveBeenCalledWith("doc-store-api");
+        expect(screen.getByRole("link", { name: "NHS National Service Desk" })).toHaveAttribute("href", helpDeskUrl);
+        expect(screen.getByRole("button", { name: "Log In" })).toHaveAttribute("href", loginHandlerUrl);
+        expect(screen.getByRole("heading", { name: "You have been logged out" })).toBeInTheDocument();
     });
 });
