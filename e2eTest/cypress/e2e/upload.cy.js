ca52c56d1a9c3645ed912f4e87a2d6c0316dcb62
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-03-28 09:13:19 +0100

    [PRMT-3200]
    WIP commit - adding some files, each for a single test
    
    Co-authored-by: Anu Doddala <anu.doddala1@nhs.net>
    # Co-authored-by: romanenhs <romane.castera1@nhs.net>
    # Co-authored-by: RachelHowellNHS <rachel.howell6@nhs.net>

---
+++ e2eTest/cypress/e2e/upload.cy.js
@@ -0,0 +1,57 @@
+import { logAccessibilityViolations } from "../support/utils";
+
+describe("Uploads docs and tests it looks OK", () => {
+    it("searches for a patient using their NHS number and uploads documents", () => {
+        const baseUrl = Cypress.config("baseUrl");
+        const nhsNumber = Cypress.env("REACT_APP_ENV") === "local" ? "9000000009" : "9449305552";
+        const username = Cypress.env("username");
+        const password = Cypress.env("password");
+        const oidcProvider = Cypress.env("oidc_provider");
+        const uploadedFilePathNames = [
+            "cypress/fixtures/test_patient_record.pdf",
+            "cypress/fixtures/test_patient_record_two.pdf",
+        ];
+
+        cy.visit("/");
+        cy.title().should("eq", "Inactive Patient Record Administration");
+        cy.injectAxe();
+        cy.checkA11y(undefined, undefined, logAccessibilityViolations, false);
+        cy.findByRole("button", {name: "Start now"}).click();
+
+        if (oidcProvider === "cis2devoidc") {
+            cy.findByPlaceholderText("User Name").type(username);
+            cy.findByPlaceholderText("Password").type(password);
+            cy.findByRole("button", {name: "Continue"}).click();
+        }
+
+        if (oidcProvider === "COGNITO") {
+            cy.get('input[name="username"]:visible').type(username);
+            cy.get('input[name="password"]:visible').type(password);
+            cy.get('input[name="signInSubmitButton"]:visible').click();
+        }
+
+        cy.url().should("eq", baseUrl + "/home");
+        cy.injectAxe();
+        cy.findByRole("radio", {name: /Upload/}).check();
+        cy.checkA11y(undefined, undefined, logAccessibilityViolations, false);
+        cy.findByRole("button", {name: "Continue"}).click();
+
+        cy.url().should("eq", baseUrl + "/upload/search-patient");
+        cy.findByRole("textbox", {name: "Enter NHS number"}).type(nhsNumber);
+        cy.findByRole("button", {name: "Search"}).click();
+        cy.checkA11y(undefined, undefined, logAccessibilityViolations, false);
+        cy.findByRole("button", {name: "Next"}).click();
+
+        cy.url().should("eq", baseUrl + "/upload/submit");
+        cy.get("input[type=file]").selectFile(uploadedFilePathNames);
+        cy.checkA11y(undefined, undefined, logAccessibilityViolations, false);
+        cy.findByRole("button", {name: "Upload"}).click();
+        cy.findByRole("table", {
+            name: "Successfully uploaded documents",
+        }).within(() => cy.findAllByRole("row").should("have.length", 3));
+        cy.checkA11y(undefined, undefined, logAccessibilityViolations, false);
+        cy.findByRole("button", {name: "Start Again"}).click();
+
+        cy.url().should("eq", Cypress.config("baseUrl") + "/home");
+    });
+});
