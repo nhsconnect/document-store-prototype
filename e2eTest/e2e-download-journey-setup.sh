2c06864f637fdd8716b74b6b56bbc40df9e64896
RachelHowellNHS <rachel.howell6@nhs.net>
2023-03-30 11:25:29 +0100

    [PRMT-3200] Updated e2e setup and teardown scripts
    Co-authored-by: Anu Doddala <anu.doddala1@nhs.net>
    romanenhs <romane.castera1@nhs.net>

---
+++ e2eTest/e2e-download-journey-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ENVIRONMENT=${1:-local}
+
+if [[ -z "$AWS_ENDPOINT" && $ENVIRONMENT == "local" ]]; then
+    AWS_ENDPOINT=http://localhost:4566
+fi
+
+S3_BUCKET_NAME=$(aws ${AWS_ENDPOINT:+--endpoint-url=$AWS_ENDPOINT} s3 ls | awk '{print $3}' | grep test)
+KEY1="file-1"
+KEY2="file-2"
+
+cp uploaded-docs.json.example uploaded-docs.json
+
+sed -i '' -e "s/%s3-location-1%/s3:\/\/${S3_BUCKET_NAME}\/${KEY1}/" uploaded-docs.json
+
+sed -i '' -e "s/%s3-location-2%/s3:\/\/${S3_BUCKET_NAME}\/${KEY2}/" uploaded-docs.json
+
+aws dynamodb batch-write-item --request-items file://uploaded-docs.json ${AWS_ENDPOINT:+--endpoint-url=$AWS_ENDPOINT}
+
+aws s3api put-object --bucket $S3_BUCKET_NAME --key $KEY1 --body content.txt ${AWS_ENDPOINT:+--endpoint-url=$AWS_ENDPOINT}
+
+aws s3api put-object --bucket $S3_BUCKET_NAME --key $KEY2 --body content.txt ${AWS_ENDPOINT:+--endpoint-url=$AWS_ENDPOINT}
--- e2eTest/e2e-teardown.sh
+++ e2eTest/e2e-teardown.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 ENVIRONMENT=${1:-local}
+BUCKET="document-store"
 
 if [[ -z "$AWS_ENDPOINT" && $ENVIRONMENT == "local" ]]; then
     AWS_ENDPOINT=http://localhost:4566
+    BUCKET="test-document-store"
 fi
 
 #Get test bucket
-ENV_S3=$(aws ${AWS_ENDPOINT:+--endpoint-url=$AWS_ENDPOINT} s3 ls | awk '{print $3}' | grep test)
+S3_BUCKET_NAME=$(aws ${AWS_ENDPOINT:+--endpoint-url=$AWS_ENDPOINT} s3 ls | awk '{print $3}' | grep $BUCKET)
 
-aws ${AWS_ENDPOINT:+--endpoint-url=$AWS_ENDPOINT} s3 rm "s3://$ENV_S3" --recursive
+aws ${AWS_ENDPOINT:+--endpoint-url=$AWS_ENDPOINT} s3 rm "s3://$S3_BUCKET_NAME" --recursive
 
 TABLE_NAME="DocumentReferenceMetadata"
 
