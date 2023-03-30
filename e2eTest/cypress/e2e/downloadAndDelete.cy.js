3ff60640aa74dd1163b841cdb16a1753976dd173
RachelHowellNHS <rachel.howell6@nhs.net>
2023-03-30 12:02:18 +0100

    [PRMT-3200] Added download and delete e2e tests
    Co-authored-by: romanenhs <romane.castera1@nhs.net>
    Co-authored-by: Anu Doddala <anu.doddala1@nhs.net>

---
+++ e2eTest/cypress/e2e/downloadAndDelete.cy.js
@@ -0,0 +1,63 @@
+import * as path from "path";
+import { logAccessibilityViolations } from "../support/utils";
+
+describe("downloads and deletes docs", () => {
+    before(() => {
+        cy.exec("./e2e-teardown.sh $ENVIRONMENT");
+        cy.exec("./e2e-download-journey-setup.sh $ENVIRONMENT");
+    })
+
+    it("searches for a patient, downloads, and then deletes docs", () => {
+        const baseUrl = Cypress.config("baseUrl");
+        const nhsNumber = "9449305552";
+        const username = Cypress.env("username");
+        const password = Cypress.env("password");
+        const oidcProvider = Cypress.env("oidc_provider");
+        const downloadedDocumentPath = path.join(Cypress.config("downloadsFolder"), `patient-record-${nhsNumber}.zip`);
+
+        cy.visit("/");
+        cy.title().should("eq", "Inactive Patient Record Administration");
+        cy.injectAxe();
+        cy.checkA11y(undefined, undefined, logAccessibilityViolations, false);
+        cy.findByRole("button", {name: "Start now"}).click();
+
+        if (oidcProvider === "cis2devoidc") {
+            cy.findByPlaceholderText("User Name").type(username);
+            cy.findByPlaceholderText("Password").type(password);
+            cy.findByRole("button", {name: "Continue"}).click();
+        }
+
+        if (oidcProvider === "COGNITO") {
+            cy.get('input[name="username"]:visible').type(username);
+            cy.get('input[name="password"]:visible').type(password);
+            cy.get('input[name="signInSubmitButton"]:visible').click();
+        }
+
+        cy.url().should("eq", baseUrl + "/home");
+        cy.injectAxe();
+
+        cy.findByRole("radio", {name: /Download/}).check();
+        cy.findByRole("button", {name: "Continue"}).click();
+
+        cy.url().should("eq", baseUrl + "/search/patient");
+        cy.findByRole("textbox", {name: "Enter NHS number"}).type(nhsNumber);
+        cy.findByRole("button", {name: "Search"}).click();
+        cy.findByRole("button", {name: "Next"}).click();
+
+        cy.url().should("eq", baseUrl + "/search/results");
+        cy.readFile(downloadedDocumentPath).should("not.exist");
+        cy.findByRole("button", {name: "Download All Documents"}).click();
+        cy.readFile(downloadedDocumentPath).should("exist");
+        cy.checkA11y(undefined, undefined, logAccessibilityViolations, false);
+
+        cy.findByRole("button", {name: "Delete All Documents"}).click();
+        cy.url().should("eq", baseUrl + "/search/results/delete");
+        cy.findByRole("radio", {name: "Yes"}).check();
+        cy.checkA11y(undefined, undefined, logAccessibilityViolations, false);
+        cy.findByRole("button", {name: "Continue"}).click();
+        cy.url().should("eq", baseUrl + "/search/results");
+
+        cy.findByRole("link", {name: "Log Out"}).click();
+        cy.url().should("eq", baseUrl + "/");
+    });
+});
