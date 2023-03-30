e86d9414f5447c0f9bf3712153644cd695802034
Nicholas Hill <nick.hill3@nhs.net>
2023-03-30 18:24:52 +0100

    [PRMT-2779] Add empty constructor to AuthoriserHandler

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/AuthoriserHandler.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/AuthoriserHandler.java
@@ -1,11 +1,15 @@
 package uk.nhs.digital.docstore.authoriser.handlers;
 
+import static uk.nhs.digital.docstore.authoriser.handlers.BaseAuthRequestHandler.getDynamodbClient;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.IamPolicyResponse;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.nhs.digital.docstore.authoriser.repository.DynamoDBSessionStore;
 import uk.nhs.digital.docstore.authoriser.repository.SessionStore;
 import uk.nhs.digital.docstore.authoriser.requestEvents.AuthoriserRequestEvent;
 
@@ -14,6 +18,11 @@ public class AuthoriserHandler
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthoriserHandler.class);
 
     private final SessionStore sessionStore;
+
+    @SuppressWarnings("unused")
+    public AuthoriserHandler() {
+        this.sessionStore = new DynamoDBSessionStore(new DynamoDBMapper(getDynamodbClient()));
+    }
 
     public AuthoriserHandler(SessionStore sessionStore) {
         this.sessionStore = sessionStore;
