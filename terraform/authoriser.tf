db66cad82984e74d7ac6e35d467da1f24e3a3ec3
Rio Knightley <rio.knightley2@nhs.net>
2023-03-31 11:58:53 +0100

    [PRMT-2779] Update terraform identity_source var for auth header

--- terraform/authoriser.tf
+++ terraform/authoriser.tf
@@ -55,6 +55,7 @@ resource "aws_api_gateway_authorizer" "cognito_authorizer" {
 resource "aws_api_gateway_authorizer" "cis2_authoriser" {
   name                   = "cis2-authoriser"
   type                   = "REQUEST"
+  identity_source        = "method.request.header.Cookie"
   rest_api_id            = aws_api_gateway_rest_api.lambda_api.id
   authorizer_uri         = aws_lambda_function.authoriser_lambda.invoke_arn
   authorizer_credentials = aws_iam_role.authoriser_execution.arn
