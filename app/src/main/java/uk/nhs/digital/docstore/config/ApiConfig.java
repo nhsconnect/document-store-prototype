915031aaf09d2cfd44d9a89d1c192be48c336ca6
Nicholas Hill <nick.hill3@nhs.net>
2023-03-31 12:15:40 +0100

    [PRMT-2779] Add Cookie to allowed response headers

--- app/src/main/java/uk/nhs/digital/docstore/config/ApiConfig.java
+++ app/src/main/java/uk/nhs/digital/docstore/config/ApiConfig.java
@@ -6,13 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ApiConfig {
-    private static final Logger logger = LoggerFactory.getLogger(ApiConfig.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiConfig.class);
     public static final String AMPLIFY_BASE_URL_ENV_VAR = "AMPLIFY_BASE_URL";
 
     public String getAmplifyBaseUrl() {
         String url = System.getenv(AMPLIFY_BASE_URL_ENV_VAR);
         if (url == null) {
-            logger.warn("Missing required environment variable: " + AMPLIFY_BASE_URL_ENV_VAR);
+            LOGGER.warn("Missing required environment variable: " + AMPLIFY_BASE_URL_ENV_VAR);
             return "__unset__AMPLIFY_BASE_URL";
         }
         return url;
