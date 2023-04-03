3cccab45a1850e75308e5265e36c9a2ad93674d0
richbyrne-nhs <richard.byrne2@nhs.net>
2023-04-03 18:47:19 +0100

    Revert "[PRMT-2779] Reformat cookie getter in auth handler"
    This reverts commit 877d0c393d5735840b7bf93ed77bd6309a49c65c.

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/requestEvents/AuthoriserRequestEvent.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/requestEvents/AuthoriserRequestEvent.java
@@ -16,28 +16,18 @@ public class AuthoriserRequestEvent extends APIGatewayProxyRequestEvent {
     private Optional<String> getCookie(String cookieName) {
         var headers = getHeaders();
 
-        var hasUpper = headers != null && headers.containsKey("Cookie");
-
-        var cookieKey = hasUpper ? "Cookie" : "cookie";
-
-        var hasNoCookie =
-                headers == null
-                        || headers.get(cookieKey) == null
-                        || headers.get(cookieKey).isEmpty();
-
-        if (hasNoCookie) {
+        if (headers == null || headers.get("cookie") == null || headers.get("cookie").isEmpty()) {
             return Optional.empty();
         }
 
-        var cookieHeader = headers.get(cookieKey);
-
+        var cookieHeader = headers.get("cookie");
         var cookies = new HashMap<String, String>();
 
         Arrays.stream(cookieHeader.split(";"))
                 .forEach(
-                        cookieString -> {
-                            var keyValueTuple = cookieString.split("=");
-                            cookies.put(keyValueTuple[0].trim(), keyValueTuple[1].trim());
+                        cookie -> {
+                            var keyValue = cookie.split("=");
+                            cookies.put(keyValue[0].trim(), keyValue[1].trim());
                         });
 
         return Optional.ofNullable(cookies.get(cookieName));
