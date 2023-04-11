240efa2a6a3b5d91d557cb98c772c9bfe37dc880
richbyrne-nhs <richard.byrne2@nhs.net>
2023-04-11 14:45:40 +0100

    [PRMT-2779] added method response to api endpoint module

--- terraform/modules/api_gateway_endpoint/main.tf
+++ terraform/modules/api_gateway_endpoint/main.tf
@@ -4,7 +4,7 @@ variable "lambda_arn" {}
 variable "authorization" {}
 variable "authorizer_id" {}
 
-variable http_method {
+variable "http_method" {
   type = string
 }
 
@@ -25,6 +25,32 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri                     = var.lambda_arn
 }
 
-output http_method {
+///NO RESPONSE 
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = var.api_gateway_id
+  resource_id   = var.resource_id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "get_method_response" {
+  rest_api_id = var.api_gateway_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers"     = true,
+    "method.response.header.Access-Control-Allow-Methods"     = true,
+    "method.response.header.Access-Control-Allow-Origin"      = true,
+    "method.response.header.Access-Control-Allow-Credentials" = true
+  }
+  depends_on = [aws_api_gateway_method.get_method]
+}
+
+
+output "http_method" {
   value = var.http_method
 }
