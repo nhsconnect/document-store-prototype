e283f65894da9487119d10ebaa4698e63908addb
RachelHowellNHS <rachel.howell6@nhs.net>
2023-04-12 12:21:54 +0100

    Revert "[PRMT-2779] Added method response to api endpoint module"
    This reverts commit 5e8b70b2891ae9447effc599c574962fbb969198.

--- terraform/modules/api_gateway_endpoint/main.tf
+++ terraform/modules/api_gateway_endpoint/main.tf
@@ -25,32 +25,6 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri                     = var.lambda_arn
 }
 
-///NO RESPONSE
-resource "aws_api_gateway_method" "get_method" {
-  rest_api_id   = var.api_gateway_id
-  resource_id   = var.resource_id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method_response" "get_method_response" {
-  rest_api_id = var.api_gateway_id
-  resource_id = var.resource_id
-  http_method = aws_api_gateway_method.get_method.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers"     = true,
-    "method.response.header.Access-Control-Allow-Methods"     = true,
-    "method.response.header.Access-Control-Allow-Origin"      = true,
-    "method.response.header.Access-Control-Allow-Credentials" = true
-  }
-  depends_on = [aws_api_gateway_method.get_method]
-}
-
-
-output "http_method" {
+output http_method {
   value = var.http_method
 }
