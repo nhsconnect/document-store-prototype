a71f4998421b39720bfea97d7dbb293cd275b718
richbyrne-nhs <richard.byrne2@nhs.net>
2023-04-17 20:05:38 +0100

    [PRMT-2779] revert fetch request

--- ui/src/auth/authCallbackRouters/AuthCallbackRouter.jsx
+++ ui/src/auth/authCallbackRouters/AuthCallbackRouter.jsx
@@ -2,13 +2,9 @@ import { useBaseAPIUrl } from "../../providers/configProvider/ConfigProvider";
 import { useEffect } from "react";
 import routes from "../../enums/routes";
 import ProgressBar from "../../components/progressBar/ProgressBar";
-import { useSessionContext } from "../../providers/sessionProvider/SessionProvider";
-import { useNavigate } from "react-router";
 
 const AuthCallbackRouter = () => {
     const baseAPIUrl = useBaseAPIUrl("doc-store-api");
-    const [session, setSession] = useSessionContext();
-    const navigate = useNavigate();
 
     useEffect(() => {
         const urlSearchParams = new URLSearchParams(window.location.search);
@@ -17,27 +13,8 @@ const AuthCallbackRouter = () => {
         const redirectUri = new URL(routes.AUTH_SUCCESS, window.location.href);
         const tokenRequestUrl = `${baseAPIUrl}/Auth/TokenRequest?code=${code}&state=${state}&redirect_uri=${redirectUri}`;
 
-        fetch(tokenRequestUrl, {
-            method: "GET",
-            mode: "cors",
-            headers: { "Content-Type": "application/json" }
-        })
-            .then((res) => {
-                res.json().then((json) => {
-                    setSession({
-                        ...session,
-                        subjectClaim: json.subjectClaim,
-                        sessionId: json.sessionId,
-                        isLoggedIn: true
-                    });
-                });
-                navigate(routes.HOME);
-            })
-            .catch(() => {
-                console.error("Something went wrong with logging in");
-                navigate(routes.ROOT);
-            });
-    }, [baseAPIUrl, setSession, session, navigate]);
+        window.location.replace(tokenRequestUrl);
+    }, [baseAPIUrl]);
 
     return <ProgressBar status="Logging in..." />;
 };
