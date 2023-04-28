e3d7068fc7dd4219d0d5cd883028500ad8722b22
Richard Smith <richard.smith33@nhs.net>
2023-04-28 10:06:10 +0100

    [PRMT-2779] Logout integration test

---
+++ authoriser/src/integrationTest/java/uk.nhs.digital.docstore/LogoutTest.java
@@ -0,0 +1,66 @@
+package uk.nhs.digital.docstore;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.lambda.runtime.Context;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.UUID;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.auth0.jwt.JWT;
+import com.nimbusds.oauth2.sdk.id.Subject;
+import com.nimbusds.openid.connect.sdk.claims.SessionID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import uk.nhs.digital.docstore.authoriser.handlers.LogoutHandler;
+import uk.nhs.digital.docstore.authoriser.models.Session;
+import uk.nhs.digital.docstore.authoriser.repository.DynamoDBSessionStore;
+import uk.nhs.digital.docstore.authoriser.requestEvents.LogoutRequestEvent;
+import uk.nhs.digital.docstore.helpers.AWSServiceContainer;
+import uk.nhs.digital.docstore.helpers.DynamoDBHelper;
+
+import static com.auth0.jwt.algorithms.Algorithm.none;
+
+public class LogoutTest {
+    @Mock private Context context;
+    private final AWSServiceContainer aws = new AWSServiceContainer();
+    private final DynamoDBHelper dynamoDBHelper = new DynamoDBHelper(aws.getDynamoDBClient());
+    private final DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(aws.getDynamoDBClient());
+    private final DynamoDBSessionStore db = new DynamoDBSessionStore(dynamoDBMapper);
+
+    @BeforeEach
+    public void setUp() {
+        String tableName = "ARFAuth";
+        dynamoDBHelper.refreshTable(tableName);
+    }
+
+    @Test
+    public void shouldRemoveSessionByIdAndSubject() {
+        var subjectToRemove = new Subject("subToRemove");
+        var sessionIdToRemove = new SessionID("sidToRemove");
+        var timeToExist = Instant.ofEpochSecond(1L);
+        var sessionOne = Session.create(UUID.randomUUID(), timeToExist, subjectToRemove, sessionIdToRemove);
+        var sessionTwo = Session.create(UUID.randomUUID(), timeToExist, new Subject("sub2"), new SessionID("sid2"));
+        db.save(sessionOne);
+        db.save(sessionTwo);
+        var request = createRequestEvent(sessionIdToRemove, subjectToRemove);
+
+        var logoutHandler = new LogoutHandler();
+        var result = logoutHandler.handleRequest((LogoutRequestEvent) request, context);
+
+        assert(db.load(subjectToRemove, UUID.fromString(sessionIdToRemove.getValue())).isEmpty());
+    }
+
+    @Test
+    public void shouldRedirectToRequestedUrl() {}
+
+    private APIGatewayProxyRequestEvent createRequestEvent(SessionID sessionId, Subject subject) {
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("cookie", "SessionId" + sessionId.getValue() + "; SubjectClaim" + subject.getValue());
+
+        return new APIGatewayProxyRequestEvent().withHeaders(headers);
+    }
+}
