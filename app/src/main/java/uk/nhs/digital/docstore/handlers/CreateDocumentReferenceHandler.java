1081e3d88f482ec339d41245aa5594ffe6f130a5
RachelHowellNHS <rachel.howell6@nhs.net>
2023-05-05 12:06:51 +0100

    [PRMT-3251] Added logs to create document reference handler

--- app/src/main/java/uk/nhs/digital/docstore/handlers/CreateDocumentReferenceHandler.java
+++ app/src/main/java/uk/nhs/digital/docstore/handlers/CreateDocumentReferenceHandler.java
@@ -90,6 +90,8 @@ public class CreateDocumentReferenceHandler
         try {
             var inputDocumentReference =
                     jsonParser.parseResource(NHSDocumentReference.class, input.getBody());
+            LOGGER.debug("Input document reference desc: " + inputDocumentReference.getDescription());
+            LOGGER.debug("Input document reference  filename: " + inputDocumentReference.getFileName());
             requestValidator.validate(inputDocumentReference);
 
             var document = inputDocumentReference.parse();
