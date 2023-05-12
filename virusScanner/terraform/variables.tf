6b6de875fa47fcd8dedd9df17903b813ec209577
RachelHowellNHS <rachel.howell6@nhs.net>
2023-05-12 16:15:51 +0100

    [PRMT-3277] Added public address variable for turning the cloud storage security console on

--- virusScanner/terraform/variables.tf
+++ virusScanner/terraform/variables.tf
@@ -12,3 +12,9 @@ variable "black_hole_address" {
   default = "198.51.100.0/24"
   description = "using reserved address that does not lead anywhere to make sure CloudStorageSecurity console is not available"
 }
+
+variable "public_address" {
+  type = string
+  default = "0.0.0.0/0"
+  description = "using public address to make sure CloudStorageSecurity console is available"
+}
