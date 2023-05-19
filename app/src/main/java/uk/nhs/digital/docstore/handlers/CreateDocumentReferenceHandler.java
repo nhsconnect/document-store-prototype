0b9ceece141d446eb687566dd1a8a1e162989a03
abbas-khan10 <abbas.khan10@nhs.net>
2023-05-19 15:31:20 +0100

    [PRMT-3324]: update kms function

--- app/src/main/java/uk/nhs/digital/docstore/handlers/CreateDocumentReferenceHandler.java
+++ app/src/main/java/uk/nhs/digital/docstore/handlers/CreateDocumentReferenceHandler.java
@@ -13,15 +13,13 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.util.Base64;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-
-import com.amazonaws.util.Base64;
 import org.hl7.fhir.r4.model.Attachment;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
@@ -192,14 +190,14 @@ public class CreateDocumentReferenceHandler
             System.out.println("Decrypting key");
             byte[] encryptedKey = Base64.decode(System.getenv("TEST_API_KEY"));
             Map<String, String> encryptionContext = new HashMap<>();
-            encryptionContext.put("LambdaFunctionName",
-                    System.getenv("AWS_LAMBDA_FUNCTION_NAME"));
+            encryptionContext.put("LambdaFunctionName", System.getenv("AWS_LAMBDA_FUNCTION_NAME"));
 
             AWSKMS client = AWSKMSClientBuilder.defaultClient();
 
-            DecryptRequest request = new DecryptRequest()
-                    .withCiphertextBlob(ByteBuffer.wrap(encryptedKey))
-                    .withEncryptionContext(encryptionContext);
+            DecryptRequest request =
+                    new DecryptRequest()
+                            .withCiphertextBlob(ByteBuffer.wrap(encryptedKey))
+                            .withEncryptionContext(encryptionContext);
 
             ByteBuffer plainTextKey = client.decrypt(request).getPlaintext();
             return new String(plainTextKey.array(), Charset.forName("UTF-8"));
