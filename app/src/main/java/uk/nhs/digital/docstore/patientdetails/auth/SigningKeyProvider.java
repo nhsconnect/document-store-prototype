359fb804bf7d3f06731a73a74dbc5e7e0addaf42
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-05-22 15:22:43 +0100

    [PRMT-3324]
    make format-app

--- app/src/main/java/uk/nhs/digital/docstore/patientdetails/auth/SigningKeyProvider.java
+++ app/src/main/java/uk/nhs/digital/docstore/patientdetails/auth/SigningKeyProvider.java
@@ -1,7 +1,5 @@
 package uk.nhs.digital.docstore.patientdetails.auth;
 
-import static uk.nhs.digital.docstore.utils.KmsKeyDecrypt.decryptCiphertextWithKey;
-
 import com.amazonaws.util.Base64;
 import com.auth0.jwt.interfaces.RSAKeyProvider;
 import java.nio.charset.StandardCharsets;
@@ -29,13 +27,14 @@ public class SigningKeyProvider implements RSAKeyProvider {
     public RSAPrivateKey getPrivateKey() {
         try {
 
-//            String decryptedPrivateKeyAsString =
-//                    decryptCiphertextWithKey(environment.getEnvVar("PDS_FHIR_PRIVATE_KEY"));
-//            String privateKeyAsString =
-//                    decryptedPrivateKeyAsString
-//                            .replace("-----BEGIN PRIVATE KEY-----", "")
-//                            .replaceAll("\\n", "")
-//                            .replace("-----END PRIVATE KEY-----", "");
+            //            String decryptedPrivateKeyAsString =
+            //
+            // decryptCiphertextWithKey(environment.getEnvVar("PDS_FHIR_PRIVATE_KEY"));
+            //            String privateKeyAsString =
+            //                    decryptedPrivateKeyAsString
+            //                            .replace("-----BEGIN PRIVATE KEY-----", "")
+            //                            .replaceAll("\\n", "")
+            //                            .replace("-----END PRIVATE KEY-----", "");
 
             String privateKeyAsString =
                     environment
@@ -43,7 +42,6 @@ public class SigningKeyProvider implements RSAKeyProvider {
                             .replace("-----BEGIN PRIVATE KEY-----", "")
                             .replaceAll("\\n", "")
                             .replace("-----END PRIVATE KEY-----", "");
-
 
             byte[] keyBytes = Base64.decode(privateKeyAsString.getBytes(StandardCharsets.UTF_8));
 
