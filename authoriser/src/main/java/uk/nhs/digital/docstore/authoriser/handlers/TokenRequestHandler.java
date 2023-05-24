1a3e6f430c522d32b338ead056cdbfd636300ef3
Rio Knightley <rio.knightley2@nhs.net>
2023-05-24 10:57:56 +0100

    [PRMT-3328] Fix formatting

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/TokenRequestHandler.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/TokenRequestHandler.java
@@ -8,19 +8,12 @@ import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.openid.connect.sdk.validators.IDTokenValidator;
 import java.net.MalformedURLException;
 import java.time.Clock;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.nhs.digital.docstore.authoriser.HTTPTokenRequestClient;
 import uk.nhs.digital.docstore.authoriser.OIDCClient;
 import uk.nhs.digital.docstore.authoriser.OIDCHttpClient;
 import uk.nhs.digital.docstore.authoriser.OIDCTokenFetcher;
-import uk.nhs.digital.docstore.authoriser.exceptions.AuthorisationException;
-import uk.nhs.digital.docstore.authoriser.models.Session;
 import uk.nhs.digital.docstore.authoriser.repository.DynamoDBSessionStore;
 import uk.nhs.digital.docstore.authoriser.requestEvents.TokenRequestEvent;
 
@@ -71,70 +64,74 @@ public class TokenRequestHandler extends BaseAuthRequestHandler
         throw new RuntimeException();
 
         // TODO: [PRMT-2779] Add identifier such as a redacted state
-//        LOGGER.debug("Handling token request");
-//
-//        if (authCode.isEmpty()) {
-//            throw new RuntimeException("Auth code is empty");
-//        }
-//
-//        if (!requestEvent.hasMatchingStateValues()) {
-//            // TODO: [PRMT-2779] Add redaction if required
-//            LOGGER.debug(
-//                    "Mismatching state values. Cookie state: "
-//                            + requestEvent.getCookieState().orElse(null)
-//                            + " and query parameter state: "
-//                            + requestEvent.getQueryParameterState().orElse(null));
-//
-//            return new APIGatewayProxyResponseEvent()
-//                    .withStatusCode(400)
-//                    .withIsBase64Encoded(false)
-//                    .withBody("");
-//        }
-//
-//        // TODO: [PRMT-2779] Add redaction if required
-//        LOGGER.debug(
-//                "Authorising session for state: " + requestEvent.getCookieState().orElse(null));
-//
-//        Session session;
-//
-//        try {
-//            session = OIDCClient.authoriseSession(authCode.get());
-//        } catch (AuthorisationException exception) {
-//            throw new RuntimeException(exception);
-//        }
-//
-//        // TODO: [PRMT-2779] Add redaction if required
-//        LOGGER.debug(
-//                "Successfully authorised session with state: "
-//                        + requestEvent.getCookieState().orElse(null));
-//
-//        var headers = new HashMap<String, String>();
-//        headers.put("Location", requestEvent.getRedirectUri().orElseThrow());
-//        headers.put("Access-Control-Allow-Credentials", "true");
-//        var maxCookieAgeInSeconds =
-//                Duration.between(Instant.now(clock), session.getTimeToExist()).getSeconds();
-//        var sessionId = session.getId().toString();
-//        var stateCookie =
-//                httpOnlyCookieBuilder(
-//                        "State", requestEvent.getCookieState().orElseThrow().getValue(), 0L);
-//        var subjectClaimCookie =
-//                httpOnlyCookieBuilder(
-//                        "SubjectClaim", session.getOIDCSubject(), maxCookieAgeInSeconds);
-//        var sessionIdCookie = httpOnlyCookieBuilder("SessionId", sessionId, maxCookieAgeInSeconds);
-//        var cookies = List.of(stateCookie, subjectClaimCookie, sessionIdCookie);
-//        var multiValueHeaders = Map.of("Set-Cookie", cookies);
-//
-//        // TODO: [PRMT-2779] Add or improve redaction if required
-//        LOGGER.debug(
-//                "Responding with auth cookies for session with ID ending in: "
-//                        + sessionId.substring(sessionId.length() - 4));
-//
-//        return new APIGatewayProxyResponseEvent()
-//                .withIsBase64Encoded(false)
-//                .withStatusCode(SEE_OTHER_STATUS_CODE)
-//                .withHeaders(headers)
-//                .withBody("")
-//                .withMultiValueHeaders(multiValueHeaders);
+        //        LOGGER.debug("Handling token request");
+        //
+        //        if (authCode.isEmpty()) {
+        //            throw new RuntimeException("Auth code is empty");
+        //        }
+        //
+        //        if (!requestEvent.hasMatchingStateValues()) {
+        //            // TODO: [PRMT-2779] Add redaction if required
+        //            LOGGER.debug(
+        //                    "Mismatching state values. Cookie state: "
+        //                            + requestEvent.getCookieState().orElse(null)
+        //                            + " and query parameter state: "
+        //                            + requestEvent.getQueryParameterState().orElse(null));
+        //
+        //            return new APIGatewayProxyResponseEvent()
+        //                    .withStatusCode(400)
+        //                    .withIsBase64Encoded(false)
+        //                    .withBody("");
+        //        }
+        //
+        //        // TODO: [PRMT-2779] Add redaction if required
+        //        LOGGER.debug(
+        //                "Authorising session for state: " +
+        // requestEvent.getCookieState().orElse(null));
+        //
+        //        Session session;
+        //
+        //        try {
+        //            session = OIDCClient.authoriseSession(authCode.get());
+        //        } catch (AuthorisationException exception) {
+        //            throw new RuntimeException(exception);
+        //        }
+        //
+        //        // TODO: [PRMT-2779] Add redaction if required
+        //        LOGGER.debug(
+        //                "Successfully authorised session with state: "
+        //                        + requestEvent.getCookieState().orElse(null));
+        //
+        //        var headers = new HashMap<String, String>();
+        //        headers.put("Location", requestEvent.getRedirectUri().orElseThrow());
+        //        headers.put("Access-Control-Allow-Credentials", "true");
+        //        var maxCookieAgeInSeconds =
+        //                Duration.between(Instant.now(clock),
+        // session.getTimeToExist()).getSeconds();
+        //        var sessionId = session.getId().toString();
+        //        var stateCookie =
+        //                httpOnlyCookieBuilder(
+        //                        "State", requestEvent.getCookieState().orElseThrow().getValue(),
+        // 0L);
+        //        var subjectClaimCookie =
+        //                httpOnlyCookieBuilder(
+        //                        "SubjectClaim", session.getOIDCSubject(), maxCookieAgeInSeconds);
+        //        var sessionIdCookie = httpOnlyCookieBuilder("SessionId", sessionId,
+        // maxCookieAgeInSeconds);
+        //        var cookies = List.of(stateCookie, subjectClaimCookie, sessionIdCookie);
+        //        var multiValueHeaders = Map.of("Set-Cookie", cookies);
+        //
+        //        // TODO: [PRMT-2779] Add or improve redaction if required
+        //        LOGGER.debug(
+        //                "Responding with auth cookies for session with ID ending in: "
+        //                        + sessionId.substring(sessionId.length() - 4));
+        //
+        //        return new APIGatewayProxyResponseEvent()
+        //                .withIsBase64Encoded(false)
+        //                .withStatusCode(SEE_OTHER_STATUS_CODE)
+        //                .withHeaders(headers)
+        //                .withBody("")
+        //                .withMultiValueHeaders(multiValueHeaders);
     }
 
     private static IDTokenValidator makeIDTokenValidator() {
