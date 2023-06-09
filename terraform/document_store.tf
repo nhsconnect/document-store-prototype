f13409b44bb763b961b940022c5c05712cead1a2
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-06-09 16:57:01 +0100

    [PRMT-3343]
    Moving terraform outputs to their own file

--- terraform/document_store.tf
+++ terraform/document_store.tf
@@ -228,9 +228,7 @@ data "aws_iam_policy_document" "s3_object_access_policy_doc" {
   }
 }
 
-output "document-store-bucket" {
-  value = aws_s3_bucket.document_store.bucket
-}
+
 
 resource "aws_s3_bucket" "test_document_store" {
   bucket_prefix = "${terraform.workspace}-test-document-store"
