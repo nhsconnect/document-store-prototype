fba43f8bbd669c9694c686674c6560a412400c0b
Rio Knightley <rio.knightley2@nhs.net>
2023-06-14 16:14:00 +0100

    [PRMT-3343] Add lambda error handling to tf script

--- scripts/sandbox.sh
+++ scripts/sandbox.sh
@@ -6,13 +6,13 @@ normal=$(tput sgr0)
 function find_workspace(){
   MODE=$2
   WORKSPACE=$1
-  printf "$yellow\nFinding local workspace...\n\n${normal}"
-  terraform workspace select $WORKSPACE || printf "${yellow}\n${WORKSPACE} not found, initialising local state...\n\n${normal}" && terraform init
-  printf "${yellow}\nTerraform state initialised, selecting workspace...\n\n${normal}"
+  printf "\n⌛${yellow} Finding local workspace...\n\n${normal}"
+  terraform workspace select $WORKSPACE || (printf "\n⌛${yellow} $WORKSPACE not found, initialising local state...\n\n${normal}" && terraform init)
+  printf "\n⌛${yellow} Terraform state initialised, selecting workspace...\n\n${normal}"
   if [ $MODE == --create ]; then
-    terraform workspace select $WORKSPACE || printf "${yellow}\nRemote state not found for $WORKSPACE,\n creating workspace...\n\n${normal}" && terraform workspace select -or-create $WORKSPACE  || (printf "${red}\nUnknown error, check aws is authorised.\n\n${normal}" && return 1)
+    terraform workspace select $WORKSPACE || (printf "\n⌛${yellow} Remote state not found for $WORKSPACE,\n creating workspace...\n\n${normal}" && terraform workspace select -or-create $WORKSPACE) || (printf "\nℹ️${red} Unknown error, check aws is authorised.\n\n${normal}" && return 1)
   else 
-    terraform workspace select $WORKSPACE  || (printf "${red}\nUnknown error, check aws is authorised.\n\n${normal}" && return 1)
+    terraform workspace select $WORKSPACE  || (printf "\nℹ️${red} Unknown error, check aws is authorised.\n\n${normal}" && return 1)
   fi
 }
 
@@ -24,54 +24,62 @@ function run_sandbox() {
     OSTYPE=$(uname)
     cd ./terraform
     if [ $MODE == --destroy ]; then
-      printf "$yellow\nFinding local workspace...\n\n${normal}"
+      printf "\n⌛${yellow} Finding local workspace...\n\n${normal}"
       terraform workspace select $WORKSPACE
+      printf "\n✅${green} Workspace $WORKSPACE selected!\n\n${normal}"
+      printf "\n⌛${yellow} Destroying resources for $WORKSPACE...\n\n${normal}"
       terraform destroy -var-file="dev.tfvars"
-      printf "$yellow\nFinished destroy process for ${WORKSPACE}.\n\n${normal}"
+      printf "\n✅${green} Finished destroy process for $WORKSPACE!\n\n${normal}"
     elif [ $MODE == --deploy-app ]; then
       find_workspace $WORKSPACE --create
-      TF_STATE=$?
-      if [[ TF_STATE -ne 0 ]]; then
+      local TF_STATE=$?
+      if [[ $TF_STATE -ne 0 ]]; then
         exit 1;
       fi
-      printf "$yellow\nBuilding lambda layers...\n\n${normal}"
+      printf "\n✅${green} Workspace $WORKSPACE selected!\n\n${normal}"
+      printf "\n⌛${yellow} Building lambda layers...\n\n${normal}"
       touch ./.dynamo_env
       echo "WORKSPACE=$WORKSPACE" > .dynamo_env
       cd ..
       ./gradlew app:build
       build_lambdas
+      local LAMBDA_STATE=$?
+      echo $LAMBDA_STATE
+      if [[ $LAMBDA_STATE -ne 0 ]]; then
+        exit 1;
+      fi
       ./gradlew app:buildZip
-      printf "$yellow\nRefreshing local plan...\n\n${normal}"
+      printf "\n⌛${yellow} Refreshing local plan...\n\n${normal}"
       cd ./terraform
       terraform refresh -var-file="dev.tfvars"
       terraform plan -var-file="dev.tfvars" -out tfplan
       terraform output -json >"../terraform_output.json"
       terraform apply tfplan
-      printf "${yellow}\n Terraform environment ready!\n Run ${normal}$ make deploy-ui-${WORKSPACE}${yellow}\n to deploy the ui to amplify.\n\n${normal}"
+      printf "\n✅${green} Terraform environment ready!\n   Run ${normal}$ make deploy-ui-$WORKSPACE${green}\n   to deploy the ui to amplify.\n\n${normal}"
     elif [ $MODE == --deploy-ui ]; then
       if [ -f ".$TF_FILE" ]; then
         local TF_STATE=$(find_workspace $WORKSPACE --create)
         if [ $TF_STATE -ne 0 ]; then 
           exit $TF_STATE
         fi
-        printf "${green}\n${WORKSPACE} selected!\n\n${normal}"
+        printf "\n✅${green} Workspace $WORKSPACE selected!\n\n${normal}"
         cd ..
-        printf "$yellow\n Using ${OSTYPE} config...\n\n${normal}"
+        printf "\n⌛${yellow} Using $OSTYPE config...\n\n${normal}"
         if [[ "$OSTYPE" == "Darwin"* ]]; then
           create_sandbox_config $TF_FILE --osx
         else
           create_sandbox_config $TF_FILE --linux
         fi
-        printf "$yellow\n Building UI for the ${ENVIRONMENT} environment...\n\n${normal}"
+        printf "\n⌛${yellow} Building UI for the $ENVIRONMENT environment...\n\n${normal}"
         REACT_APP_ENV=${ENVIRONMENT} npm --prefix ./ui run build
         cd ui/build
         zip -r ../../ui.zip *
         cd ../..
-        printf "$yellow\nDeploying UI...\n\n${normal}"
+        printf "\n⌛${yellow} Deploying UI...\n\n${normal}"
         deploy_sandbox_ui $TF_FILE ui.zip $WORKSPACE
-        printf "$yellow\nFinished deployment process.\n\n${normal}"
+        printf "\n✅${green} Finished deployment process.\n\n${normal}"
       else
-        printf "${yellow}\n Terraform config not found.\n Please run ${normal}$ deploy-app-${WORKSPACE}${yellow}\n for the first time.\n\n${normal}"
+        printf "\nℹ️${red} Terraform config not found.\n   Please run ${normal}$ deploy-app-$WORKSPACE${red}\n   for the first time.\n\n${normal}"
       fi
     fi
     exit 0;
@@ -85,7 +93,7 @@ function build_lambdas() {
     ./gradlew lambdas:FakeVirusScannedEvent:build &&
     ./gradlew lambdas:ReRegistrationEvent:build &&
     ./gradlew lambdas:SearchPatientDetails:build &&
-    ./gradlew lambdas:VirusScannedEvent:build) || printf "${red}\nError building lambdas, ensure you are in root directory.\n\n${normal}"
+    ./gradlew lambdas:VirusScannedEvent:build) || (printf "\nℹ️${red} Error building lambdas, ensure you are in root directory and gradle is installed.\n\n${normal}" && return 1);
 }
 
 function create_sandbox_config() {
