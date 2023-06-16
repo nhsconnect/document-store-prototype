787b5551640ec7454cda7b3ad0787748317789b3
Rio Knightley <rio.knightley2@nhs.net>
2023-06-16 17:09:55 +0100

    [PRMT-3343] Finish script

--- scripts/sandbox.sh
+++ scripts/sandbox.sh
@@ -1,23 +1,11 @@
-set -e
-
 readonly aws_region=eu-west-2
 readonly green=$(tput setaf 2)
 readonly red=$(tput setaf 1)
 readonly yellow=$(tput setaf 3)
 readonly normal=$(tput sgr0)
-    
-function find_workspace(){
-  MODE=$2
-  WORKSPACE=$1
-  printf "\n⌛${yellow} Finding local workspace...\n\n${normal}"
-  terraform workspace select $WORKSPACE || (printf "\n⌛${yellow} $WORKSPACE not found, initialising local state...\n\n${normal}" && terraform init)
-  printf "\n⌛${yellow} Terraform state initialised, selecting workspace...\n\n${normal}"
-  if [ $MODE == --create ]; then
-    terraform workspace select $WORKSPACE || (printf "\n⌛${yellow} Remote state not found for $WORKSPACE,\n creating workspace...\n\n${normal}" && terraform workspace select -or-create $WORKSPACE) || (printf "\nℹ️${red} Unknown error, check aws is authorised.\n\n${normal}" && return 1)
-  else 
-    terraform workspace select $WORKSPACE  || (printf "\nℹ️${red} Unknown error, check aws is authorised.\n\n${normal}" && return 1)
-  fi
-}
+readonly icon_ld="⌛"
+readonly icon_sc="✅"
+readonly icon_er="ℹ️"
 
 function run_sandbox() {
     WORKSPACE=$1
@@ -27,70 +15,101 @@ function run_sandbox() {
     OSTYPE=$(uname)
     cd ./terraform
     if [ $MODE == --destroy ]; then
-      printf "\n⌛${yellow} Finding local workspace...\n\n${normal}"
-      terraform workspace select $WORKSPACE
-      printf "\n✅${green} Workspace $WORKSPACE selected!\n\n${normal}"
-      printf "\n⌛${yellow} Destroying resources for $WORKSPACE...\n\n${normal}"
+      find_workspace $WORKSPACE
+      local TF_STATE=$?
+      if [[ $TF_STATE -ne 0 ]]; then
+        printf "\n${icon_er}${red} Unknown error, check awsume is authorised.\n\n${normal}" && exit $TF_STATE;
+      fi
+      printf "\n${icon_sc}${green} Workspace $WORKSPACE selected!\n\n${normal}"
+      printf "\n${icon_ld}${yellow} Destroying resources for $WORKSPACE...\n\n${normal}"
       terraform destroy -var-file="dev.tfvars"
-      printf "\n✅${green} Finished destroy process for $WORKSPACE!\n\n${normal}"
+      printf "\n${icon_sc}${green} Finished destroy process for $WORKSPACE!\n\n${normal}"
     elif [ $MODE == --plan-app ]; then
       find_workspace $WORKSPACE --create
       local TF_STATE=$?
       if [[ $TF_STATE -ne 0 ]]; then
-        exit 1;
+        printf "\n${icon_ld}${red} Unknown error, check awsume is authorised.\n\n${normal}" && exit $TF_STATE;
       fi
-      printf "\n✅${green} Workspace $WORKSPACE selected!\n\n${normal}"
-      printf "\n⌛${yellow} Building lambda layers...\n\n${normal}"
+      exit 0;
+      printf "\n${icon_ld}${yellow} Building lambda layers...\n\n${normal}"
+
       touch ./.dynamo_env
       echo "WORKSPACE=$WORKSPACE" > .dynamo_env
       cd ..
       ./gradlew app:build
-      build_lambdas
-      local LAMBDA_STATE=$?
-      echo $LAMBDA_STATE
+      local LAMBDA_STATE=$(build_lambdas)
       if [[ $LAMBDA_STATE -ne 0 ]]; then
-        exit 1;
+       printf "\n${icon_er}${red} Error building lambdas, ensure you are in root directory and gradle is installed.\n\n${normal}" && exit $LAMBDA_STATE;
       fi
       ./gradlew app:buildZip
-      printf "\n⌛${yellow} Refreshing local plan...\n\n${normal}"
+      printf "\n${icon_ld}${yellow} Syncing local plan with remote plan...\n\n${normal}"
       cd ./terraform
       refresh_plan_and_output
     elif [ $MODE == --deploy-app ]; then
-      terraform apply tfplan || (refresh_plan_and_output && terraform apply tfplan)
-      printf "\n✅${green} Terraform environment ready!\n   Run ${normal}$ make deploy-ui-$WORKSPACE${green}\n   to deploy the ui to amplify.\n\n${normal}"
+      if [ -f ".$TF_FILE" ]; then
+        find_workspace $WORKSPACE
+        local TF_STATE=$?
+        if [[ $TF_STATE -ne 0 ]]; then
+          printf "\n${icon_er}${red} Unknown error, check aws is authorised.\n\n${normal}" && exit $TF_STATE;
+        fi
+        printf "\n${icon_sc}${green} Terraform environment ready!\n   Run ${normal}$ make deploy-ui-$WORKSPACE${green}\n   to deploy the ui to amplify.\n\n${normal}"
+      else
+        printf "\n${icon_er}${red} Terraform config not found.\n   Please run ${normal}$ plan-app-$WORKSPACE${red}\n   for the first time.\n\n${normal}"
+      fi
     elif [ $MODE == --deploy-ui ]; then
       if [ -f ".$TF_FILE" ]; then
-        local TF_STATE=$(find_workspace $WORKSPACE --create)
-        if [ $TF_STATE -ne 0 ]; then 
-          exit $TF_STATE
+        local TF_STATE=$(find_workspace $WORKSPACE)
+        if [[ $TF_STATE -ne 0 ]]; then
+          printf "\n${icon_er}${red} Unknown error, check aws is authorised.\n\n${normal}" && exit $TF_STATE;
         fi
-        printf "\n✅${green} Workspace $WORKSPACE selected!\n\n${normal}"
+        printf "\n${icon_sc}${green} Workspace $WORKSPACE selected!\n\n${normal}"
         cd ..
-        printf "\n⌛${yellow} Using $OSTYPE config...\n\n${normal}"
-        if [[ "$OSTYPE" == "Darwin"* ]]; then
+        if [ "$OSTYPE" == "Darwin"* ]; then
           create_sandbox_config $TF_FILE --osx
         else
           create_sandbox_config $TF_FILE --linux
         fi
-        printf "\n⌛${yellow} Building UI for the $ENVIRONMENT environment...\n\n${normal}"
+        printf "\n${icon_ld}${yellow} Building UI for the $ENVIRONMENT environment...\n\n${normal}"
         REACT_APP_ENV=${ENVIRONMENT} npm --prefix ./ui run build
         cd ui/build
         zip -r ../../ui.zip *
         cd ../..
-        printf "\n⌛${yellow} Deploying UI...\n\n${normal}"
+        printf "\n${icon_ld}${yellow} Deploying UI...\n\n${normal}"
         deploy_sandbox_ui $TF_FILE ./ui.zip $WORKSPACE
-        printf "\n✅${green} Finished deployment process.\n\n${normal}"
+        printf "\n${icon_sc}${green} Finished deployment process.\n\n${normal}"
       else
-        printf "\nℹ️${red} Terraform config not found.\n   Please run ${normal}$ deploy-app-$WORKSPACE${red}\n   for the first time.\n\n${normal}"
+        printf "\n${icon_er}${red} Terraform config not found.\n   Please run ${normal}$ plan-app-$WORKSPACE${red}\n   for the first time.\n\n${normal}"
       fi
     fi
     exit 0;
 }
 
+function find_workspace(){
+  MODE=$2
+  WORKSPACE=$1
+  LOCK_FILE="./.terraform.lock.hcl"
+  printf "\n${icon_ld}${yellow} Checking Terraform...\n\n${normal}"
+  if [ ! -f $LOCK_FILE ]; then
+    printf "\n${icon_sc}${green} Terraform not found, initialising Terraform for the first time.\n\n${normal}"
+    terraform init || return 1;
+  else
+    printf "\n${icon_sc}${green} Successfully found Terraform!\n\n${normal}"
+  fi
+  terraform refresh -var-file="dev.tfvars"
+  printf "\n${icon_ld}${yellow} Terraform state initialised, selecting workspace...\n\n${normal}"
+  if [ $MODE == --create ]; then
+    (terraform workspace select -or-create $WORKSPACE || return 1);
+  else 
+    (terraform workspace select $WORKSPACE || return 1);
+  fi
+  return 0;
+}
+
 function refresh_plan_and_output() {
   terraform refresh -var-file="dev.tfvars"
-  terraform plan -var-file="dev.tfvars" -out tfplan
+  terraform plan -var-file="dev.tfvars" -out tfplan 2>&1 | tee tfplan.log
   terraform output -json >"../terraform_output.json"
+  printf "\n${icon_sc}${green} Terraform plan created!\n   Plan output has been logged to ${normal}tfplan.log${green}\n   check the output then run ${normal}$ make deploy-app-${WORKSPACE} to continue deployment.${green}.\n\n${normal}"
 }
 
 function build_lambdas() {
@@ -101,7 +120,7 @@ function build_lambdas() {
     ./gradlew lambdas:FakeVirusScannedEvent:build &&
     ./gradlew lambdas:ReRegistrationEvent:build &&
     ./gradlew lambdas:SearchPatientDetails:build &&
-    ./gradlew lambdas:VirusScannedEvent:build) || (printf "\nℹ️${red} Error building lambdas, ensure you are in root directory and gradle is installed.\n\n${normal}" && return 1);
+    ./gradlew lambdas:VirusScannedEvent:build) || return 1;
 }
 
 function create_sandbox_config() {
@@ -112,6 +131,8 @@ function create_sandbox_config() {
   api_endpoint="$(jq -r '.api_gateway_url.value' "$1")"
   cognito_domain="$(jq -r '.cognito_user_pool_domain.value' "$1")"
   amplify_app_id="$(jq -r '.amplify_app_ids.value[0]' "$1")"
+
+  printf "\n${icon_ld}${yellow} Using $OSTYPE config...\n\n${normal}"
   if [ $MODE == --osx ]; then
     sed -i "" "s/%pool-id%/${user_pool}/" ui/src/config.js
     sed -i "" "s/%client-id%/${user_pool_client_id}/" ui/src/config.js
