f6f6a1d8daf58f6a8de88eaa7ec2ae623790979e
RachelHowellNHS <rachel.howell6@nhs.net>
2023-06-19 15:53:51 +0100

    [PRMT-3456] add error handling to ODSAPIClient
    Co-authored-by: Richard Smith <richard.smith33@nhs.net>

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/ODSAPIClient.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/ODSAPIClient.java
@@ -5,20 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.nhs.digital.docstore.authoriser.handlers.TokenRequestHandler;
 
 public class ODSAPIClient {
+    public static final Logger LOGGER = LoggerFactory.getLogger(TokenRequestHandler.class);
+
     private static final String url =
             "https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations/";
 
     public static String getOrgData(String odsCode) throws IOException {
         var requestUrl = new URL(url + odsCode);
         HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
-
         connection.setRequestMethod("GET");
 
         int responseCode = connection.getResponseCode();
-        System.out.println("Response Code: " + responseCode);
 
+        if (responseCode != 200) {
+            LOGGER.error("Response code: " + responseCode + " problem retrieving ODS code");
+            throw new IOException(connection.getResponseMessage());
+        }
+
+        var response = getResponse(connection);
+
+        connection.disconnect();
+
+        return response;
+    }
+
+    private static String getResponse(HttpURLConnection connection) throws IOException {
         BufferedReader reader =
                 new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String line;
@@ -29,11 +45,6 @@ public class ODSAPIClient {
         }
 
         reader.close();
-
-        System.out.println("Response Body: " + response);
-
-        connection.disconnect();
-
         return response.toString();
     }
 }
--- authoriser/src/test/java/uk/nhs/digital/docstore/authoriser/ODSAPIClientTest.java
+++ authoriser/src/test/java/uk/nhs/digital/docstore/authoriser/ODSAPIClientTest.java
@@ -1,5 +1,7 @@
 package uk.nhs.digital.docstore.authoriser;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +12,10 @@ class ODSAPIClientTest {
 
         assert (response.contains("PrimaryRoleId"));
         assert (response.contains("X26"));
+    }
+
+    @Test
+    void throwsErrorIfResponseCodeIsNot200() {
+        assertThrows(IOException.class, () -> ODSAPIClient.getOrgData(""));
     }
 }
