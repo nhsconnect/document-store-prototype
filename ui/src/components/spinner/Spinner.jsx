b7b772626d45e08dd8d0b9ba3eed79c813333653
NogaNHS <noga.sasson1@nhs.net>
2023-06-20 11:19:04 +0100

    [PRMT-3327] reorganise Css

--- ui/src/components/spinner/Spinner.jsx
+++ ui/src/components/spinner/Spinner.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 
 const Spinner = ({ status }) => {
-    const spinnerStyle = {
-        boxSizing: "border-box",
-        margin: "auto",
-        width: "120px",
-        height: "120px",
-        borderRadius: "50%",
-    };
     return (
         <div className="nhsuk-loader" role="Spinner" aria-label={status}>
-            <span className="spinner-blue" style={spinnerStyle}></span>
             <span className="nhsuk-loader__text" role="status">
                 {status}
             </span>
+            <span className="spinner-blue"></span>
         </div>
     );
 };
--- ui/src/components/spinnerButton/SpinnerButton.jsx
+++ ui/src/components/spinnerButton/SpinnerButton.jsx
@@ -3,19 +3,12 @@ import { Button } from "nhsuk-react-components";
 
 const SpinnerButton = ({ status }) => {
     const buttonStyle = { display: "flex" };
-    const spinnerStyle = {
-        boxSizing: "border-box",
-        margin: "5px",
-        marginRight: "10px",
-        width: "20px",
-        height: "20px",
-        borderRadius: "50%",
-    };
+    const spinnerStyle = {};
 
     return (
         <>
             <Button aria-label={status} style={buttonStyle} role="SpinnerButton">
-                <div className="spinner-white" style={spinnerStyle}></div>
+                <div className="spinner-button" style={spinnerStyle}></div>
                 <div role="status" className="nhsuk-loader__text">
                     {status}
                 </div>
