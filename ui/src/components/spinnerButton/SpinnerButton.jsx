4432edbe85d1f424a23513d69f0a19393a4934cb
NogaNHS <noga.sasson1@nhs.net>
2023-06-20 11:33:32 +0100

    [PRMT-3327] delete const

--- ui/src/components/spinnerButton/SpinnerButton.jsx
+++ ui/src/components/spinnerButton/SpinnerButton.jsx
@@ -3,12 +3,11 @@ import { Button } from "nhsuk-react-components";
 
 const SpinnerButton = ({ status }) => {
     const buttonStyle = { display: "flex" };
-    const spinnerStyle = {};
 
     return (
         <>
             <Button aria-label={status} style={buttonStyle} role="SpinnerButton">
-                <div className="spinner-button" style={spinnerStyle}></div>
+                <div className="spinner-button"></div>
                 <div role="status" className="nhsuk-loader__text">
                     {status}
                 </div>
