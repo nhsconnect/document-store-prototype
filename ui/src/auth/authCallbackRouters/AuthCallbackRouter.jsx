7cfea2d5eb81a5a545e6b2df827c14cd1896df3b
RachelHowellNHS <rachel.howell6@nhs.net>
2023-07-05 14:32:53 +0100

    Revert "[PRMT-3489] change axios credentials config"
    This reverts commit a7c9ea15ed487129381bd2c47cf563108aa2f3ac.

--- ui/src/auth/authCallbackRouters/AuthCallbackRouter.jsx
+++ ui/src/auth/authCallbackRouters/AuthCallbackRouter.jsx
@@ -14,9 +14,9 @@ const AuthCallbackRouter = () => {
         const state = urlSearchParams.get("state");
         const error_uri = new URL(routes.AUTH_ERROR, window.location.href);
         const redirect_uri = new URL(routes.AUTH_SUCCESS, window.location.href);
-        axios.defaults.withCredentials = true
         axios
             .get(`${baseAPIUrl}/Auth/TokenRequest`, {
+                withCredentials: true,
                 params: { code, state, redirect_uri, error_uri },
             })
             .then((res) => {
