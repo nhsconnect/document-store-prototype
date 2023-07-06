479b3efd6453587b180466e1b26ecc868d6dfee5
RachelHowellNHS <rachel.howell6@nhs.net>
2023-07-06 10:03:31 +0100

    [PRMT-3489] add access-control-allow-origin  getAmplifyBaseUrl()) to response header

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/TokenRequestHandler.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/TokenRequestHandler.java
@@ -125,6 +125,7 @@ public class TokenRequestHandler extends BaseAuthRequestHandler
         var headers = new HashMap<String, String>();
         headers.put("Location", requestEvent.getRedirectUri().orElseThrow());
         headers.put("Access-Control-Allow-Credentials", "true");
+        headers.put("Access-Control-Allow-Origin", getAmplifyBaseUrl());
         var maxCookieAgeInSeconds =
                 Duration.between(Instant.now(clock), session.getTimeToExist()).getSeconds();
         var sessionId = session.getId().toString();
