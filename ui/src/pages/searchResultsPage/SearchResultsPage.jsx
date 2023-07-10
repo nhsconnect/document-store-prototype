c695dc5249f9e091bef59d09237fac1ca03d4972
RachelHowellNHS <rachel.howell6@nhs.net>
2023-07-10 12:31:30 +0100

    [PRMT-3450] add tableColumnHeader className to increase font size of 'Filename' and 'Uploaded at' text

--- ui/src/pages/searchResultsPage/SearchResultsPage.jsx
+++ ui/src/pages/searchResultsPage/SearchResultsPage.jsx
@@ -110,7 +110,7 @@ const SearchResultsPage = () => {
                                     >
                                         <Table.Head>
                                             <Table.Row>
-                                                <Table.Cell>Filename</Table.Cell>
+                                                <Table.Cell className={"tableColumnHeader"}>Filename</Table.Cell>
                                             </Table.Row>
                                         </Table.Head>
                                         <Table.Body>
@@ -131,8 +131,8 @@ const SearchResultsPage = () => {
                                 <Table caption={docsAvailableTableCaption}>
                                     <Table.Head>
                                         <Table.Row>
-                                            <Table.Cell>Filename</Table.Cell>
-                                            <Table.Cell>Uploaded At</Table.Cell>
+                                            <Table.Cell className={"tableColumnHeader"}>Filename</Table.Cell>
+                                            <Table.Cell className={"tableColumnHeader"}>Uploaded At</Table.Cell>
                                         </Table.Row>
                                     </Table.Head>
                                     <Table.Body>
