d7b7d774b4545cbabe2f88ca11d064ae1f49d5cc
RachelHowellNHS <rachel.howell6@nhs.net>
2023-07-11 12:30:07 +0100

    [PRMT-3498] hardcode url to try to fix log in issue
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>
    Co-authored-by: abbas-khan10 <abbas.khan10@nhs.net> <noga.sasson1@nhs.net>

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/TokenRequestHandler.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/TokenRequestHandler.java
@@ -125,7 +125,7 @@ public class TokenRequestHandler extends BaseAuthRequestHandler
         var headers = new HashMap<String, String>();
         headers.put("Location", requestEvent.getRedirectUri().orElseThrow());
         headers.put("Access-Control-Allow-Credentials", "true");
-        headers.put("Access-Control-Allow-Origin", getAmplifyBaseUrl());
+        headers.put("Access-Control-Allow-Origin", "https://main.dhpu8o94qbs05.amplifyapp.com");
         var maxCookieAgeInSeconds =
                 Duration.between(Instant.now(clock), session.getTimeToExist()).getSeconds();
         var sessionId = session.getId().toString();
