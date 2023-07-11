ec5fba58f6b355da6d4751a111d3a7628e6691ff
Rio Knightley <rio.knightley2@nhs.net>
2023-07-11 11:44:44 +0100

    [PRMT-3498] Add logging

--- ui/src/auth/authCallbackRouters/AuthCallbackRouter.jsx
+++ ui/src/auth/authCallbackRouters/AuthCallbackRouter.jsx
@@ -14,10 +14,11 @@ const AuthCallbackRouter = () => {
         const state = urlSearchParams.get("state");
         const error_uri = new URL(routes.AUTH_ERROR, window.location.href);
         const redirect_uri = new URL(routes.AUTH_SUCCESS, window.location.href);
-        axios.defaults.withCredentials = true;
+        console.log(code, state);
         axios
             .get(`${baseAPIUrl}/Auth/TokenRequest`, {
                 params: { code, state, redirect_uri, error_uri },
+                withCredentials: true,
             })
             .then(() => {
                 navigate(routes.AUTH_SUCCESS);
