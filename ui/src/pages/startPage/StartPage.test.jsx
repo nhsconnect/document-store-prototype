01c7436cca957f43e154a07b53112c63c4f48315
Rio Knightley <rio.knightley2@nhs.net>
2023-07-19 16:06:11 +0100

    [PRMT-3536] Remove invalid tests

--- ui/src/pages/startPage/StartPage.test.jsx
+++ ui/src/pages/startPage/StartPage.test.jsx
@@ -1,22 +1,39 @@
 import { render, screen } from "@testing-library/react";
 import StartPage from "./StartPage";
+import { MemoryRouter, useNavigate } from "react-router";
 import { useBaseAPIUrl, useFeatureToggle } from "../../providers/configProvider/ConfigProvider";
 
 jest.mock("../../providers/configProvider/ConfigProvider");
+jest.mock("react-router", () => ({
+    ...jest.requireActual("react-router"),
+    useNavigate: jest.fn(),
+}));
 
+const renderPage = () =>
+    render(
+        <MemoryRouter>
+            <StartPage />
+        </MemoryRouter>
+    );
 describe("StartPage", () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
 
     it("renders the page header", () => {
-        render(<StartPage />);
+        const navigateMock = jest.fn();
+        useNavigate.mockReturnValue(navigateMock);
+
+        renderPage();
 
         expect(screen.getByRole("heading", { name: "Inactive Patient Record Administration" })).toBeInTheDocument();
     });
 
     it("renders service info", () => {
-        render(<StartPage />);
+        const navigateMock = jest.fn();
+        useNavigate.mockReturnValue(navigateMock);
+
+        renderPage();
 
         expect(screen.getByText(/When a patient is inactive/)).toBeInTheDocument();
         expect(screen.getByText(/General Practice Staff/)).toBeInTheDocument();
@@ -24,7 +41,10 @@ describe("StartPage", () => {
     });
 
     it("renders service issue guidance with a link to service desk that opens in a new tab", () => {
-        render(<StartPage />);
+        const navigateMock = jest.fn();
+        useNavigate.mockReturnValue(navigateMock);
+
+        renderPage();
 
         expect(screen.getByText(/If there is an issue/)).toBeInTheDocument();
         const nationalServiceDeskLink = screen.getByRole("link", { name: /National Service Desk/ });
@@ -36,29 +56,13 @@ describe("StartPage", () => {
     });
 
     it("renders a 'Before you start' section", () => {
+        const navigateMock = jest.fn();
+        useNavigate.mockReturnValue(navigateMock);
         render(<StartPage />);
 
         expect(screen.getByRole("heading", { name: "Before You Start" })).toBeInTheDocument();
         expect(screen.getByText(/valid NHS smartcard/)).toBeInTheDocument();
     });
 
-    it("renders a button link with an href to /home when OIDC federation is enabled", () => {
-        useFeatureToggle.mockReturnValue(true);
-        useFeatureToggle.mockReturnValue("https://api.url");
-
-        render(<StartPage />);
-
-        expect(screen.getByRole("button", { name: "Start now" })).toHaveAttribute("href", "/home");
-    });
-
-    it("renders a button link with an href to the auth login endpoint when OIDC federation is disabled", () => {
-        const baseAPIUrl = "https://api.url";
-
-        useFeatureToggle.mockReturnValue(false);
-        useBaseAPIUrl.mockReturnValue(baseAPIUrl);
-
-        render(<StartPage />);
-
-        expect(screen.getByRole("button", { name: "Start now" })).toHaveAttribute("href", `${baseAPIUrl}/Auth/Login`);
-    });
+    // Todo : New tests for organisation and axios get
 });
