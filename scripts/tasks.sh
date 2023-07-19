26c57ddc05b2025f5b6f3be4274cfb2fa979b8c4
NogaNHS <noga.sasson1@nhs.net>
2023-07-19 11:42:10 +0100

    [PRMT-3343] - change order

--- scripts/tasks.sh
+++ scripts/tasks.sh
@@ -115,9 +115,9 @@ function export_cypress_base_url() {
 function tf_init {
   check_env
   cd terraform
-  terraform workspace list
   terraform init \
     -backend-config ${ENVIRONMENT}.s3.tfbackend
+  terraform workspace list
   echo ${WORKSPACE}
   terraform workspace select -or-create ${WORKSPACE}
 }
