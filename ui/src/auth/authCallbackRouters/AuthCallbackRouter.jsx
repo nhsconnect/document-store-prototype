79df4357d430e10ccfad9fcbf895af0266ce6d79
Rio Knightley <rio.knightley2@nhs.net>
2023-07-20 14:21:33 +0100

    [PRMT-3547] Alter session context for role selection

--- ui/src/auth/authCallbackRouters/AuthCallbackRouter.jsx
+++ ui/src/auth/authCallbackRouters/AuthCallbackRouter.jsx
@@ -4,28 +4,38 @@ import routes from "../../enums/routes";
 import axios from "axios";
 import { useNavigate } from "react-router";
 import Spinner from "../../components/spinner/Spinner";
+import { useSessionContext } from "../../providers/sessionProvider/SessionProvider";
 
 const AuthCallbackRouter = () => {
     const navigate = useNavigate();
     const baseAPIUrl = useBaseAPIUrl("doc-store-api");
+    const [session, setSession] = useSessionContext();
+
     useEffect(() => {
         const urlSearchParams = new URLSearchParams(window.location.search);
         const code = urlSearchParams.get("code");
         const state = urlSearchParams.get("state");
-        console.log(code, state);
         axios
             .get(`${baseAPIUrl}/Auth/TokenRequest`, {
                 params: { code, state },
                 withCredentials: true,
             })
-            .then(() => {
-                navigate(routes.AUTH_SUCCESS);
+            .then((res) => {
+                console.log(res);
+                setSession({
+                    ...session,
+                    isLoggedIn: true,
+                });
+                if (session.orgs) {
+                    navigate(routes.ORG_SELECT);
+                } else {
+                    navigate(routes.HOME);
+                }
             })
-            .catch((err) => {
-                console.log(err);
+            .catch(() => {
                 navigate(routes.AUTH_ERROR);
             });
-    }, [baseAPIUrl, navigate]);
+    }, [baseAPIUrl, navigate, session, setSession]);
 
     return <Spinner status="Logging in..." />;
 };
