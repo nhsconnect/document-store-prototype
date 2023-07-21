44cfe9d3d2f0fe254af77f047e91cfcc4b61969f
Rio Knightley <rio.knightley2@nhs.net>
2023-07-21 14:53:15 +0100

    [PRMT-3547] Add org response

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/models/Organisation.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/models/Organisation.java
@@ -4,24 +4,24 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class Organisation {
-    private final String orgCode;
+    private final String odsCode;
     private final String orgName;
+    private final String orgType;
 
     public Organisation(
-            @JsonProperty("org_code") String orgCode, @JsonProperty("org_name") String orgName) {
-        this.orgCode = orgCode;
+            @JsonProperty("org_name") String orgName,
+            @JsonProperty("ods_code") String odsCode,
+            @JsonProperty("org_type") String orgType) {
+        this.odsCode = odsCode;
         this.orgName = orgName;
+        this.orgType = orgType;
     }
 
-    public String getOrgCode() {
-        return orgCode;
-    }
-
-    public String getOrgName() {
-        return orgName;
+    public String getOdsCode() {
+        return odsCode;
     }
 
     public static boolean containsOrganisation(List<Organisation> organisations, String code) {
-        return organisations.stream().anyMatch(org -> org.getOrgCode().equals(code));
+        return organisations.stream().anyMatch(org -> org.getOdsCode().equals(code));
     }
 }
