aa3d12e9164972558c7740abf0a6b7bb71c2526c
NogaNHS <noga.sasson1@nhs.net>
2023-07-25 09:06:37 +0100

    [PRMT-3610] only dev workspace have integration test runner

--- terraform/integration_test.tf
+++ terraform/integration_test.tf
@@ -2,6 +2,7 @@ resource "aws_iam_role" "integration_test_runner" {
   name               = "IntegrationTestRunner"
   description        = "Role for running integration tests against AWS resources"
   assume_role_policy = data.aws_iam_policy_document.integration_test_runner_trust_policy.json
+  count              = terraform.workspace == 'dev' ? 1 : 0
 }
 
 data "aws_iam_policy_document" "integration_test_runner_trust_policy" {
@@ -18,9 +19,11 @@ data "aws_iam_policy_document" "integration_test_runner_trust_policy" {
 resource "aws_iam_role_policy_attachment" "integration_test_runner_dynamodb_access_policy_attachment" {
   role       = aws_iam_role.integration_test_runner.name
   policy_arn = aws_iam_policy.dynamodb_table_access_policy.arn
+  count      = terraform.workspace == 'dev' ? 1 : 0
 }
 
 resource "aws_iam_role_policy_attachment" "integration_test_runner_s3_access_policy_attachment" {
   role       = aws_iam_role.integration_test_runner.name
   policy_arn = aws_iam_policy.s3_object_access_policy.arn
+  count      = terraform.workspace == 'dev' ? 1 : 0
 }
