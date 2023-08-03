267b65d09dd13ac73e8503294fb72eb92405ef5d
RachelHowellNHS <rachel.howell6@nhs.net>
2023-08-03 15:03:14 +0100

    [PRMT-3542] fix unit tests

--- ui/src/auth/authCallbackRouters/AuthCallbackRouter.test.jsx
+++ ui/src/auth/authCallbackRouters/AuthCallbackRouter.test.jsx
@@ -4,8 +4,10 @@ import { useNavigate } from "react-router";
 import { useBaseAPIUrl } from "../../providers/configProvider/ConfigProvider";
 import routes from "../../enums/routes";
 import axios from "axios";
+import { useSessionContext } from "../../providers/sessionProvider/SessionProvider";
 
 jest.mock("react-router");
+jest.mock("../../providers/sessionProvider/SessionProvider");
 jest.mock("../../providers/configProvider/ConfigProvider");
 jest.mock("axios");
 
@@ -29,6 +31,11 @@ describe("AuthCallbackRouter", () => {
 
     const mockNavigate = jest.fn();
 
+    const session = { isLoggedIn: false };
+    const setSessionMock = jest.fn();
+
+    useSessionContext.mockReturnValue([session, setSessionMock]);
+
     beforeEach(() => {
         delete window.location;
         window.location = Object.defineProperties({}, windowLocationProperties);
@@ -44,18 +51,105 @@ describe("AuthCallbackRouter", () => {
         jest.clearAllMocks();
     });
 
-    it("will hopefully work", async () => {
+    it("navigates to the Home page if user has one valid org", async () => {
         // Mock the GET request
         const responseData = {
-
+            State: "State=some-state; SameSite=None; Secure; Path=/; Max-Age=0; HttpOnly",
+            SessionId:
+                "SessionId=8634b700-fe04-4c30-a95c-c10ad378ec5c; SameSite=None; Secure; Path=/; Max-Age=3592; HttpOnly",
+            RoleId: "RoleId=ADMIN; SameSite=None; Secure; Path=/; Max-Age=3592; HttpOnly",
             status: 200,
-            Organisations: [
-                {
-                    orgType: "GP Practice",
-                    orgName: "PORTWAY LIFESTYLE CENTRE",
-                    odsCode: "A9A5A",
-                },
-            ],
+            data: {
+                Organisations: [{ orgType: "GP Practice", orgName: "PORTWAY LIFESTYLE CENTRE", odsCode: "A9A5A" }],
+            },
+        };
+
+        axios.get.mockResolvedValue(responseData);
+
+        render(<AuthCallbackRouter />);
+
+        // Wait for the navigation to occur
+        await waitFor(() => {
+            expect(axios.get).toHaveBeenCalledWith(`${baseAPIUrl}/Auth/TokenRequest`, {
+                params,
+                withCredentials: true,
+            });
+
+            expect(axios.get).toHaveBeenCalledTimes(2);
+            expect(mockNavigate).toHaveBeenCalledWith(routes.HOME);
+        });
+    });
+
+    it("returns a loading state until redirection to token request handler", async () => {
+        render(<AuthCallbackRouter />);
+
+        expect(screen.getByRole("Spinner", { name: "Logging in..." })).toBeInTheDocument();
+    });
+
+    it("navigates to the auth error page when response status code is 403", async () => {
+        useNavigate.mockImplementation(() => mockNavigate);
+        // Mock the expected error response
+        const errorResponse = {
+            response: {
+                status: 403,
+                message: "403 forbidden",
+            },
+        };
+
+        axios.get.mockRejectedValueOnce(errorResponse);
+
+        render(<AuthCallbackRouter />);
+
+        // Wait for the navigation to occur
+        await waitFor(() => {
+            expect(axios.get).toHaveBeenCalledWith(`${baseAPIUrl}/Auth/TokenRequest`, {
+                params,
+                withCredentials: true,
+            });
+            expect(axios.get).toHaveBeenCalledTimes(1);
+            expect(mockNavigate).toHaveBeenCalledWith(routes.AUTH_ERROR);
+        });
+    });
+
+    it("navigates to the no valid organisation page when response status code is 401", async () => {
+        // Mock the expected error response
+        const errorResponse = {
+            response: {
+                status: 401,
+                message: "401 unauthorised",
+            },
+        };
+
+        axios.get.mockRejectedValueOnce(errorResponse);
+
+        render(<AuthCallbackRouter />);
+
+        // Wait for the navigation to occur
+        await waitFor(() => {
+            expect(axios.get).toHaveBeenCalledWith(`${baseAPIUrl}/Auth/TokenRequest`, {
+                params,
+                withCredentials: true,
+            });
+            expect(axios.get).toHaveBeenCalledTimes(1);
+            expect(mockNavigate).toHaveBeenCalledWith(routes.NO_VALID_ORGANISATION);
+        });
+    });
+
+    it("navigates to the org selection page when response status code is 200 and more than one org is included", async () => {
+        // Mock the expected response
+        const responseData = {
+            State: "State=some-state; SameSite=None; Secure; Path=/; Max-Age=0; HttpOnly",
+            SessionId:
+                "SessionId=8634b700-fe04-4c30-a95c-c10ad378ec5c; SameSite=None; Secure; Path=/; Max-Age=3592; HttpOnly",
+            RoleId: "RoleId=ADMIN; SameSite=None; Secure; Path=/; Max-Age=3592; HttpOnly",
+            status: 200,
+            data: {
+                Organisations: [
+                    { orgType: "GP Practice", orgName: "Town GP", odsCode: "A100" },
+                    { orgType: "Dev", orgName: "City clinic", odsCode: "A142" },
+                    { orgType: "Primary Care Support England", orgName: "National care support", odsCode: "A410" },
+                ],
+            },
         };
 
         axios.get.mockResolvedValue(responseData);
@@ -69,127 +163,7 @@ describe("AuthCallbackRouter", () => {
                 withCredentials: true,
             });
             expect(axios.get).toHaveBeenCalledTimes(1);
-            expect(mockNavigate).toHaveBeenCalledWith(routes.AUTH_SUCCESS);
+            expect(mockNavigate).toHaveBeenCalledWith(routes.ORG_SELECT);
         });
     });
-
-    //     it("navigates to the token request handler URl", async () => {
-    //         // Mock the GET request
-    //         const responseData = {
-    //             State: "State=some-state; SameSite=None; Secure; Path=/; Max-Age=0; HttpOnly",
-    //             SessionId:
-    //                 "SessionId=8634b700-fe04-4c30-a95c-c10ad378ec5c; SameSite=None; Secure; Path=/; Max-Age=3592; HttpOnly",
-    //             RoleId: "RoleId=ADMIN; SameSite=None; Secure; Path=/; Max-Age=3592; HttpOnly",
-    //             response: {
-    //                 status: 200,
-    //                 message: {
-    //                     Organisations: [{ orgType: "GP Practice", orgName: "Town GP", odsCode: "A100" }],
-    //                 },
-    //             },
-    //         };
-    //
-    //         axios.get.mockResolvedValue(responseData);
-    //
-    //         render(<AuthCallbackRouter />);
-    //
-    //         // Wait for the navigation to occur
-    //         await waitFor(() => {
-    //             expect(axios.get).toHaveBeenCalledWith(`${baseAPIUrl}/Auth/TokenRequest`, {
-    //                 params,
-    //                 withCredentials: true,
-    //             });
-    //             expect(axios.get).toHaveBeenCalledTimes(1);
-    //             expect(mockNavigate).toHaveBeenCalledWith(routes.AUTH_SUCCESS);
-    //         });
-    //     });
-
-    //     it("returns a loading state until redirection to token request handler", async () => {
-    //         render(<AuthCallbackRouter />);
-    //
-    //         expect(screen.getByRole("Spinner", { name: "Logging in..." })).toBeInTheDocument();
-    //     });
-    //
-    //     it("navigates to the auth error page when response status code is 403", async () => {
-    //
-    //         useNavigate.mockImplementation(() => mockNavigate);
-    //         // Mock the expected error response
-    //         const errorResponse = {
-    //             response: {
-    //                 status: 403,
-    //                 message: "403 forbidden",
-    //             },
-    //         };
-    //
-    //         axios.get.mockRejectedValueOnce(errorResponse);
-    //
-    //         render(<AuthCallbackRouter />);
-    //
-    //         // Wait for the navigation to occur
-    //         await waitFor(() => {
-    //             expect(axios.get).toHaveBeenCalledWith(`${baseAPIUrl}/Auth/TokenRequest`, {
-    //                 params,
-    //                 withCredentials: true,
-    //             });
-    //             expect(axios.get).toHaveBeenCalledTimes(1);
-    //             expect(mockNavigate).toHaveBeenCalledWith(routes.AUTH_ERROR);
-    //         });
-    //     });
-    //
-    //     it("navigates to the no valid organisation page when response status code is 401", async () => {
-    //             // Mock the expected error response
-    //                     const errorResponse = {
-    //                         response: {
-    //                             status: 403,
-    //                             message: "403 forbidden",
-    //                         },
-    //                     };
-    //
-    //                     axios.get.mockRejectedValueOnce(errorResponse);
-    //
-    //                     render(<AuthCallbackRouter />);
-    //
-    //                     // Wait for the navigation to occur
-    //                     await waitFor(() => {
-    //                         expect(axios.get).toHaveBeenCalledWith(`${baseAPIUrl}/Auth/TokenRequest`, {
-    //                             params,
-    //                             withCredentials: true,
-    //                         });
-    //                         expect(axios.get).toHaveBeenCalledTimes(1);
-    //                         expect(mockNavigate).toHaveBeenCalledWith(routes.AUTH_ERROR);
-    //             });
-    //         });
-    //
-    //     it("navigates to the org selection page when response status code is 200 and more than one org is included", async () => {
-    //         // Mock the expected response
-    //         const responseData = {
-    //             State: "State=some-state; SameSite=None; Secure; Path=/; Max-Age=0; HttpOnly",
-    //             SessionId:
-    //                 "SessionId=8634b700-fe04-4c30-a95c-c10ad378ec5c; SameSite=None; Secure; Path=/; Max-Age=3592; HttpOnly",
-    //             RoleId: "RoleId=ADMIN; SameSite=None; Secure; Path=/; Max-Age=3592; HttpOnly",
-    //             response: {
-    //                 status: 200,
-    //                 message: {
-    //                     Organisations: [
-    //                         { orgType: "GP Practice", orgName: "Town GP", odsCode: "A100" },
-    //                         { orgType: "Dev", orgName: "City clinic", odsCode: "A142" },
-    //                         { orgType: "Primary Care Support England", orgName: "National care support", odsCode: "A410" },
-    //                     ],
-    //                 },
-    //             },
-    //         };
-    //
-    //         axios.get.mockResolvedValue(responseData);
-    //
-    //         render(<AuthCallbackRouter />);
-    //
-    //         // Wait for the navigation to occur
-    //         await waitFor(() => {
-    //             expect(axios.get).toHaveBeenCalledWith(`${baseAPIUrl}/Auth/TokenRequest`, {
-    //                 params,
-    //                 withCredentials: true,
-    //             });
-    //             expect(axios.get).toHaveBeenCalledTimes(1);
-    //             expect(mockNavigate).toHaveBeenCalledWith(routes.ORG_SELECT);
-    //         });
-    //     });
 });
