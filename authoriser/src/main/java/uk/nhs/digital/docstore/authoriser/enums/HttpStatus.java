099d9e5d00e35ca72924f197137c7c50d4d320b8
Richard Smith <richard.smith33@nhs.net>
2023-08-04 11:51:31 +0100

    [prmt-3556] Code review

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/enums/HttpStatus.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/enums/HttpStatus.java
@@ -1,10 +1,12 @@
 package uk.nhs.digital.docstore.authoriser.enums;
 
 public enum HttpStatus {
+    OK(200),
     SEE_OTHER(303),
     BAD_REQUEST(400),
     UNAUTHORISED(401),
     FORBIDDEN(403),
+    NOT_FOUND(404),
     INTERNAL_ERROR(500);
 
     public final int code;
