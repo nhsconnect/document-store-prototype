b37bd1f84c14d0dd4e637936763f55d0c1d76b6d
Richard Smith <richard.smith33@nhs.net>
2023-08-07 14:31:29 +0100

    [prmt-3556] Unit test fix

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/VerifyOrganisationHandler.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/handlers/VerifyOrganisationHandler.java
@@ -11,7 +11,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
