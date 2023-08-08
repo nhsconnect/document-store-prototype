db844b305a099f486ffc0dc82c1de53dbc6c3db3
Richard Smith <richard.smith33@nhs.net>
2023-08-08 11:12:00 +0100

    [prmt-3542] Make app URLs constants
    Co-authored-by: AlexHerbertNHS <alexandra.herbert1@nhs.net>

--- authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/Utils.java
+++ authoriser/src/main/java/uk/nhs/digital/docstore/authoriser/Utils.java
@@ -6,6 +6,10 @@ import org.slf4j.LoggerFactory;
 
 public class Utils {
     public static final Logger LOGGER = LoggerFactory.getLogger(Utils.class);
+    private static final String PROD_URL =
+            "https://access-request-fulfilment.patient-deductions.nhs.uk";
+    private static final String NON_PROD_URL =
+            "https://%s.access-request-fulfilment.patient-deductions.nhs.uk";
 
     public static String decodeURL(String value) {
         return java.net.URLDecoder.decode(value, StandardCharsets.UTF_8);
@@ -16,10 +20,8 @@ public class Utils {
         String workspace = System.getenv("WORKSPACE");
         String url =
                 (workspace == null || workspace.isEmpty())
-                        ? "https://access-request-fulfilment.patient-deductions.nhs.uk"
-                        : String.format(
-                                "https://%s.access-request-fulfilment.patient-deductions.nhs.uk",
-                                workspace);
+                        ? PROD_URL
+                        : String.format(NON_PROD_URL, workspace);
         if (url == null) {
             LOGGER.warn("Missing required environment variable: " + AMPLIFY_BASE_URL_ENV_VAR);
             return "__unset__AMPLIFY_BASE_URL";
